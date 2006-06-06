2b19d81b128b4d0fa5830349845a1dbb651b46ca
Ivan Lucas <ivanlucas@users.sourceforge.net>
2006-06-06 16:53:25 +0000

    Stub for inbound email handling
    git-svn-id: https://sitracker.svn.sourceforge.net/svnroot/sitracker@180 6e0da8f0-d90d-0410-b14f-823b0ad1652c

---
+++ htdocs/inboundemail.php
@@ -0,0 +1,21 @@
+<?php
+// inboundemail.php - Process incoming emails
+//
+// SiT (Support Incident Tracker) - Support call tracking system
+// Copyright (C) 2000-2006 Salford Software Ltd.
+//
+// This software may be used and distributed according to the terms
+// of the GNU General Public License, incorporated herein by reference.
+//
+// Author: Ivan Lucas
+
+
+// The intention is that emails are piped to this script
+
+require('db_connect.inc.php');
+require('functions.inc.php');
+require('mime_email.class.php');
+
+
+
+?>
