be65d75e15f912b36c9d7604a131b76344cb635a
Paul Heaney <paulheaney@users.sourceforge.net>
2006-06-07 20:39:30 +0000

    added mention of multiple global signatures to the relevent help section.
    git-svn-id: https://sitracker.svn.sourceforge.net/svnroot/sitracker@185 6e0da8f0-d90d-0410-b14f-823b0ad1652c

--- htdocs/help/help-43.inc.php
+++ htdocs/help/help-43.inc.php
@@ -2,6 +2,8 @@
 A signature to insert at the bottom of outgoing emails. It's recommended that
 you begin this signature with two dashes, a space and a line feed.</p>
 
+<p>Multiple global signatures can be defined one of which will be choosen at random to be inserted at the bottom of each outgoing email.</p>
+
 <p>Remember that any changes here will be effective immediately and outgoing emails with the
 &lt;globalsignature&gt; placeholderwill carry the new signature.</p>
 
