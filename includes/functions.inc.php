d7c2b45f3889b609fc290f20eca4e0bd3617d65c
Ivan Lucas <ivanlucas@users.sourceforge.net>
2006-07-10 12:33:20 +0000

    Up version number to 3.24 alpha-2
    git-svn-id: https://sitracker.svn.sourceforge.net/svnroot/sitracker@240 6e0da8f0-d90d-0410-b14f-823b0ad1652c

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -18,7 +18,7 @@
 // Version number of the application, (numbers only)
 $application_version='3.24';
 // Revision string, e.g. 'beta2' or ''
-$application_revision='alpha-1';
+$application_revision='alpha-2';
 
 // Clean PHP_SELF server variable to avoid potential XSS security issue
 $_SERVER['PHP_SELF'] = substr($_SERVER['PHP_SELF'], 0, (strlen($_SERVER['PHP_SELF']) - @strlen($_SERVER['PATH_INFO'])));
