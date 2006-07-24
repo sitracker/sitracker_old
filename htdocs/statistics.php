6b4db9ca3702f7f2a686a932f929dd9a3c98da89
Ivan Lucas <ivanlucas@users.sourceforge.net>
2006-07-24 12:34:27 +0000

    Fix spelling
    git-svn-id: https://sitracker.svn.sourceforge.net/svnroot/sitracker@282 6e0da8f0-d90d-0410-b14f-823b0ad1652c

--- htdocs/statistics.php
+++ htdocs/statistics.php
@@ -28,11 +28,11 @@ function give_overview()
     if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
     $todaysincidents=mysql_num_rows($result);
     mysql_free_result($result);
-    
+
     $string = "<h4>$todaysincidents Incidents logged today<h4>";
     if($todaysincidents > 0)
     {
-        $string .= "<table align='center' width='50%'><tr><td colspan='2'>Which where assigned as follows:</td></tr>";
+        $string .= "<table align='center' width='50%'><tr><td colspan='2'>Which were assigned as follows:</td></tr>";
         $sql = "SELECT count(incidents.id), realname, users.id AS owner FROM incidents, users WHERE opened > '$todayrecent' AND incidents.owner = users.id GROUP BY owner DESC";
 
         $result = mysql_query($sql);
@@ -40,7 +40,7 @@ function give_overview()
         while($row = mysql_fetch_array($result))
         {
             $sql = "SELECT id, title FROM incidents WHERE opened > '$todayrecent' AND owner = '".$row['owner']."'";
-            
+
             $string .= "<tr><th>".$row['count(incidents.id)']."</th>";
             $string .= "<td class='shade2' align='left'><a href='incidents.php?user=".$row['owner']."&amp;queue=1&amp;type=support'>".$row['realname']."</a> ";
 
@@ -50,25 +50,25 @@ function give_overview()
             {
                 $string .= "<small><a href=\"javascript:incident_details_window('".$irow['id']."', 'incident".$irow['id']."')\"  title='".$irow['title']."'>[".$irow['id']."]</a></small> ";
             }
-            
+
             $string .= "</td></tr>";
         }
         $string .= "</table>";
     }
 
-    
+
     // Count incidents closed today
 
     $sql = "SELECT id FROM incidents WHERE closed > '$todayrecent'";
     $result = mysql_query($sql);
     if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
     $todaysclosed=mysql_num_rows($result);
-    
+
 
     $string .= "<h4>$todaysclosed Incidents closed today</h4>";
     if($todaysclosed > 0)
     {
-        
+
         $sql = "SELECT count(incidents.id), realname, users.id AS owner FROM incidents, users WHERE closed > '$todayrecent' AND incidents.owner = users.id GROUP BY owner";
         $string .= "<table align='center' width='50%'>";
         $result = mysql_query($sql);
@@ -76,12 +76,12 @@ function give_overview()
         while($row = mysql_fetch_array($result))
         {
             $string .= "<tr><th colspan='4' align='left'>".$row['count(incidents.id)']." Closed by ".$row['realname']."</th></tr>";
-            
+
             $sql = "SELECT incidents.id, incidents.title, closingstatus.name ";
             $sql .= "FROM incidents, closingstatus ";
             $sql .= "WHERE incidents.closingstatus = closingstatus.id AND closed > '$todayrecent' AND incidents.owner = '".$row['owner']."' ORDER BY closed";
 
-            
+
     	    $string .= "<tr><th>ID</th><th>Title</th><th>Owner</th><th>Closing status</th></tr>";
             $iresult = mysql_query($sql);
             if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
@@ -112,7 +112,7 @@ switch($mode)
     default:
         echo "<h2>$title - Overview</h2>";
         echo give_overview();
-        break;        
+        break;
 }
 
 include('htmlfooter.inc.php');
