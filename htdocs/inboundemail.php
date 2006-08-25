5800fa8c46d9767ceac4423adc28f29c47a0755e
Ivan Lucas <ivanlucas@users.sourceforge.net>
2006-08-25 10:14:49 +0000

    Inbound email stub
    git-svn-id: https://sitracker.svn.sourceforge.net/svnroot/sitracker@316 6e0da8f0-d90d-0410-b14f-823b0ad1652c

--- htdocs/inboundemail.php
+++ htdocs/inboundemail.php
@@ -1,3 +1,4 @@
+#!/usr/bin/php
 <?php
 // inboundemail.php - Process incoming emails
 //
@@ -10,12 +11,83 @@
 // Author: Ivan Lucas
 
 
+// TODO Stub
+// This script is uninished as of 25 Aug 06, committing it to SVN so I can work on it
+// elsewhere -- Ivan
 // The intention is that emails are piped to this script
 
-require('db_connect.inc.php');
-require('functions.inc.php');
+echo "Mail decoder\n";
+//require('db_connect.inc.php');
+//require('functions.inc.php');
 require('mime_email.class.php');
 
 
+
+// read the email from stdin (it should be piped to us by the MTA)
+$fp = fopen("php://stdin", "r");
+$rawemail = '';
+while (!feof($fp))
+{
+    $rawemail .= fread($fp, 1024);
+}
+fclose($fp);
+
+// Create and populate the email object
+$email = new mime_email;
+$email->set_emaildata($rawemail);
+
+print_r($email);
+//echo "------------------------------\n\n\n\n";
+
+$decoded_email = $email->go_decode();
+
+echo "Decoded mail...\n";
+//print_r($decoded_email->mime_block);
+echo "----\n----\n-------------------------------\n\n\n\n";
+$part=1;
+foreach($decoded_email->mime_block AS $block)
+{
+    // Do the decoding
+    switch ($block->mime_transferenc)
+    {
+        case 'quoted-printable':
+            $block->mime_content = quoted_printable_decode($block->mime_content);
+        break;
+
+        case 'base64':
+            $block->mime_content = base64_decode($block->mime_content);
+        break;
+
+        default:
+            // Do no decoding
+    }
+    // Extract any inline text into the incident log (if it's HTML strip the tags first)
+    if ($block->mime_contentdisposition=='inline')
+    {
+        switch ($block->mime_contenttype)
+        {
+            case 'text/plain':
+                $message .= htmlentities($block->mime_content);
+            break;
+
+            case 'text/html':
+                $message .= htmlentities(strip_tags($block->mime_content));
+            break;
+
+            default:
+                $message .= "Inline content of type {$block->mime_contenttype} ommitted.";
+        }
+    }
+    else
+    {
+        $filename=str_replace(' ','_',$block->mime_contentdispositionname);
+        if (empty($filename)) $filename = "part{$part}";
+        echo "FILENAME: $filename\n\n\n";
+    }
+    $part++;
+}
+
+echo $message;
+echo "---------------------------------------\n\n\n\n";
 
 ?>
--- includes/mime_email.class.php
+++ includes/mime_email.class.php
@@ -1,5 +1,5 @@
-<?php 
-# 
+<?php
+#
 # mime_email - PHP class for decoding MIME Emails
 # Version 0.1
 #
@@ -8,56 +8,56 @@
 # just include it and go decoding your emails.
 # Its pretty hard to follow all the email RFCs, so i went a different
 # way, i looked at many emails and hope to be RFC compliant :)
-# 
+#
 # Copyright 2001
 # Marc Logemann (loge@mail.com)
 #
 # Originally written for www.phpgroupware.org
-# 
-# Licence: 
+#
+# Licence:
 # GPL
-# 
-# 
-# Methods: 
-# 
-# mime_email() 
-#    Constructor used when creating a new instance of the mime_devode class. 
-#    Returns true on success. 
-#    ie. 
+#
+#
+# Methods:
+#
+# mime_email()
+#    Constructor used when creating a new instance of the mime_devode class.
+#    Returns true on success.
+#    ie.
 #        $a=new mime_email;
-# 
-# 
-# set_emaildata(string email) 
+#
+#
+# set_emaildata(string email)
 #    method to submit the raw email data in order work with it furthermore
-#    Returns true on success. 
-#    ie. 
+#    Returns true on success.
+#    ie.
 #        $a->set_emaildata($youremail);
 #
-# 
+#
 # set_htmlentities(boolean bool)
 #	 method where you can turn on htmlentities conversion of all attributes
 #    must be called before go_decode* methods of course
 #
 #
-# go_decode() 
+# go_decode()
 #    method that will do the nasty work to parse the email in a way the w3c wants it :)
 #    Returns mime_mail object ($this)
-#    ie. 
+#    ie.
 #        $a->go_decode();
 #
 #
-# go_decode_headeronly() 
+# go_decode_headeronly()
 #    method that will do the nasty work to parse the email in a way the w3c wants it :)
 #	 but compared to go_decode, this method will only parse the header, useful for
 #	 displaying lists which only contain header informations
 #    Returns mime_mail object ($this)
-#    ie. 
+#    ie.
 #        $a->go_decode();
 #
 
 class mime_email
-{ 
-    
+{
+
 // =============================================
 // Public Attributes
 // =============================================
@@ -65,7 +65,7 @@ class mime_email
     var $from;  							# from-header value
     var $to;								# to-header value
     var $replyto;							# replyto-header value
-    var $in_replyto;						# inreplyto-header value (Reference to 
+    var $in_replyto;						# inreplyto-header value (Reference to
     										# message which this  message is a reply to.)
 
     var $date;								# date-header value
@@ -77,31 +77,31 @@ class mime_email
 	var $importance;						# importance header value
 	var $sensitivity;						# sensitivity header value
 	var $disposition_notification_to;		# MDN header value
-	
+
     var $contenttype;						# content-type-header value (the real content type i.e. text/plain)
 	var $contenttypebound;					# content-type-boundary value (boundary value of content type)
-	var $contenttypecharset;				# content-type-charset value (charset value of content type)	
-	var $contenttransencoding;				# content-transferencoding value (encoding value of content type)		
-	
+	var $contenttypecharset;				# content-type-charset value (charset value of content type)
+	var $contenttransencoding;				# content-transferencoding value (encoding value of content type)
+
 	var $emailtextplain;					# email text (plain)
 	var $emailtexthtml;						# email text (html)
-	
+
 	var $xmailer;							# signature of sending email client
 	var $xpriority;							# X-Priority header
 
     var $mime_block = array(); 				# array of mime_block objects
-    
+
     var $isHTMLmail;						# indicates whether this i an HTML email
     var $isATTACHMENTmail;					# indicates whether this mail has attachments
     var $from_name;							# only the name of FROM field (for example "James Clark <clark@clark.com>"
     										#								only James Clark will be there)
 	var $from_email;						# only the email of FROM field (for example "James Clark <clark@clark.com>"
 											#								only clark@clark.com will be there)
-    
+
 // =============================================
 // Private Attributes
 // =============================================
-    
+
     var $rawemail = array();				# complete email, with all headers and content
     var $varhtmlentities = false;			# true if each special char in the attributes should be entities
     var $bodystart; 						# array index where the body starts
@@ -109,32 +109,35 @@ class mime_email
 // =============================================
 // Public Methods
 // =============================================
-    
-    function mime_email(){ 
-    } 
 
-    function set_emaildata ($myemail){ 
+    function mime_email(){
+    }
+
+    function set_emaildata ($myemail){
+        $myemail = str_replace("\r", null, $myemail);
+        $myemail = explode("\n",$myemail);
+
     	$this->rawemail = $myemail;
     }
 
 	function set_htmlentities($boolean) {
 		$this->varhtmlentities = $boolean;
 	}
-    
+
     function go_decode() {
 		$this->parseHeader($this->rawemail);
 		$this->parseBody($this->rawemail);
 		return $this;
 	}
-	
-	
+
+
     function go_decode_headeronly() {
 		$this->parseHeader($this->rawemail);
 		return $this;
 	}
-		
+
     function dump($complete_email = false) {
-	
+
 		if($complete_email) {
 			echo "============= RAW EMAIL================<br> ";
 			while ( list ($linenbr, $line) = each ($this->rawemail) ) {
@@ -144,7 +147,7 @@ class mime_email
 		echo "==============<br>";
 		echo " AFTER PARSING<br>";
 		echo "==============<br>";
-		
+
 		echo "============= Standard Headerfields================<br> ";
 		echo "From: ".$this->from ."<br>";
 		echo "To: ".$this->to ."<br>";
@@ -188,30 +191,29 @@ class mime_email
     # a method for performance measuring, we have some problems
     # with big attachments, so we need this internally right now
     # hopefully we can delete this method soon :)
-    
-    function getmicrotime(){ 
-    	list($usec, $sec) = explode(" ",microtime()); 
-    	return ((float)$usec + (float)$sec); 
-	} 
-	
-	
+
+    function getmicrotime(){
+    	list($usec, $sec) = explode(" ",microtime());
+    	return ((float)$usec + (float)$sec);
+	}
+
+
 	# function to grab all the necessary data from the email header and put
 	# them into the object/attributes
-	
+
 	function parseHeader($rawemail)
 	{
 		// for performance measuring
 		// $time_start = $this->getmicrotime();
-
 		while ( list ($linenbr, $line) = each ($rawemail) ) {
 
 			$hc++;
-			
+
 			// if there is a blank line, we know that the header ended and
 			// now the body begins, but we dont need the body right now,
 			// so we break out of the loop
 			if(trim($line) == "") {$this->bodystart = $hc; break;}
-			
+
     		if ( (!$from) and (eregi ("^From:(.*)", $line, $match)) ) {
       			$this->from = trim ( $match[1] );
 
@@ -229,13 +231,13 @@ class mime_email
 
 	    	} elseif (eregi ("^Reply-to:(.*)", $line, $match)) {
     	  		$this->replyto = trim ( $match[1] );
-    	  		
+
 			} elseif (eregi ("^Message-ID:(.*)", $line, $match)) {
 				$this->message_id = trim ( $match[1] );
 
 			} elseif (eregi ("^In-Reply-To:(.*)", $line, $match)) {
 				$this->in_replyto = trim ( $match[1] );
-    	  		
+
 	    	} elseif (eregi ("^Sensitivity:(.*)", $line, $match)) {
     	  		$this->sensitivity = trim ( $match[1] );
 
@@ -244,10 +246,10 @@ class mime_email
 
 	    	} elseif (eregi ("^Disposition-Notification-To:(.*)", $line, $match)) {
     	  		$this->disposition_notification_to = trim ( $match[1] );
-    	
+
     		} elseif (eregi ("^X-Priority:(.*)", $line, $match)) {
     	  		$this->xpriority = trim ( $match[1] );
-	    	
+
 	    	} elseif (eregi ("^X-Mailer:(.*)", $line, $match)) {
     	  		$this->xmailer = trim ( $match[1] );
 
@@ -266,7 +268,7 @@ class mime_email
 					$this->contenttypebound = $myhash[boundary];
 					$this->contenttypecharset = $myhash[charset];
 				}
-				
+
 	    	} elseif (eregi ("^Content-Transfer-Encoding:(.*)", $line, $match)) {
 	    		$this->contenttransencoding = trim ( $match[1] );
 
@@ -284,7 +286,7 @@ class mime_email
 			}
 	   	  }
     	}
-    	
+
     	//split from adress like: Firstname Lastname <foo@bar.com>  into two data pieces
     	// name will be stored in from_name and adress will be stored in from_email
 		// cut away these silly quotations BTW (if there are any)
@@ -296,7 +298,7 @@ class mime_email
 			$this->from_email = $this->from;
 			$this->from_name = str_replace("\"","",$this->from);
 		}
-    	
+
     	// do some FLAG things (only the first shot, a multipart/mixed can also be HTML email, but this
     	// 						will be checked in the body part, not here)
     	if(strtolower($this->contenttype) == "multipart/mixed") $this->isATTACHMENTmail = true;
@@ -304,7 +306,7 @@ class mime_email
     	if(strtolower($this->contenttype) == "text/plain") {$this->isHTMLmail = false;$this->isATTACHMENTmail = false;}
     	$ctypecontinue = false;
     	$ctypelinehold = "";
-    	
+
     	// if htmlentities flag is on, convert all normally visible attributes according to htmlentities
     	if($varhtmlentities) {
     		$this->from = htmlentities($this->from);
@@ -313,18 +315,18 @@ class mime_email
     		$this->replyto = htmlentities($this->replyto);
     		$this->xmailer = htmlentities($this->xmailer);
 		}
-    	
+
    	// for performance measuring
    	// $time_end = $this->getmicrotime();
 	// $time = $time_end - $time_start;
 	// echo "<h3>echo *HEADERPARSE Start: $time_start   End: $time_end    Diff: $time</h3>";
-    
+
     }
 
-	
+
 	# function to grab all the necessary data from the email body and put
 	# them into the object/attributes
-	
+
 	function parseBody($rawemail)
 	{
 		if($this->contenttype == "text/plain" or $this->contenttype=="") {
@@ -334,12 +336,12 @@ class mime_email
 			for($l=$this->bodystart;$l<count($rawemail);$l++) {
 				$body .= $rawemail[$l];
 			}
-			
+
 			if($this->contenttransencoding=="quoted-printable")
 				$this->emailtextplain = quoted_printable_decode($body);
 			else
 				$this->emailtextplain = $body;
-			
+
 			if($varhtmlentities) $this->emailtextplain = htmlentities($this->emailtextplain);
 		}
 		else {
@@ -353,36 +355,36 @@ class mime_email
 
 	# parse given Boundary Marker, this has to be a function because for example a html email
 	# with attachments has several (mostly 2) nested Boundary Markers
-	
+
 	function parseBoundary($boundary, $rawemail) {
-		
+
 		// default value for newboundary, because if we leave it blank, our
 		// regex below would become true if some write in his email things like "--"
 		// so our app would thing that this is a boundary marker
 		// ThisIsAUniqueLineFromMarcLogemann should be unique, i cant imagine that someone
 		// writes this into his email
 		$newboundary = "ThisIsAUniqueLineFromMarcLogemann";
-		
+
     	// for performance measuring
     	// $time_start = $this->getmicrotime();
-		
+
 		while ( list ($linenbr, $line) = each ($rawemail) ) {
 
 			$line = trim($line);
-			
+
 			if(($hit and eregi("^--".$boundary,$line)) or
 				($hit and eregi("^--".$boundary."--",$line)) or
 				($hit and eregi("^--".$newboundary,$line)) or
 				($hit and eregi("^--".$newboundary."--",$line))) { $hit = false; $body = false;}
-				
+
 			if($hit) {
 				$i++;
 				// -------------------------------------------------------
 				if(eregi ("^Content-Type:(.*)", $line, $match)) {
-					
+
 					$my_mime_block = new mime_block();
 					$cdispocontinue = false;
-					
+
 					//check to see if content-type is splitted up into several lines
 					//if yes, the first line ends with a colon ";"
 					if(substr($match[1], strlen(trim($match[1])),1)==";") {
@@ -396,11 +398,11 @@ class mime_email
 						$my_mime_block->setMimeContentTypeBoundary($myhash[boundary]);
 						$my_mime_block->setMimeContentTypeCharset($myhash[charset]);
 						$my_mime_block->setMimeContentTypeName($myhash[name]);
-						
-						if($my_mime_block->getMimeContentTypeBoundary()!="") 
+
+						if($my_mime_block->getMimeContentTypeBoundary()!="")
 							$newboundary = $myhash[boundary];
 					}
-					
+
 				// -------------------------------------------------------
 				} elseif(eregi ("^Content-Transfer-Encoding:(.*)", $line, $match)) {
 					$my_mime_block->setMimeTransferEncoding(htmlentities(trim ( $match[1] )));
@@ -409,12 +411,12 @@ class mime_email
 
 				// -------------------------------------------------------
 				} elseif(eregi ("^Content-Disposition:(.*)", $line, $match)) {
-					
+
 					$ctypecontinue = false;
-					
+
 					//check to see if content-disposition is splitted up into several lines
 					//if yes, the first line ends with a colon ";"
-					
+
 					if(substr(trim($match[1]), strlen(trim($match[1]))-1,1)==";") {
 						$cdispocontinue = true;
 						$cdispolinehold = $match[1];
@@ -473,7 +475,7 @@ class mime_email
 					}
 				}
 	   	  		// -------------------------------------------------------
-				
+
 				// we append each line oif the body and append manually a newline...
 				if($body) {
 					 $bodycontent .= $line . "\n";
@@ -481,11 +483,11 @@ class mime_email
 
 				if($line =="") $body = true;
 			}
-				
+
 			if($line == "--" . $boundary OR $line == "--" . $boundary . "--") {
 				$z++;
 				$hit = true;
-					
+
 				// only add content to array if this is not the first MIME marker
 				if($z>1) {
 					$my_mime_block->setMimeContent($bodycontent);
@@ -494,68 +496,68 @@ class mime_email
 						if($varhtmlentities) $this->emailtextplain = htmlentities($bodycontent);
 						else $this->emailtextplain = $bodycontent;
 					}
-					
+
 					if($myhash[type]=="text/html") {
 						$this->emailtexthtml = $bodycontent;
 						$this->isHTMLmail = true;
 					}
 				}
-					
+
 				// clear bodycontent var
-				$bodycontent = ""; 
-				
+				$bodycontent = "";
+
 				// if boundary ends....leave the loop, because of performance gains with large attachment
 				if($line == "--" . $boundary . "--") break;
 			}
 		}
-	    
+
 	    // for performance measuring
 	    //$time_end = $this->getmicrotime();
 		//$time = $time_end - $time_start;
 		//echo "<h3>echo *LOOP Start: $time_start   End: $time_end    Diff: $time</h3>";
-		
+
 		if($newboundary != "" and $newboundary != "ThisIsAUniqueLineFromMarcLogemann") $this->parseBoundary($newboundary, $rawemail);
 	}
-	
-	
+
+
 	# function to grab all the different content-type data (like boundary, charset and type)
 	# input w/o "Content-Type:" String
 	# return hashmap $array[type], $array[boundary], $array[charset]
-	
-	
+
+
 	function parseContentTypeLine($ctline)
 	{
    		$ctline = trim($ctline);
-   		   		
+
    		if(!strpos($ctline,";")) $endpointer = strlen($ctline); else $endpointer = strpos($ctline,";");
     	$hashmap[type] = substr($ctline,0,$endpointer);
-    	
+
     	if(eregi("charset", $ctline)) {
     		if(strpos($ctline, ";", strpos($ctline, "charset"))>0) {
     			//
     			// if charset is not at the end of the string and therefore a colon is present
-				//    			
+				//
     			$substrfrom = strpos($ctline,"=",strpos($ctline,"charset"))+1;
     			$substrtoA = strpos($ctline,";", strpos($ctline, "charset"));
     			$substrtoB = strpos($ctline,"=",strpos($ctline,"charset"))+1;
-    	  				
+
     			$hashmap[charset] = str_replace("\"","", substr($ctline,	$substrfrom, $substrtoA - $substrtoB));
     	  		}
     	  	else {
     			//
     			// if charset is at the end of the content-type line, so there is only a \n at the end
 				//
-    			$hashmap[charset] = str_replace("\"","", substr($ctline, 
+    			$hashmap[charset] = str_replace("\"","", substr($ctline,
     	  							strpos($ctline,"=",strpos($ctline,"charset"))+1,
     	  							strlen($ctline)- strpos($ctline,"=", strpos($ctline, "charset"))));
-    	  	}    	  				
+    	  	}
     	}
-    	  		
-    	  		
-    	if(eregi("boundary", $ctline)) $hashmap[boundary] = str_replace("\"","", substr($ctline, 
+
+
+    	if(eregi("boundary", $ctline)) $hashmap[boundary] = str_replace("\"","", substr($ctline,
     	  												strpos($ctline,"=",strpos($ctline,"boundary"))+1,
     	  												strlen($ctline)- strpos($ctline,"=")));
-    	if(eregi("name", $ctline)) $hashmap[name] = str_replace("\"","", substr($ctline, 
+    	if(eregi("name", $ctline)) $hashmap[name] = str_replace("\"","", substr($ctline,
     	  												strpos($ctline,"=",strpos($ctline,"name"))+1,
     	  												strlen($ctline)- strpos($ctline,"=")));
     	return $hashmap;
@@ -564,16 +566,16 @@ class mime_email
 	# function to grab all the different content-type data (like boundary, charset and type)
 	# input w/o "Content-Type:" String
 	# return hashmap $array[type], $array[boundary], $array[charset]
-	
-	
+
+
 	function parseContentDispositionLine($cdline)
 	{
    		$cdline = trim($cdline);
 
    		if(!strpos($cdline,";")) $endpointer = strlen($cdline); else $endpointer = strpos($cdline,";");
     	$hashmap[type] = trim(substr($cdline,0,$endpointer));
-    	
-    	if(eregi("filename", $cdline)) $hashmap[name] = str_replace("\"","", substr($cdline, 
+
+    	if(eregi("filename", $cdline)) $hashmap[name] = str_replace("\"","", substr($cdline,
     	  												strpos($cdline,"=",strpos($cdline,"filename"))+1,
     	  												strlen($cdline)- strpos($cdline,"=")));
     	return $hashmap;
@@ -581,7 +583,7 @@ class mime_email
 }
 
 # Class mime_block
-# 
+#
 # If you have a MIME email, you have several MIME blocks in there (boundaries), these
 # blocks have their own content-type, transfer encoding and content itself
 # this class represents the data in such a boundary
@@ -591,18 +593,18 @@ class mime_email
 # getter and setter Methods for each attribute, its not mandatory to use them, but
 # because i programmed a lot java these days, i just cant manipulate the attributes
 # directly :) some kind of a psycho thing .... hehe
-# 
-# 
+#
+#
 
 class mime_block {
-	
+
 	var $mime_contenttype; 				# content-type (i.e. text/plain)
     var $mime_contenttypebound;			# coontent-type boundary marker
     var $mime_contenttypecharset; 		# content-type charset (i.e. iso-8859-1)
     var $mime_contenttypename;			# content-type name (i.e. filename.txt)
 
     var $mime_transferenc;				# transfer-encoding (i.e. base64)
-	
+
 	var $mime_contentdisposition;		# content-disposition (i.e. attachment or inline)
 	var $mime_contentdispositionname; 	# content-disposition name (i.e. filename.txt)
 
@@ -611,43 +613,43 @@ class mime_block {
 	function setMimeContentType($ctype) {
 		$this->mime_contenttype = $ctype;
 	}
-	
+
 	function getMimeContentType() {
 		return $this->mime_contenttype;
 	}
-	
+
 	function setMimeContentTypeBoundary($boundary) {
 		$this->mime_contenttypebound = $boundary;
 	}
-	
+
 	function getMimeContentTypeBoundary() {
 		return $this->mime_contenttypebound;
 	}
-	
+
 	function setMimeContentTypeCharset($charset) {
 		$this->mime_contenttypecharset = $charset;
 	}
-		
+
 	function getMimeContentTypeCharset() {
 		return $this->mime_contenttypecharset;
 	}
-	
+
 	function setMimeContentTypeName($name) {
 		$this->mime_contenttypename = $name;
 	}
-	
+
 	function getMimeContentTypeName() {
 		return $this->mime_contenttypename;
 	}
-	
+
 	function setMimeTransferEncoding($transenc) {
 		$this->mime_transferenc = $transenc;
 	}
-	
+
 	function getMimeTransferEncoding() {
 		return $this->mime_transferenc;
 	}
-	
+
 	function setMimeContentDisposition($cdispo) {
 		$this->mime_contentdisposition = $cdispo;
 	}
@@ -667,10 +669,10 @@ class mime_block {
 	function setMimeContent($content) {
 		$this->mime_content = $content;
 	}
-	
+
 	function getMimeContent() {
 		return $this->mime_content;
 	}
 }
-    
+
 ?>
