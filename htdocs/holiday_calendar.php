a6bd8852523c3e34e94ed91aafe5cd74f5052d67
Tom Gerrard <tomgerrard@users.sourceforge.net>
2006-10-31 18:09:20 +0000

    tidy
    git-svn-id: https://sitracker.svn.sourceforge.net/svnroot/sitracker@486 6e0da8f0-d90d-0410-b14f-823b0ad1652c

--- htdocs/holiday_calendar.php
+++ htdocs/holiday_calendar.php
@@ -537,15 +537,16 @@ else
         {
             echo "<table align='center'>";
             echo "<tr class='shade2'><td><strong>Dates waiting for approval</strong>:</td></tr>";
+            echo "<tr class='shade1'><td>";
             while ($dates = mysql_fetch_array($result))
             {
-                echo "<tr class='shade1'><td>";
                 echo date('l jS F Y', $dates['startdate']);
                 if ($dates['length']=='am') echo " Morning only";
                 if ($dates['length']=='pm') echo " Afternoon only";
-                echo "</td></tr>\n";
+                echo "<br/>\n";
             }
-            echo "<tr class='shade1'><td><a href='holiday_request.php'>Send reminder request</a></td></tr>";
+            echo "</td></tr>\n";
+            echo "<tr class='shade1'><td><a href='holiday_request.php'>Send holiday request</a></td></tr>";
             echo "</table>";
         }
         mysql_free_result($result);
