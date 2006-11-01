a88b0a9ec2df250a32aae84476730fe8f0769957
Tom Gerrard <tomgerrard@users.sourceforge.net>
2006-11-01 14:50:17 +0000

    Send out a mail when the holiday is approved or ejected
    git-svn-id: https://sitracker.svn.sourceforge.net/svnroot/sitracker@504 6e0da8f0-d90d-0410-b14f-823b0ad1652c

--- htdocs/holiday_approve.php
+++ htdocs/holiday_approve.php
@@ -35,6 +35,23 @@ if ($startdate!='all') $sql.="AND startdate='$startdate' AND type='$type' AND le
 $sql .= "AND approved='0'";
 $result = mysql_query($sql);
 
+$bodytext = "Message from {$CONFIG['application_shortname']}: ".$sit[2]." has ";
+if ($approve=='FALSE') $bodytext.="rejected";
+else $bodytext.="approved";
+echo " the following holidays:\n\n";
+    
+    $bodytext .= "\n";
+}
+$email_from = user_email($sit[2]);
+$email_to = user_email($user);
+$email_subject = "Re: {$CONFIG['application_shortname']}: Holiday Approval Request";
+$extra_headers  = "From: $email_from\nReply-To: $email_from\nErrors-To: {$CONFIG['support_email']}\n";
+$extra_headers .= "X-Mailer: {$CONFIG['application_shortname']} {$application_version_string}/PHP " . phpversion()."\n";
+$rtnvalue = mail($email_to, stripslashes($email_subject), stripslashes($bodytext), $extra_headers);
+
+if ($rtnvalue===TRUE) echo "<p align='center'>".user_realname($user)." has been notified of your decision</p>";
+else echo "<p class='error'>There was a problem sending your notification</p>";
+
 plugin_do('holiday_ack');
 
 if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
