608204b6fe856f0db248de840670cee7b87bf1fa
Ivan Lucas <ivanlucas@users.sourceforge.net>
2006-11-06 21:23:24 +0000

    Moved into trunk

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// index.php - Redirect to sit homepage
+//
+// SiT (Support Incident Tracker) - Support call tracking system
+// Copyright (C) 2000-2006 Salford Software Ltd.
+//
+// This software may be used and distributed according to the terms
+// of the GNU General Public License, incorporated herein by reference.
+//
+
+// This is a redirection page, there is nothing of interest here
+// SiT is run from the htdocs/ subdirectory which should be set as your
+// DocumentRoot (web root) directory
+
+// Documentation for SiT is in the doc/ subdirectory
+
+
+header("Location: htdocs/index.php");
+
+?>
