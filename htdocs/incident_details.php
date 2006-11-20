f342d90f69507cc45c020c122f9f07c23f62058f
Paul Heaney <paulheaney@users.sourceforge.net>
2006-11-20 20:21:38 +0000

    When viewing an incident from the holding queue you can only see the log (we dont want people doing things to the incident)

--- htdocs/incident_details.php
+++ htdocs/incident_details.php
@@ -22,6 +22,7 @@ require('auth.inc.php');
 $incidentid = cleanvar($_REQUEST['id']);
 $javascript = cleanvar($_REQUEST['javascript']); //is javascript enabled
 $action = cleanvar($_REQUEST['action']);
+$view = cleanvar($_REQUEST['view']);
 $id = $incidentid;
 $title='Details';
 
@@ -55,103 +56,114 @@ else
     </style>
     <?php
 
-    if($action != 'email')
+    if($view != 'lockedview')
     {
-        echo "<div id='mainTabContainer' dojoType='TabContainer' style='width: 100%; height: 500px' selectedTab='log'>";
+        if($action != 'email')
+        {
+            echo "<div id='mainTabContainer' dojoType='TabContainer' style='width: 100%; height: 500px' selectedTab='log'>";
+        }
+        else
+        {
+            echo "<div id='mainTabContainer' dojoType='TabContainer' style='width: 100%; height: 500px' selectedTab='Email'>";
+        }
     }
     else
     {
-        echo "<div id='mainTabContainer' dojoType='TabContainer' style='width: 100%; height: 500px' selectedTab='Email'>";
+        echo "<hr />";
     }
-    
+
     echo "<div id='log' dojoType='ContentPane' label='Incident Log'>";
     include('incident/log.inc.php');
     echo "</div>";
     
-    if (incident_status($id) != 2)
+    if($view != 'lockedview')
     {
-        if(user_permission($sit[2],8)) //udpate incidents
-        {
-            //echo "<a dojoType='LinkPane' href='update_incident.php?id={$id}' refreshOnShow='true' style='display: none'>Update</a>";
-            echo "<div id='Update' dojoType='ContentPane' label='Update'>";
-            include('incident/update.inc.php');
-            echo "</div>";
-        }
     
-        if(user_permission($sit[2],12)) //Reassign incident
+        if (incident_status($id) != 2)
         {
-            //echo "<a dojoType='LinkPane' href=reassign_incident.php?id={$id}' refreshOnShow='true' style='display: none'>Reassign</a>";
-            echo "<div id='Reassign' dojoType='ContentPane' label='Reassign'>";
-            include('incident/reassign.inc.php');
-            echo "</div>";
-        }
-    
-        if(user_permission($sit[2],7)) //Edit incident
-        {
-            //echo "<a dojoType='LinkPane' href=edit_incident.php?id={$id}' refreshOnShow='true' style='display: none'>Edit</a>";
-            echo "<div id='Edit' dojoType='ContentPane' label='Edit'>";
-            include('incident/edit.inc.php');
-            echo "</div>";
-        }
-    
-        if(user_permission($sit[2],18)) //Close incident
-        {
-            echo "<div id='Close' dojoType='ContentPane' label='Close'>";
-            include('incident/close.inc.php');
-            echo "</div>";
-        }
-    
-        if(user_permission($sit[2],6)) //View incident
-        {
-            //echo "<a dojoType='LinkPane' href=incident_service_levels.php?id={$id}' refreshOnShow='true' style='display: none'>Service</a>";
-            echo "<div id='sla' dojoType='ContentPane' label='Service'>";
-            include('incident/sla.inc.php');
-            echo "</div>";
-        }
-    
-        if(user_permission($sit[2],61)) //Incident details
-        {
-            //echo "<a dojoType='LinkPane' href=incident_relationships.php?id={$id}' refreshOnShow='true' style='display: none'>Relationships</a>";
-            echo "<div id='Relationships' dojoType='ContentPane' label='Relationships'>";
-            include('incident/relationships.inc.php');
-            echo "</div>";
-        }
-    
-        if(user_permission($sit[2],62)) //View attachements
-        {
-            //echo "<a dojoType='LinkPane' href=incident_attachments.php?id={$id}' refreshOnShow='true' style='display: none'>Files</a>";
-            echo "<div id='Files' dojoType='ContentPane' label='Files'>";
-            include('incident/files.inc.php');
-            echo "</div>";
-        }
-    
-        if(user_permission($sit[2],33)) //Send email
-        {
-            //echo "<a dojoType='LinkPane' href=incident_attachments.php?id={$id}' refreshOnShow='true' style='display: none'>Files</a>";
-            if($action == 'email')
+            if(user_permission($sit[2],8)) //udpate incidents
             {
-                //show send email form
-                echo "<div id='Email' dojoType='ContentPane' label='Email'>";
-                include('incident/send_email.inc.php');
+                //echo "<a dojoType='LinkPane' href='update_incident.php?id={$id}' refreshOnShow='true' style='display: none'>Update</a>";
+                echo "<div id='Update' dojoType='ContentPane' label='Update'>";
+                include('incident/update.inc.php');
                 echo "</div>";
             }
-            else
+        
+            if(user_permission($sit[2],12)) //Reassign incident
             {
-                //show select email form
-                echo "<div id='Email' dojoType='ContentPane' label='Email'>";
-                include('incident/email.inc.php');
+                //echo "<a dojoType='LinkPane' href=reassign_incident.php?id={$id}' refreshOnShow='true' style='display: none'>Reassign</a>";
+                echo "<div id='Reassign' dojoType='ContentPane' label='Reassign'>";
+                include('incident/reassign.inc.php');
                 echo "</div>";
             }
+        
+            if(user_permission($sit[2],7)) //Edit incident
+            {
+                //echo "<a dojoType='LinkPane' href=edit_incident.php?id={$id}' refreshOnShow='true' style='display: none'>Edit</a>";
+                echo "<div id='Edit' dojoType='ContentPane' label='Edit'>";
+                include('incident/edit.inc.php');
+                echo "</div>";
+            }
+        
+            if(user_permission($sit[2],18)) //Close incident
+            {
+                echo "<div id='Close' dojoType='ContentPane' label='Close'>";
+                include('incident/close.inc.php');
+                echo "</div>";
+            }
+        
+            if(user_permission($sit[2],6)) //View incident
+            {
+                //echo "<a dojoType='LinkPane' href=incident_service_levels.php?id={$id}' refreshOnShow='true' style='display: none'>Service</a>";
+                echo "<div id='sla' dojoType='ContentPane' label='Service'>";
+                include('incident/sla.inc.php');
+                echo "</div>";
+            }
+        
+            if(user_permission($sit[2],61)) //Incident details
+            {
+                //echo "<a dojoType='LinkPane' href=incident_relationships.php?id={$id}' refreshOnShow='true' style='display: none'>Relationships</a>";
+                echo "<div id='Relationships' dojoType='ContentPane' label='Relationships'>";
+                include('incident/relationships.inc.php');
+                echo "</div>";
+            }
+        
+            if(user_permission($sit[2],62)) //View attachements
+            {
+                //echo "<a dojoType='LinkPane' href=incident_attachments.php?id={$id}' refreshOnShow='true' style='display: none'>Files</a>";
+                echo "<div id='Files' dojoType='ContentPane' label='Files'>";
+                include('incident/files.inc.php');
+                echo "</div>";
+            }
+        
+            if(user_permission($sit[2],33)) //Send email
+            {
+                //echo "<a dojoType='LinkPane' href=incident_attachments.php?id={$id}' refreshOnShow='true' style='display: none'>Files</a>";
+                if($action == 'email')
+                {
+                    //show send email form
+                    echo "<div id='Email' dojoType='ContentPane' label='Email'>";
+                    include('incident/send_email.inc.php');
+                    echo "</div>";
+                }
+                else
+                {
+                    //show select email form
+                    echo "<div id='Email' dojoType='ContentPane' label='Email'>";
+                    include('incident/email.inc.php');
+                    echo "</div>";
+                }
+            }
+    
+            echo "<div id='escalate' dojoType='ContentPane' label='Escalate'>";
+            include('incident/escalate.inc.php');
+            echo "</div>";
+        
+            //echo "<a class='barlink' href='javascript:email_window({$id})' accesskey='E'><em>E</em>mail</a> | ";
+        
         }
-
-        echo "<div id='escalate' dojoType='ContentPane' label='Escalate'>";
-        include('incident/escalate.inc.php');
         echo "</div>";
-    
-        //echo "<a class='barlink' href='javascript:email_window({$id})' accesskey='E'><em>E</em>mail</a> | ";
-    
     }
-    echo "</div>";
     
     include('incident_html_bottom.inc.php');
 }
--- htdocs/webtrack.js
+++ htdocs/webtrack.js
@@ -3,7 +3,10 @@
 function incident_details_window(incidentid,win)
 {
     // URL = "incident.php?popup=yes&id=" + incidentid;
-    URL = "incident_details.php?id=" + incidentid + "&javascript=enabled";
+    if(win=='holdingview')
+        URL = "incident_details.php?id=" + incidentid + "&javascript=enabled&view=lockedview";
+    else
+        URL = "incident_details.php?id=" + incidentid + "&javascript=enabled";
     window.open(URL, "sit_popup", "toolbar=yes,status=yes,menubar=no,scrollbars=yes,resizable=yes,width=700,height=600");
 }
 
