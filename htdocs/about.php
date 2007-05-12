5fb0691795a935811fd0fe9774491a239d1958f8
Paul Heaney <paulheaney@users.sourceforge.net>
2007-05-12 14:07:56 +0000

    * Addition of dojo 0.4.2

--- htdocs/about.php
+++ htdocs/about.php
@@ -56,9 +56,11 @@ Completely free for commercial and non-commercial use.</p>
 <p align='center'>whatever:hover (csshover.htc) 1.41 by <a href='http://www.xs4all.nl/~peterned/'>Peter Nederlof</a><br />
 &copy; 2005 - Peter Nederlof.  Licensed under the LGPL.</p>
 
-<p align='center'>Dojo 0.3.1 by <a href='http://dojotoolkit.org/'>The Dojo Foundation</a><br />
-Copyright &copy; 2004-2006, The Dojo Foundation. Licensed under the BSD license.</p>
+<p align='center'>Dojo 0.4.2 by <a href='http://dojotoolkit.org/'>The Dojo Foundation</a><br />
+Copyright &copy; 2004-2006, The Dojo Foundation. All rights reserved. Licensed under the BSD license.</p>
 
+<p align='center'>MagpieRSS 0.72 by <a href='http://magpierss.sourceforge.net/'>Kellan Elliott-McCrea</a><br />
+Copyright &copy;, Kellan Elliott-McCrea. Licensed under the GPL license.</p>
 
 </td></tr>
 <tr><td class='shade1' colspan="2">Licence:</td></tr>
@@ -105,5 +107,8 @@ if (count($CONFIG['plugins']) >= 1)
 else echo "<p>None</p>";
 echo "</td></tr>";
 echo "</table>\n";
+
+plugin_do('about');
+
 include('htmlfooter.inc.php');
 ?>
