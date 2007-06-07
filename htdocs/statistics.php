04e3ff2802ccb76853e838a19b4065f93e3942ff
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-06-07 10:56:04 +0000

    Added a summary table showing incidents opened/updated/closed/handled etc. over various periods. today, this week, last year etc.

--- htdocs/statistics.php
+++ htdocs/statistics.php
@@ -22,12 +22,90 @@ function give_overview()
 {
     global $todayrecent;
 
+    // Show Open, Closed, Updated today, this week, this month etc.
+    function count_incidents($startdate, $enddate)
+    {
+        // Counts the number of incidents opened between a start date and an end date
+        // Returns an associative array
+        $sql = "SELECT count(*) FROM incidents ";
+        $sql .= "WHERE opened BETWEEN '{$startdate}' AND '{$enddate}' ";
+        $result= mysql_query($sql);
+        list($count['opened'])=mysql_fetch_row($result);
+        mysql_free_result($result);
+
+        $sql = "SELECT count(*) FROM incidents ";
+        $sql .= "WHERE closed BETWEEN '{$startdate}' AND '{$enddate}' ";
+        $result= mysql_query($sql);
+        list($count['closed'])=mysql_fetch_row($result);
+        mysql_free_result($result);
+
+        $sql = "SELECT count(*) FROM incidents ";
+        $sql .= "WHERE lastupdated BETWEEN '{$startdate}' AND '{$enddate}' ";
+        $result= mysql_query($sql);
+        list($count['updated'])=mysql_fetch_row($result);
+        mysql_free_result($result);
+
+        $sql = "SELECT count(*) FROM incidents WHERE opened <= '{$enddate}' AND (closed >= '$startdate' OR closed = 0) ";
+        $result= mysql_query($sql);
+        list($count['handled'])=mysql_fetch_row($result);
+        mysql_free_result($result);
+
+        $sql = "SELECT count(*) FROM updates WHERE timestamp >= '$startdate' AND timestamp <= '$enddate' ";
+        $result= mysql_query($sql);
+        if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
+        list($count['updates'])=mysql_fetch_row($result);
+        mysql_free_result($result);
+
+        return $count;
+    }
+
+    function stats_period_row($desc, $start, $end)
+    {
+        global $shade;
+        if ($shade=='') $shade='shade1';
+        $count = count_incidents($start,$end);
+        $html = "<tr class='$shade'><td>$desc</td><td>{$count['opened']}</td><td>{$count['updated']}</td><td>{$count['closed']}</td><td>{$count['handled']}</td><td>{$count['updates']}</td><td>".number_format($count['updates']/$count['updated'], 2)."</td></tr>\n";
+        if ($shade=='shade1') $shade='shade2';
+        else $shade='shade1';
+        return $html;
+    }
+
+    echo "<table align='center'>";
+    echo "<tr><th>Period</th><th>Opened</th><th>Updated</th><th>Closed</th><th>Handled</th><th>Updates</th><th>per incident</th></tr>\n";
+    echo stats_period_row('Today', mktime(0,0,0,date('m'),date('d'),date('Y')),mktime(23,59,59,date('m'),date('d'),date('Y')));
+    echo stats_period_row('Yesterday', mktime(0,0,0,date('m'),date('d')-1,date('Y')),mktime(23,59,59,date('m'),date('d')-1,date('Y')));
+    echo stats_period_row(date('l',mktime(0,0,0,date('m'),date('d')-2,date('Y'))), mktime(0,0,0,date('m'),date('d')-2,date('Y')),mktime(23,59,59,date('m'),date('d')-2,date('Y')));
+    echo stats_period_row(date('l',mktime(0,0,0,date('m'),date('d')-3,date('Y'))), mktime(0,0,0,date('m'),date('d')-3,date('Y')),mktime(23,59,59,date('m'),date('d')-3,date('Y')));
+    echo stats_period_row(date('l',mktime(0,0,0,date('m'),date('d')-4,date('Y'))), mktime(0,0,0,date('m'),date('d')-4,date('Y')),mktime(23,59,59,date('m'),date('d')-4,date('Y')));
+    echo stats_period_row(date('l',mktime(0,0,0,date('m'),date('d')-5,date('Y'))), mktime(0,0,0,date('m'),date('d')-5,date('Y')),mktime(23,59,59,date('m'),date('d')-5,date('Y')));
+    echo stats_period_row(date('l',mktime(0,0,0,date('m'),date('d')-6,date('Y'))), mktime(0,0,0,date('m'),date('d')-6,date('Y')),mktime(23,59,59,date('m'),date('d')-6,date('Y')));
+    echo stats_period_row('Past 7 days', mktime(0,0,0,date('m'),date('d')-6,date('Y')),mktime(23,59,59,date('m'),date('d'),date('Y')));
+    echo stats_period_row('Previous 7 days', mktime(0,0,0,date('m'),date('d')-13,date('Y')),mktime(23,59,59,date('m'),date('d')-7,date('Y')));
+
+    echo stats_period_row('This month', mktime(0,0,0,date('m'),1,date('Y')),mktime(23,59,59,date('m'),date('d'),date('Y')));
+    echo stats_period_row(date('F y',mktime(0,0,0,date('m')-1,1,date('Y'))), mktime(0,0,0,date('m')-1,date('d'),date('Y')),mktime(23,59,59,date('m'),0,date('Y')));
+    echo stats_period_row(date('F y',mktime(0,0,0,date('m')-2,1,date('Y'))), mktime(0,0,0,date('m')-2,date('d'),date('Y')),mktime(23,59,59,date('m')-1,0,date('Y')));
+    echo stats_period_row(date('F y',mktime(0,0,0,date('m')-3,1,date('Y'))), mktime(0,0,0,date('m')-3,date('d'),date('Y')),mktime(23,59,59,date('m')-2,0,date('Y')));
+    echo stats_period_row(date('F y',mktime(0,0,0,date('m')-4,1,date('Y'))), mktime(0,0,0,date('m')-4,date('d'),date('Y')),mktime(23,59,59,date('m')-3,0,date('Y')));
+    echo stats_period_row(date('F y',mktime(0,0,0,date('m')-5,1,date('Y'))), mktime(0,0,0,date('m')-5,date('d'),date('Y')),mktime(23,59,59,date('m')-4,0,date('Y')));
+    echo stats_period_row(date('F y',mktime(0,0,0,date('m')-6,1,date('Y'))), mktime(0,0,0,date('m')-6,date('d'),date('Y')),mktime(23,59,59,date('m')-5,0,date('Y')));
+
+    echo stats_period_row('This year', mktime(0,0,0,1,1,date('Y')),mktime(23,59,59,date('m'),date('d'),date('Y')));
+    echo stats_period_row('Last year', mktime(0,0,0,1,1,date('Y')-1),mktime(23,59,59,12,31,date('Y')-1));
+    echo stats_period_row(date('Y',mktime(0,0,0,1,1,date('Y')-2)), mktime(0,0,0,1,1,date('Y')-2),mktime(23,59,59,12,31,date('Y')-2));
+    echo stats_period_row(date('Y',mktime(0,0,0,1,1,date('Y')-3)), mktime(0,0,0,1,1,date('Y')-3),mktime(23,59,59,12,31,date('Y')-3));
+    echo stats_period_row(date('Y',mktime(0,0,0,1,1,date('Y')-4)), mktime(0,0,0,1,1,date('Y')-4),mktime(23,59,59,12,31,date('Y')-4));
+    echo stats_period_row(date('Y',mktime(0,0,0,1,1,date('Y')-5)), mktime(0,0,0,1,1,date('Y')-5),mktime(23,59,59,12,31,date('Y')-5));
+    echo "</table>\n";
+
+    echo "<br />\n";
+
     $sql = "SELECT COUNT(incidents.id), incidentstatus.name FROM incidents, incidentstatus ";
     $sql .= "WHERE incidents.status = incidentstatus.id AND status != 2 AND status != 7 GROUP BY incidents.status";
-
     $result = mysql_query($sql);
     if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
 
+    echo "<h2>Current Incidents</h2>";
     echo "<table class='vertical' align='center'>";
     if(mysql_num_rows($result) > 0)
     {
@@ -55,7 +133,7 @@ function give_overview()
 
     if(mysql_num_rows($result) > 1)
     {
-        echo "<p><h2>By vendor</h2><table class='vertical' align='center'>";
+        echo "<h2>By vendor</h2><table class='vertical' align='center'>";
         while($vendors = mysql_fetch_array($result))
         {
             $sqlVendor = "SELECT COUNT(incidents.id), incidentstatus.name FROM incidents, incidentstatus, products ";
@@ -79,7 +157,7 @@ function give_overview()
                 echo "<tr><th>Total Open</th><td class='shade2' align='left'><strong>$openCallsVendor</strong></td></tr></table></td>";
             }
         }
-        echo "</table></p>";
+        echo "</table>";
     }
 
 
