8ddc2da8d0d2785f709c850610727095a81df38a
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-06-20 16:12:47 +0000

    Improvements to product and software editing and display

--- htdocs/products.php
+++ htdocs/products.php
@@ -21,10 +21,11 @@ require('auth.inc.php');
 
 // External Variables
 $productid = cleanvar($_REQUEST['productid']);
+$display = cleanvar($_REQUEST['display']);
 
 include('htmlheader.inc.php');
 
-if (empty($productid))
+if (empty($productid) AND $display!='skills')
 {
     $sql = "SELECT * FROM vendors ORDER BY name";
     $result = mysql_query($sql);
@@ -57,7 +58,8 @@ if (empty($productid))
                     if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
                     list($countcontracts)=mysql_fetch_row($sresult);
 
-                    if ($countlinked < 1) $shade='expired';
+                    if ($countlinked < 1) $shade='urgent';
+                    if ($countcontracts < 1) $shade='expired';
                     echo "<tr class='{$shade}'><td><a href='{$_SERVER['PHP_SELF']}?productid={$product->id}' name='{$product->id}'>{$product->name}</a></td>";
                     echo "<td>{$product->description}</td>";
                     echo "<td align='right'>{$countlinked}</td>";
@@ -78,13 +80,13 @@ if (empty($productid))
     }
     else echo "<p class='error'>No software vendors defined</p>";
 
-    echo "<h2>Software not linked</h2>";
-    echo "<p align='center'>This software is not linked to any product</p>";
+    echo "<h2>Skills not linked</h2>";
+    echo "<p align='center'>These skills are not linked to any product</p>";
     $sql = "SELECT software.* FROM software LEFT JOIN softwareproducts ON software.id=softwareproducts.softwareid WHERE softwareproducts.softwareid IS NULL";
     $result = mysql_query($sql);
     if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
     echo "<table summary='' align='center' width='55%'>";
-    echo "<tr><th>Software</th><th>Lifetime</th><th>Engineers</th><th>Incidents</th><th>Actions</th></tr>";
+    echo "<tr><th>Skill</th><th>Lifetime</th><th>Engineers</th><th>Incidents</th><th>Actions</th></tr>";
     while ($software = mysql_fetch_array($result))
     {
         $ssql = "SELECT COUNT(userid) FROM usersoftware, users WHERE usersoftware.userid = users.id AND users.status!=0 AND usersoftware.softwareid='{$software['id']}'";
@@ -115,6 +117,64 @@ if (empty($productid))
         else $shade='shade1';
     }
     echo "</table>";
+}
+elseif (empty($productid) AND ($display=='skills' OR $display=='software'))
+{
+    echo "<h2>Skills</h2>";
+    $sql = "SELECT * FROM software ORDER BY name";
+    $result = mysql_query($sql);
+    if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
+    if (mysql_num_rows($result) >= 1)
+    {
+        echo "<table align='center'>";
+        echo "<tr><th>Skill</th><th>Lifetime</th><th>Linked to # Products</th><th>Engineers</th><th>Incidents</th><th>Actions</th></tr>";
+        $shade='shade1';
+        while ($software = mysql_fetch_object($result))
+        {
+
+            $ssql = "SELECT COUNT(userid) FROM usersoftware, users WHERE usersoftware.userid = users.id AND users.status!=0 AND usersoftware.softwareid='{$software->id}'";
+            $sresult = mysql_query($ssql);
+            if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
+            list($countengineers) = mysql_fetch_row($sresult);
+
+            // Count linked products
+            $ssql = "SELECT COUNT(productid) FROM softwareproducts WHERE softwareid={$software->id}";
+            $sresult = mysql_query($ssql);
+            if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
+            list($countlinked)=mysql_fetch_row($sresult);
+
+            $ssql = "SELECT COUNT(id) FROM incidents WHERE softwareid='{$software->id}'";
+            $sresult = mysql_query($ssql);
+            if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
+            list($countincidents) = mysql_fetch_row($sresult);
+
+            $lifetime_start=mysql2date($software->lifetime_start);
+            $lifetime_end=mysql2date($software->lifetime_end);
+
+            if ($lifetime_start > $now OR ($lifetime_end > 1 AND $lifetime_end < $now)) $shade='expired';
+            echo "<tr class='$shade'>";
+            echo "<td>{$software->name}</td>";
+            echo "<td>";
+            if ($software->lifetime_start > 1) echo date($CONFIG['dateformat_shortdate'],$lifetime_start).' to ';
+            else echo "&#8734;";
+            if ($software->lifetime_end > 1) echo date($CONFIG['dateformat_shortdate'],$lifetime_end);
+            elseif ($software->lifetime_start >1) echo "&#8734;";
+            echo "</td>";
+            echo "<td>{$countlinked}</td>";
+            echo "<td>{$countengineers}</td>";
+            echo "<td>{$countincidents}</td>";
+            echo "<td><a href='add_product_software.php?softwareid={$software->id}'>Link</a> ";
+            echo "| <a href='edit_software.php?id={$software->id}'>Edit</a> ";
+            echo "| <a href='edit_software.php?id={$software->id}&amp;action=delete'>Delete</a>";
+            echo "</td>";
+            echo "</tr>\n";
+            if ($shade=='shade1') $shade='shade2';
+            else $shade='shade1';
+        }
+        echo "</table>";
+    }
+    else echo "<p class='warning'>No records to display</p>";
+
 }
 else
 {
@@ -184,7 +244,7 @@ else
                 $shade = 'shade1';
                 while ($contract = mysql_fetch_object($result))
                 {
-                    if ($results['term']=='yes' || $results['expirydate']<$now) $shade = "expired";
+                    if ($contract->term=='yes' OR $contract->expirydate < $now) $shade = "expired";
                     echo "<tr class='{$shade}'>";
                     echo "<td><a href='maintenance_details.php?id={$contract->id}'>Contract {$contract->id}</a></td>";
                     echo "</tr>\n";
@@ -192,6 +252,26 @@ else
                     else $shade='shade1';
                 }
                 echo "</table>\n";
+            }
+
+            $sql = "SELECT * FROM incidents WHERE product={$product->id} ORDER BY id DESC";
+            $result = mysql_query($sql);
+            if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
+            if (mysql_num_rows($result) >= 1)
+            {
+                echo "<h3>Related Incidents</h3>";
+                echo "<table align='center'>";
+                $shade = 'shade1';
+                while ($incident = mysql_fetch_object($result))
+                {
+                    echo "<tr class='{$shade}'>";
+                    echo "<td><a href=\"javascript:incident_details_window('{$incident->id}','incident{$incident->id}');\">Incident {$incident->id}</a></td>";
+                    echo "</tr>\n";
+                    if ($shade=='shade1') $shade='shade2';
+                    else $shade='shade1';
+                }
+                echo "</table>\n";
+
             }
         }
 
@@ -201,7 +281,7 @@ else
     echo "<p align='center'><a href='{$_SERVER['PHP_SELF']}#{$productid}'>Back to list of products</a></p>";
 }
 
-echo "<p align='center'><a href='add_vendor.php'>Add Vendor</a> | <a href='add_product.php'>Add Product</a> | <a href='add_software.php'>Add Software</a></p>";
+echo "<p align='center'><a href='add_vendor.php'>Add Vendor</a> | <a href='add_product.php'>Add Product</a> | <a href='add_software.php'>Add Skill</a></p>";
 
 include('htmlfooter.inc.php');
 ?>
