4be73458075318fd8f9a227cf5ae4e8781fb634c
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-08-24 16:53:12 +0000

    Completed the forgotten password procedure.  It's now possible to get a reminder of contact passwords and to reset user passwords.

--- htdocs/forgotpwd.php
+++ htdocs/forgotpwd.php
@@ -27,10 +27,11 @@ switch($_REQUEST['action'])
     case 'forgotpwd':
         include('htmlheader.inc.php');
         // First look to see if this is a SiT user
-        $sql = "SELECT username, password FROM users WHERE email = '{$email}' LIMIT 1";
+        $sql = "SELECT id, username, password FROM users WHERE email = '{$email}' LIMIT 1";
         $userresult = mysql_query($sql);
         if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
         $usercount = mysql_num_rows($userresult);
+        $userdetails = mysql_fetch_object($userresult);
         if($usercount == 1)
         {
             $extra_headers = "Reply-To: {$CONFIG['support_email']}\n";
@@ -39,8 +40,8 @@ switch($_REQUEST['action'])
             $extra_headers .= "\n"; // add an extra crlf to create a null line to separate headers from body
             $bodytext = "To reset your password please visit:\n";
             $url = parse_url($_SERVER['HTTP_REFERER']);
-            $hash = md5($row->username.'.'.$row->password);
-            $reseturl = "{$url['scheme']}://{$url['host']}{$url['path']}?action=confirmreset&amp;userid={$row->id}&amp;hash={$hash}";
+            $hash = md5($userdetails->username.'.'.$userdetails->password);
+            $reseturl = "{$url['scheme']}://{$url['host']}{$url['path']}?action=confirmreset&amp;userid={$userdetails->id}&amp;hash={$hash}";
             $bodytext .= "{$reseturl}";
             mail($email, "Information for resetting your password", stripslashes($bodytext), $extra_headers);
             echo "<h3>Information sent</h3>";
@@ -49,6 +50,7 @@ switch($_REQUEST['action'])
         }
         else
         {
+            // This is a SiT contact, not a user
             $sql = "SELECT username,password FROM contacts WHERE email = '{$email}' LIMIT 1";
             $contactresult = mysql_query($sql);
             if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
@@ -62,7 +64,6 @@ switch($_REQUEST['action'])
                     $extra_headers .= "X-Mailer: {$CONFIG['application_shortname']} {$application_version_string}/PHP " . phpversion() . "\n";
                     $extra_headers .= "X-Originating-IP: {$_SERVER['REMOTE_ADDR']}\n";
                     $extra_headers .= "\n"; // add an extra crlf to create a null line to separate headers from body
-
 
                     $bodytext = "Username: {$row->username}\nPassword: {$row->password}";
                     mail($email, "Forgotten password details", stripslashes($bodytext), $extra_headers);
@@ -81,31 +82,130 @@ switch($_REQUEST['action'])
 
     case 'confirmreset':
         include('htmlheader.inc.php');
-        echo "<h2>Reset user password</h2>";
-        echo "<p align='center'>Please confirm your username</p>";
-        echo "<form action='{$_SERVER['PHP_SELF']}' method='post'>";
-        ?>
-        <table class='vertical'>
-        <tr><th>Username</th><td><input name="username" size="30" type="text" /></td></tr>
-        </table>
-        <p><input type="submit" value='Continue' /></p>
-        <?php
-        echo "<input type='hidden' name='userid' value='{$userid}' />";
-        echo "<input type='hidden' name='hash' value='{$userhash}' />";
-        echo "<input type='hidden' name='action' value='resetuserpassword' />";
-        echo "</form>";
+        $sql = "SELECT id, username, password FROM users WHERE id = '{$userid}' LIMIT 1";
+        $userresult = mysql_query($sql);
+        if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
+        $usercount = mysql_num_rows($userresult);
+        if ($usercount > 0)
+        {
+            $userdetails = mysql_fetch_object($userresult);
+            $hash = md5($userdetails->username.'.'.$userdetails->password);
+            if ($hash == $userhash)
+            {
+                echo "<h2>Reset user password</h2>";
+                echo "<p align='center'>Please confirm your username</p>";
+                echo "<form action='{$_SERVER['PHP_SELF']}' method='post'>";
+                ?>
+                <table class='vertical'>
+                <tr><th>Username</th><td><input name="username" size="30" type="text" /></td></tr>
+                </table>
+                <p><input type="submit" value='Continue' /></p>
+                <?php
+                echo "<input type='hidden' name='userid' value='{$userid}' />";
+                echo "<input type='hidden' name='hash' value='{$userhash}' />";
+                echo "<input type='hidden' name='action' value='resetpasswordform' />";
+                echo "</form>";
+            }
+            else
+            {
+                echo "<h3>Error</h3>";
+                echo "<p>Did you paste the full URL you received in the email?</p>";
+                echo "<p><a href='index.php'>Back to login page</a></p>";
+            }
+        }
+        else
+        {
+            echo "<h3>Error</h3>";
+            echo "<p>Did you paste the full URL you received in the email?</p>";
+            echo "<p><a href='index.php'>Back to login page</a></p>";
+        }
         include('htmlfooter.inc.php');
     break;
 
-    case 'resetuserpassword':
+    case 'resetpasswordform':
         include('htmlheader.inc.php');
-        echo "<h2>Password Reset</h2>";
-        // TODO password reset needs completing, and some parts need disabling if customer portal is not enabled INL 19Nov06
-//         echo "<h3>Sorry</h3>";
-//         echo "<p>Password reset feature not yet available.</p>";
-        echo "<tr class='password'><th>New Password:</th><td><input maxlength='50' name='newpassword1' size='30' type='password' /></td></tr>";
-        echo "<tr class='password'><th>Confirm New Password:</th><td><input maxlength='50' name='newpassword2' size='30' type='password' /></td></tr>";
-        echo "<p><a href='index.php'>Back to login page</a></p>";
+        $sql = "SELECT id, username, password FROM users WHERE id = '{$userid}' LIMIT 1";
+        $userresult = mysql_query($sql);
+        if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
+        $usercount = mysql_num_rows($userresult);
+        if ($usercount > 0)
+        {
+            $userdetails = mysql_fetch_object($userresult);
+            $hash = md5($userdetails->username.'.'.$userdetails->password);
+            if ($hash == $userhash AND $username==$userdetails->username)
+            {
+                $newhash = md5($userdetails->username.'.ok.'.$userdetails->password);
+                echo "<h2>Set your new password</h2>";
+                echo "<form action='{$_SERVER['PHP_SELF']}' method='post'>";
+                echo "<table align='center' class='vertical'>";
+                echo "<tr class='password'><th>New Password:</th><td><input maxlength='50' name='newpassword1' size='30' type='password' /></td></tr>";
+                echo "<tr class='password'><th>Confirm New Password:</th><td><input maxlength='50' name='newpassword2' size='30' type='password' /></td></tr>";
+                echo "</table>";
+                echo "<input type='hidden' name='userid' value='{$userid}' />";
+                echo "<input type='hidden' name='hash' value='{$newhash}' />";
+                echo "<input type='hidden' name='action' value='savepassword' />";
+                echo "<p><input type='submit' value='Set Password' />";
+                echo "</form>";
+                echo "<p><a href='index.php'>Back to login page</a></p>";
+            }
+            else
+            {
+                echo "<h3>Error</h3>";
+                echo "<p>Have you forgotten your username?  If so you should contact an administrator.</p>";
+                echo "<p><a href='index.php'>Back to login page</a></p>";
+            }
+        }
+        else
+        {
+            echo "<h3>Error</h3>";
+            echo "<p>Invalid user ID</p>";
+            echo "<p><a href='index.php'>Back to login page</a></p>";
+        }
+        include('htmlfooter.inc.php');
+    break;
+
+    case 'savepassword':
+        $newpassword1 = cleanvar($_REQUEST['newpassword1']);
+        $newpassword2 = cleanvar($_REQUEST['newpassword2']);
+        include('htmlheader.inc.php');
+        $sql = "SELECT id, username, password FROM users WHERE id = '{$userid}' LIMIT 1";
+        $userresult = mysql_query($sql);
+        if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
+        $usercount = mysql_num_rows($userresult);
+        if ($usercount > 0)
+        {
+            $userdetails = mysql_fetch_object($userresult);
+            $newhash = md5($userdetails->username.'.ok.'.$userdetails->password);
+            if ($newhash == $userhash)
+            {
+                if ($newpassword1==$newpassword2)
+                {
+                    $usql = "UPDATE users SET password=MD5({$newpassword1}) WHERE id={$userid} LIMIT 1";
+                    mysql_query($usql);
+                    echo "<h3>Password reset</h3>";
+                    echo "<p>Your password has been reset, you can now login using the new details.</p>";
+                    echo "<p><a href='index.php'>Back to login page</a></p>";
+                }
+                else
+                {
+                    echo "<h3>Error</h3>";
+                    echo "<p>The new password you entered was not confirmed correctly.</p>";
+                    echo "<p><a href='index.php'>Back to login page</a></p>";
+                }
+            }
+            else
+            {
+                echo "<h3>Error</h3>";
+                echo "<p>Invalid details</p>";
+                echo "<p><a href='index.php'>Back to login page</a></p>";
+            }
+        }
+        else
+        {
+            echo "<h3>Error</h3>";
+            echo "<p>Invalid user ID</p>";
+            echo "<p><a href='index.php'>Back to login page</a></p>";
+        }
         include('htmlfooter.inc.php');
     break;
 
