e68a3db7a0a8a1c3e8689c2aff622c02abd9f49d
Paul Heaney <paulheaney@users.sourceforge.net>
2007-09-02 19:07:21 +0000

    * Opps need to make now global (else expired contacts can log calls)

--- htdocs/add_incident.php
+++ htdocs/add_incident.php
@@ -127,6 +127,7 @@ elseif ($action=='findcontact')
         <?php
         function to_row($contactrow)
         {
+            global $now;
             $str = "";
             if ($contactrow['expirydate']<$now || $contactrow['term']=='yes') $class = 'expired';
             else $class = "shade2";
