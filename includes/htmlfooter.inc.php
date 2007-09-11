4182b861cadacf76690755d808cc5b4ef278ec3d
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-09-11 09:24:54 +0000

    Don't put a hyperlink to the about page on the SiT icon in the status bar when the user isn't logged in

--- includes/htmlfooter.inc.php
+++ includes/htmlfooter.inc.php
@@ -7,13 +7,15 @@ if ($CONFIG['debug'] == TRUE)
   $exec_time = $exec_time_end - $exec_time_start;
   echo "<p>CPU Time: ".number_format($exec_time,3)." seconds</p>";
 }
-echo "<div id='statusbar'><a href='about.php'><img src='{$CONFIG['application_webpath']}images/sitting_man_logo16x16.png' width='16' height='16' border='0' alt='About {$CONFIG['application_shortname']}' /></a> ";
-echo "<strong><a href='http://sitracker.sourceforge.net/'>Support Incident Tracker</a> {$application_version_string} </strong> running ";
+echo "<div id='statusbar'>";
+if ($_SESSION['auth']==TRUE) echo "<a href='about.php'>";
+echo "<img src='{$CONFIG['application_webpath']}images/sitting_man_logo16x16.png' width='16' height='16' border='0' alt='About {$CONFIG['application_shortname']}' />";
+if ($_SESSION['auth']==TRUE) echo "</a>";
+echo " <strong><a href='http://sitracker.sourceforge.net/'>Support Incident Tracker</a> {$application_version_string} </strong> running ";
 if ($CONFIG['demo']) echo "in DEMO mode ";
 echo "on ".strip_tags($_SERVER["SERVER_SOFTWARE"]);
 echo " at ".date('H:i',$now);
 if ($CONFIG['bugtracker_url']!='') echo ", Report bugs in <a href='{$CONFIG['bugtracker_url']}' class='barlink'>{$CONFIG['bugtracker_name']}</a>";
 echo "</div>\n";
+echo "</body>\n</html>\n";
 ?>
-</body>
-</html>
