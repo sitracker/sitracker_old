b7430eb6be30f4f802738d83e7d1971a85631654
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2007-09-26 15:04:30 +0000

    Changed Lock to View and made it open the email

--- htdocs/review_incoming_updates.php
+++ htdocs/review_incoming_updates.php
@@ -80,7 +80,7 @@ function generate_row($update)
         {
             $html_row.="<a href='{$_SERVER['PHP_SELF']}?unlock={$update['tempid']}' title='Unlock this update so it can be modified by someone else'> Unlock</a> | ";
         }
-        else $html_row.= "<a href='{$_SERVER['PHP_SELF']}?lock={$update['tempid']}' title='Lock this update so it cannot be modified by anyone else'> Lock</a> | ";
+        else $html_row.= "<a href=\"javascript:incident_details_window('{$update['tempid']}','incomingview');\" id='update{$update['id']}' class='info' title='View and lock this held e-mail'> View</a> | ";
         $html_row.= "<a href='delete_update.php?updateid=".$update['id']."&amp;tempid=".$update['tempid']."&amp;timestamp=".$update['timestamp']."' title='Remove this item permanently' onclick='return confirm_delete();'> Delete</a>";
     }
     $html_row.="</td></tr>\n";
