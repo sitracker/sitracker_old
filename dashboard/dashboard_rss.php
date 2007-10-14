689414c237935ea27055f9fb6eaac9e7ef24b54e
Paul Heaney <paulheaney@users.sourceforge.net>
2007-10-14 15:56:33 +0000

    * Move RSS feed get and display into a separate thread * Created javascript function to get and display a webpage in a html component

--- dashboard/dashboard_rss.php
+++ dashboard/dashboard_rss.php
@@ -13,74 +13,16 @@
 function dashboard_rss($row,$dashboardid)
 {
     global $sit, $CONFIG, $iconset;
-    require_once('magpierss/rss_fetch.inc');
-
-    $sql = "SELECT url FROM dashboard_rss WHERE owner = {$sit[2]} AND enabled = 'true'";
-    $result = mysql_query($sql);
-    if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
-
-    define('MAGPIE_CACHE_ON',TRUE);
-    define('MAGPIE_CACHE_DIR', $CONFIG['attachment_fspath'].'feeds');
-    define('MAGPIE_OUTPUT_ENCODING', 'UTF-8');
-
-    $feedallowedtags = '<img><strong><em><br><p>';
 
     echo "<div class='windowbox' style='width: 95%' id='$row-$dashboardid'>";
     echo "<div class='windowtitle'><div style='float: right'><a href='edit_rss_feeds.php'>edit</a></div><img src='{$CONFIG['application_webpath']}images/icons/{$iconset}/16x16/feed-icon.png' width='16' height='16' alt='' /> Feeds</div>";
-    echo "<div class='window'>";
+    echo "<div class='window' id='rss_window'>";
 
-    if(mysql_num_rows($result) > 0)
-    {
-        while($row = mysql_fetch_row($result))
-        {
-            $url = $row[0];
-            if($rss = fetch_rss( $url ))
-            {
-//                  echo "<pre>".print_r($rss,true)."</pre>";
-                echo "<table align='center' style='width: 100%'>";
-                echo "<tr><th><span style='float: right;'><a href='".htmlspecialchars($url)."'>";
-                echo "<img src='{$CONFIG['application_webpath']}images/icons/{$iconset}/12x12/feed-icon.png' style='border: 0px;' alt='Feed Icon' />";
-                echo "</a></span>";
-                echo "<a href='{$rss->channel['link']}' style='color: #000;' class='info'>{$rss->channel['title']}";
-                if (!empty($rss->image['url']) OR !empty($rss->channel['description']))
-                {
-                    echo "<span>";
-                    if (!empty($rss->image['url'])) echo "<img src='{$rss->image['url']}' alt='{$rss->image['title']}' style='float: right; margin-right: 2px; margin-left: 5px; margin-top: 2px;' />";
-                    echo "{$rss->channel['description']}</span>";
-                }
-                echo "</a>";
-                echo "</th></tr>";
-                foreach($rss->items as $item)
-                {
-//                      echo "<pre>".print_r($item,true)."</pre>";
-                    echo "<tr><td><a href='{$item['link']}' class='info'>{$item['title']}";
-                    if($rss->feed_type == 'RSS')
-                    {
-                        if (!empty($item['pubdate'])) $itemdate = strtotime($item['pubdate']);
-                        elseif (!empty($item['dc']['date'])) $itemdate = strtotime($item['dc']['date']);
-                        else $itemdate = '';
-                        $d = strip_tags($item['description'],$feedallowedtags);
-                    }
-                    elseif($rss->feed_type == 'Atom')
-                    {
-                        if (!empty($item['issued'])) $itemdate = strtotime($item['issued']);
-                        $d = strip_tags($item['atom_content'],$feedallowedtags);
-                    }
-                    if ($itemdate > 10000) $itemdate = date($CONFIG['dateformat_datetime'], $itemdate);
-                    echo "<span>";
-                    if (!empty($itemdate)) echo "<strong>{$itemdate}</strong><br />";
-                    echo "{$d}</span></a></td></tr>";
-                }
-                echo "</table>";
-            }
-            else
-            {
-                echo "Error: It's not possible to get $url...";
-            }
-        }
-    }
+    echo "<p align='center'><img src='{$CONFIG['application_webpath']}images/icons/{$iconset}/ajax-loader.gif' alt='Loading icon' /></p>";
+
     echo "</div>";
     echo "</div>";
+    echo "<script type='text/javascript'>get_and_display('display_rss_feed.inc.php','rss_window');</script>";
 }
 
 function dashboard_rss_install()
