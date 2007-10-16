185064a17e160dd289a57a165f4c55fbfa88e1e3
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2007-10-16 15:38:15 +0000

    initial, basic translate page, changed ' to " for the inc files.

--- includes/i18n/en-gb.inc.php
+++ includes/i18n/en-gb.inc.php
@@ -1,116 +1,116 @@
 <?php
 
 $languagestring="English";
-$i18ncharset='UTF-8';
+$i18ncharset="UTF-8";
 
 // list of strings (Alphabetical)
-$strActionNeeded = 'Action Needed';
-$strActionPlanBy = 'Action Plan by';
-$strAdd = 'Add';
-$strAddIncident = 'Add Incident';
-$strAddTask = 'Add Task';
-$strAddTemplate = 'Add Template';
-$strAddUser = 'Add User';
-$strApproveHolidays = 'Approve Holidays';
+$strActionNeeded = "Action Needed";
+$strActionPlanBy = "Action Plan by";
+$strAdd = "Add";
+$strAddIncident = "Add Incident";
+$strAddTask = "Add Task";
+$strAddTemplate = "Add Template";
+$strAddUser = "Add User";
+$strApproveHolidays = "Approve Holidays";
 
-$strBrowse = 'Browse';
-$strBrowseContacts = 'Browse contacts';
-$strBrowseFeedback = 'Browse feedback';
-$strBulkModify = 'Bulk Modify';
+$strBrowse = "Browse";
+$strBrowseContacts = "Browse contacts";
+$strBrowseFeedback = "Browse feedback";
+$strBulkModify = "Bulk Modify";
 
-$strClosed = 'closed';
-$strContact = 'Contact';
-$strContacts = 'Contacts';
-$strControlPanel = 'Control Panel';
-$strCustomers = 'Customers';
-$strCurrentlyOpen = 'currently open';
+$strClosed = "closed";
+$strContact = "Contact";
+$strContacts = "Contacts";
+$strControlPanel = "Control Panel";
+$strCustomers = "Customers";
+$strCurrentlyOpen = "currently open";
 
-$strDashboard = 'Dashboard';
+$strDashboard = "Dashboard";
 
-$strEdit = 'edit';
-$strEditContract = 'Edit Contract';
-$strEditedBy = 'Edited by';
-$strEditHolidyEntitlement = 'Edit Holiday Entitlement';
-$strEditTemplate = 'Edit Template';
-$strEmailSettings = 'Email Settings';
-$strEscalationPaths = 'Escalation Paths';
+$strEdit = "edit";
+$strEditContract = "Edit Contract";
+$strEditedBy = "Edited by";
+$strEditHolidyEntitlement = "Edit Holiday Entitlement";
+$strEditTemplate = "Edit Template";
+$strEmailSettings = "Email Settings";
+$strEscalationPaths = "Escalation Paths";
 
-$strFeedbackForms = 'Feedback Forms';
-$strFeeds = 'Feeds';
-$strFindContact = 'Find Contact';
-$strForgottenDetails = 'Forgotten your details?';
-$strFTPFilesDB = 'FTP Files DB';
+$strFeedbackForms = "Feedback Forms";
+$strFeeds = "Feeds";
+$strFindContact = "Find Contact";
+$strForgottenDetails = "Forgotten your details?";
+$strFTPFilesDB = "FTP Files DB";
 
-$strGlobalSignature = 'Global Signature';
+$strGlobalSignature = "Global Signature";
 
-$strHelp = 'Help';
-$strHelpAbout = 'About';
-$strHelpContents = 'Help Contents';
-$strHoldingQueue = 'Holding Queue';
-$strHolidayPlanner = 'Holiday Planner';
+$strHelp = "Help";
+$strHelpAbout = "About";
+$strHelpContents = "Help Contents";
+$strHoldingQueue = "Holding Queue";
+$strHolidayPlanner = "Holiday Planner";
 
-$strKnowledgeBase = 'Knowledge Base';
+$strKnowledgeBase = "Knowledge Base";
 
-$strListSkills = 'List Skills';
-$strLogged = 'logged';
-$strLogIn = 'Log In';
-$strLogin = 'Login';
-$strLogout = 'Logout';
+$strListSkills = "List Skills";
+$strLogged = "logged";
+$strLogIn = "Log In";
+$strLogin = "Login";
+$strLogout = "Logout";
 
-$strMaintenance = 'Maintenance';
-$strManageDashboardComponents = 'Manage Dashboard Components';
-$strManageUsers = 'Manage Users';
-$strMyDashboard = 'My Dashboard';
-$strMyDetails = 'My Details';
-$strMyHolidays = 'My Holidays';
-$strMyProfile = 'My Profile';
-$strMySkills = 'My Skills';
-$strMySubstitutes = 'My Substitutes';
+$strMaintenance = "Maintenance";
+$strManageDashboardComponents = "Manage Dashboard Components";
+$strManageUsers = "Manage Users";
+$strMyDashboard = "My Dashboard";
+$strMyDetails = "My Details";
+$strMyHolidays = "My Holidays";
+$strMyProfile = "My Profile";
+$strMySkills = "My Skills";
+$strMySubstitutes = "My Substitutes";
 
-$strNewContact = 'New Contact';
-$strNewContract = 'New Contract';
-$strNewEscalationPath = 'New Escalation Path';
-$strNewKBArticle = 'New Article';
-$strNewReseller = 'New Reseller';
-$strNewSite = 'New Site';
-$strNewTemplate = 'New Email Template';
-$strNoKBArticles = 'No KB Articles';
-$strNoIncidents = 'No Incidents';
+$strNewContact = "New Contact";
+$strNewContract = "New Contract";
+$strNewEscalationPath = "New Escalation Path";
+$strNewKBArticle = "New Article";
+$strNewReseller = "New Reseller";
+$strNewSite = "New Site";
+$strNewTemplate = "New Email Template";
+$strNoKBArticles = "No KB Articles";
+$strNoIncidents = "No Incidents";
 
-$strPassword = 'Password';
+$strPassword = "Password";
 $strPublished = "published";
 
-$strQueue = 'Queue';
+$strQueue = "Queue";
 
-$strRandomTip = 'Random Tip';
-$strReports = 'Reports';
+$strRandomTip = "Random Tip";
+$strReports = "Reports";
 
-$strSave = 'Save';
-$strSearch = 'Search';
-$strSearchAgain = 'Search Again';
-$strServiceLevel = 'Service Level';
-$strServiceLevels = 'Service Levels';
-$strSetPermissions = 'Set Permissions';
-$strSetPublicHolidays = 'Set Public Holidays';
-$strShowExpired = 'Show Expired';
-$strShowRenewals = 'Show Renewals';
-$strSkillsMatrix = 'Skills Matrix';
-$strSites = 'Sites';
-$strSupport = 'Support';
+$strSave = "Save";
+$strSearch = "Search";
+$strSearchAgain = "Search Again";
+$strServiceLevel = "Service Level";
+$strServiceLevels = "Service Levels";
+$strSetPermissions = "Set Permissions";
+$strSetPublicHolidays = "Set Public Holidays";
+$strShowExpired = "Show Expired";
+$strShowRenewals = "Show Renewals";
+$strSkillsMatrix = "Skills Matrix";
+$strSites = "Sites";
+$strSupport = "Support";
 
-$strTags = 'Tags';
-$strTasks = 'Tasks';
-$strTodaysStats = 'Todays Statistics';
+$strTags = "Tags";
+$strTasks = "Tasks";
+$strTodaysStats = "Todays Statistics";
 
-$strUpdated = 'updated';
-$strUnknown = 'Unknown';
-$strUserGroups = 'User Groups';
-$strUsername = 'Username';
-$strUsers = 'Users';
+$strUpdated = "updated";
+$strUnknown = "Unknown";
+$strUserGroups = "User Groups";
+$strUsername = "Username";
+$strUsers = "Users";
 
-$strViewIncidents = 'View Incidents';
-$strViewTasks = 'View Tasks';
-$strViewUsers = 'View Users';
+$strViewIncidents = "View Incidents";
+$strViewTasks = "View Tasks";
+$strViewUsers = "View Users";
 
-$strWatchIncidents = 'Watch Incidents';
+$strWatchIncidents = "Watch Incidents";
 ?>
