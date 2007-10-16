5a3711fc07516988143aaa30ea21603d29127123
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-10-16 13:05:53 +0000

    More i18n strings for translation

--- includes/i18n/en-gb.inc.php
+++ includes/i18n/en-gb.inc.php
@@ -5,16 +5,30 @@ $i18ncharset='UTF-8';
 
 // list of strings (Alphabetical)
 $strClosed = 'closed';
+$strControlPanel = 'Control Panel';
 $strCustomers = 'Customers';
 $strCurrentlyOpen = 'currently open';
+$strDashboard = 'Dashboard';
+$strHelp = 'Help';
+$strKnowledgeBase = 'Knowledge Base';
 $strLogged = 'logged';
+$strLogout = 'Logout';
 $strNoKBArticles = 'No KB Articles';
 $strNoIncidents = 'No Incidents';
+$strMyDashboard = 'My Dashboard';
+$strMyDetails = 'My Details';
+$strMyHolidays = 'My Holidays';
+$strMyProfile = 'My Profile';
+$strMySkills = 'My Skills';
+$strMySubstitutes = 'My Substitutes';
 $strPublished = "published";
 $strRandomTip = 'Random Tip';
+$strReports = 'Reports';
+$strSearch = 'Search';
 $strSupport = 'Support';
 $strTasks = 'Tasks';
 $strTodaysStats = 'Todays Statistics';
-$strUpdated= 'updated';
+$strUpdated = 'updated';
+$strUsers = 'Users';
 
 ?>
