9b08fdc43465fb48d3ef8586e548e68f22ff861e
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2007-10-17 13:47:28 +0000

    few more translations

--- includes/i18n/en-gb.inc.php
+++ includes/i18n/en-gb.inc.php
@@ -1,26 +1,29 @@
 <?php
 
-$languagestring="English";
-$i18ncharset="UTF-8";
+$languagestring='English';
+$i18ncharset='UTF-8';
 
 // list of strings (Alphabetical)
-$strActionNeeded = "Action Needed";
-$strActionPlanBy = "Action Plan by";
-$strAdd = "Add";
+$strActionNeeded = 'Action Needed';
+$strActionPlanBy = 'Action Plan by';
+$strActions = 'Actions';
+$strAdd = 'Add';
+$strAddIncident = 'Add Incident';
+$strAddTask = 'Add Task';
+$strAddTemplate = 'Add Template';
+$strAddUser = 'Add User';
+$strAgo = '%s ago';
+$strAllClosed = 'All Closed';
+$strAllOpen = 'All Open';
+$strApproveHolidays = 'Approve Holidays';
 $strAddContact = "Add Contact";
 $strAddContract = "Add Contract";
 $strAddIncident = "Add Incident";
 $strAddress = "Address";
 $strAddress1 = "Address1";
 $strAddress2 = "Address2";
-$strAddSite = "Add Site";
 $strAddTask = "Add Task";
-$strAddTemplate = "Add Template";
-$strAddUser = "Add User";
-$strAgo = "%s ago";
-$strAllClosed = "All Closed";
-$strAllOpen = "All Open";
-$strApproveHolidays = "Approve Holidays";
+
 
 $strBrowse = "Browse";
 $strBrowseContacts = "Browse contacts";
@@ -42,8 +45,13 @@ $strCustomers = "Customers";
 $strCounty = "County";
 $strCountry = "Country";
 $strCurrentlyOpen = "currently open";
+$strClosureRequestForIncident = 'Closure request for Incident';
+$strContractID = 'Contract ID';
+$strCurrentlyOpen = 'currently open';
+$strCustomerImpact = 'Customer Impact';
+$strCustomerImpactCustomerText = 'How many users are affected by this issue? Is this an issue of downtime or a minor niggle? Does the issue prevent people from working?';
 
-$strDashboard = "Dashboard";
+$strDashboard = 'Dashboard';
 $strDataProtection = "Data Protection";
 $strDate = "Date";
 $strDelete = 'Delete';
@@ -80,28 +88,33 @@ $strGlobalSignature = "Global Signature";
 $strGo = "Go";
 $strGroupMembership = "Group Membership";
 
-$strHelp = "Help";
-$strHelpAbout = "About";
-$strHelpContents = "Help Contents";
-$strHoldingQueue = "Holding Queue";
-$strHolidayEntitlement = "Holiday Entitlement";
-$strHolidayPlanner = "Holiday Planner";
+$strHelp = 'Help';
+$strHelpAbout = 'About';
+$strHelpContents = 'Help Contents';
+$strHoldingQueue = 'Holding Queue';
+$strHolidayEntitlement = 'Holiday Entitlement';
+$strHolidayPlanner = 'Holiday Planner';
 
+$strID = 'ID';
+$strIncidentsAvailable = 'Incidents Available';
+$strIncidentTitle = 'Incident Title';
+$strIncidentsUsed = 'Incidents Used';
+$strMultiIncidents = '%d Incidents';
+$strSingleIncident = '%d Incident';
 $strInactive = "Inactive";
 $strIncident = "Incident";
 $strIncidents = "Incidents";
 $strInfo = "Info";
 
+$strKnowledgeBase = 'Knowledge Base';
 $strJobTitle = "Job Title";
 
-$strKnowledgeBase = "Knowledge Base";
-
-$strLastUpdated = "Last Updated";
-$strListSkills = "List Skills";
-$strLogged = "logged";
-$strLogIn = "Log In";
-$strLogin = "Login";
-$strLogout = "Logout";
+$strLastUpdated = 'Last Updated';
+$strListSkills = 'List Skills';
+$strLogged = 'logged';
+$strLogIn = 'Log In';
+$strLogin = 'Login';
+$strLogout = 'Logout';
 
 $strMaintenance = "Maintenance";
 $strManageDashboardComponents = "Manage Dashboard Components";
@@ -131,9 +144,13 @@ $strNoIncidents = "No Incidents";
 $strNotes = "Notes";
 $strNow = "Now";
 
-$strOtherIncidents = "Other Incidents";
-$strOwner = "Owner";
+$strOtherIncidents = 'Other Incidents';
+$strOwner = 'Owner';
 
+$strProblemDescription = 'Problem Description';
+$strProblemDescriptionCustomerText = 'As much information as possible about the problem, enough for an engineer to begin researching the issue without having to contact you.';
+$strProblemReproduction = 'Problem Reproduction';
+$strProblemReproductionCustomerText = 'Does the problem exist with more than one machine? What steps will an engineer need to take to reproduce this issue?';
 $strPassword = "Password";
 $strPostcode = "Postcode";
 $strPriority = "Priority";
@@ -141,13 +158,13 @@ $strPrivacy = "Privacy";
 $strProduct = "Product";
 $strPublished = "published";
 
-$strQueue = "Queue";
+$strQueue = 'Queue';
 
-$strRandomTip = "Random Tip";
-$strRealName = "Real Name";
-$strReports = "Reports";
-$strReset = "Reset";
-$strRole = "Role";
+$strRandomTip = 'Random Tip';
+$strRealName = 'Real Name';
+$strReports = 'Reports';
+$strReset = 'Reset';
+$strRole = 'Role';
 
 $strSalesperson = "Salesperson";
 $strSave = "Save";
@@ -165,6 +182,7 @@ $strSites = "Sites";
 $strSiteName = "Site Name";
 $strSkillsMatrix = "Skills Matrix";
 $strSLATarget = "SLA Target";
+$strSoftwareVersion = 'Software Version';
 $strStartDate = "Start Date";
 $strStatus = "Status";
 $strSupport = "Support";
@@ -178,7 +196,7 @@ $strTitle = "Title";
 $strTodaysStats = "Todays Statistics";
 $strToday = "Today";
 
-$strUpdated = "updated";
+$strUpdated = 'updated';
 $strUpdatedAutomaticallyBy = 'Updated automatically by';
 $strUpdateType = "Update Type";
 $strUnknown = "Unknown";
@@ -193,12 +211,17 @@ $strViewTask = "View Task";
 $strViewTasks = "View Tasks";
 $strViewUsers = "View Users";
 
-$strWaiting = "Waiting";
-$strWatchIncidents = "Watch Incidents";
+$strWaiting = 'Waiting';
+$strWatchIncidents = 'Watch Incidents';
+$strWelcome = 'Welcome';
+$strWorkAroundsAttempted = 'Workarounds Attempted';
+$strWorkAroundsAttemptedCustomerText = 'What have you done to try and solve the issue? What config changes were made? What patches/service packs were installed?';
 $strWebsite = "Website";
 $strWithheld = "Withheld";
 
-$strYesterday = "Yesterday";
+$strYesterday = 'Yesterday';
+$strYourCurrentIncidents = 'Your current incidents';
+$strYourSupportEntitlement = 'Your support entitlement';
 
 $strZeroRemaining = "Zero remaining";
 ?>
