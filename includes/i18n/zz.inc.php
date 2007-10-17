44308e00aff7ff9dad7c832a9ddc53f04e1692b6
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2007-10-17 15:01:04 +0000

    some lovely zs

--- includes/i18n/zz.inc.php
+++ includes/i18n/zz.inc.php
@@ -1,8 +1,271 @@
 <?php
 
-$languagestring="Test Language";
-$i18ncharset='utf-8';
+$languagestring='ZZ (ZZ)';
+$i18ncharset='UTF-8';
 
 // list of strings (Alphabetical)
+$strAbout = 'zz';
+$strAccepting = 'zz';
+$strActionNeeded = 'zz';
+$strActionPlan = 'zz';
+$strActionPlanBy = 'zz';
+$strActive = 'zz';
+$strAdd = 'zz';
+$strAddIncident = 'zz';
+$strAddTask = 'zz';
+$strAddTemplate = 'zz';
+$strAddUser = 'zz';
+$strAddContact = 'zz';
+$strAddContract = 'zz';
+$strAddIncident = 'zz';
+$strAddProduct = 'zz';
+$strAddReseller = 'zz';
+$strAddServiceLevel = 'zz';
+$strAddSkill = 'zz';
+$strAddTask = 'zz';
+$strAddTemplate = 'zz';
+$strAddUser = 'zz';
+$strAddVendor = 'zz';
+$strAddress = 'zz';
+$strAddress1 = 'zz';
+$strAddress2 = 'zz';
+$strAdminContact = 'zz';
+$strAgo = 'zz';
+$strAllClosed = 'zz';
+$strAllOpen = 'zz';
+$strApproveHolidays = 'zz';
+$strAuthor = 'zz';
 
+$strBrowse = 'zz';
+$strBrowseContacts = 'zz';
+$strBrowseFeedback = 'zz';
+$strBrowseKB = 'zz';
+$strBrowseSites = 'zz';
+$strBulkModify = 'zz';
+
+$strCity = 'zz';
+$strClose = 'zz';
+$strClosed = 'zz';
+$strContact = 'zz';
+$strContacts = 'zz';
+$strContract = 'zz';
+$strContracts = 'zz';
+$strControlPanel = 'zz';
+$strCompleted = 'zz';
+$strCompletion = 'zz';
+$strCustomers = 'zz';
+$strCounty = 'zz';
+$strCountry = 'zz';
+$strCurrentlyOpen = 'zz';
+$strClosureRequestForIncident = 'zz';
+$strContractID = 'zz';
+$strCurrentlyOpen = 'zz';
+$strCustomerImpact = 'zz';
+$strCustomerImpactCustomerText = 'zz';
+
+$strDashboard = 'zz';
+$strDataProtection = 'zz';
+$strDate = 'zz';
+$strDelete = 'zz';
+$strDeleteTask = 'zz';
+$strDepartment = 'zz';
+$strDescription = 'zz';
+$strDoesThisUpdateMeetSLA = 'zz';
+$strDueDate = 'zz';
+
+$strEdit = 'zz';
+$strEditContract = 'zz';
+$strEditedBy = 'zz';
+$strEditHolidyEntitlement = 'zz';
+$strEditProfileFor = 'zz';
+$strEditProduct = 'zz';
+$strEditSkill = 'zz';
+$strEditTask = 'zz';
+$strEditTemplate = 'zz';
+$strEmail = 'zz';
+$strEmailSettings = 'zz';
+$strEnabled = 'zz';
+$strEndDate = 'zz';
+$strEntitlement = 'zz';
+$strEngineer = 'zz';
+$strEngineers = 'zz';
+$strEscalationPaths = 'zz';
+$strExpired = 'zz';
+$strExpiryDate = 'zz';
+
+$strFax = 'zz';
+$strFeedback = 'zz';
+$strFeedbackForms = 'zz';
+$strFeeds = 'zz';
+$strFindContact = 'zz';
+$strForgottenDetails = 'zz';
+$strForm = 'zz';
+$strFTPFilesDB = 'zz';
+
+$strGlobalSignature = 'zz';
+$strGo = 'zz';
+$strGroup = 'zz';
+$strGroupMembership = 'zz';
+
+$strHelp = 'zz';
+$strHelpAbout = 'zz';
+$strHelpContents = 'zz';
+$strHoldingQueue = 'zz';
+$strHolidayEntitlement = 'zz';
+$strHolidayPlanner = 'zz';
+
+$strID = 'zz';
+$strIncidentsAvailable = 'zz';
+$strIncidentTitle = 'zz';
+$strIncidentsUsed = 'zz';
+$strInactive = 'zz';
+$strIncident = 'zz';
+$strIncidents = 'zz';
+$strInfo = 'zz';
+$strInstall = 'zz';
+$strInitialResponse = 'zz';
+
+$strKnowledgeBase = 'zz';
+$strJobTitle = 'zz';
+
+$strLastUpdated = 'zz';
+$strLicense = 'zz';
+$strLifetime = 'zz';
+$strLink = 'zz';
+$strListProducts = 'zz';
+$strListSkills = 'zz';
+$strLogged = 'zz';
+$strLoggedInAs = 'zz';
+$strLogIn = 'zz';
+$strLogin = 'zz';
+$strLogout = 'zz';
+
+$strMaintenance = 'zz';
+$strManageDashboardComponents = 'zz';
+$strManageUsers = 'zz';
+$strManageYourDashboard = 'zz';
+$strMarkComplete = 'zz';
+$strMine = 'zz';
+$strMobile = 'zz';
+$strMultiIncidents = 'zz';
+$strMyDashboard = 'zz';
+$strMyDetails = 'zz';
+$strMyHolidays = 'zz';
+$strMyProfile = 'zz';
+$strMySkills = 'zz';
+$strMySubstitutes = 'zz';
+
+$strName = 'zz';
+$strNewContact = 'zz';
+$strNewContract = 'zz';
+$strNewEscalationPath = 'zz';
+$strNewKBArticle = 'zz';
+$strNewProduct = 'zz';
+$strNewReseller = 'zz';
+$strNewSite = 'zz';
+$strNewSkill = 'zz';
+$strNewTemplate = 'zz';
+$strNewUser = 'zz';
+$strNo = 'zz';
+$strNoKBArticles = 'zz';
+$strNoIncidents = 'zz';
+$strNone = 'zz';
+$strNotAccepting = 'zz';
+$strNotes = 'zz';
+$strNow = 'zz';
+
+$strOtherIncidents = 'zz';
+$strOwner = 'zz';
+
+$strProblemDescription = 'zz';
+$strProblemDescriptionCustomerText = 'zz';
+$strProblemReproduction = 'zz';
+$strProblemReproductionCustomerText = 'zz';
+$strPassword = 'zz';
+$strPermission = 'zz';
+$strPermissions = 'zz';
+$strPlugins = 'zz';
+$strPostcode = 'zz';
+$strPriority = 'zz';
+$strPrivacy = 'zz';
+$strProduct = 'zz';
+$strPublished = 'zz';
+
+$strQueue = 'zz';
+
+$strRandomTip = 'zz';
+$strRealName = 'zz';
+$strRemove = 'zz';
+$strReports = 'zz';
+$strReset = 'zz';
+$strReview = 'zz';
+$strRole = 'zz';
+
+$strSalesperson = 'zz';
+$strSave = 'zz';
+$strSearch = 'zz';
+$strSearchAgain = 'zz';
+$strServiceLevel = 'zz';
+$strServiceLevels = 'zz';
+$strServicePacksApplied = 'zz';
+$strSetPermissions = 'zz';
+$strSetPublicHolidays = 'zz';
+$strSetYourStatus = 'zz';
+$strShowExpired = 'zz';
+$strShowRenewals = 'zz';
+$strSingleIncident = 'zz';
+$strSite = 'zz';
+$strSites = 'zz';
+$strSiteName = 'zz';
+$strSkill = 'zz';
+$strSkills = 'zz';
+$strSkillsMatrix = 'zz';
+$strSLATarget = 'zz';
+$strSoftware = 'zz';
+$strSoftwareVersion = 'zz';
+$strStartDate = 'zz';
+$strStatus = 'zz';
+$strSupport = 'zz';
+
+$strTags = 'zz';
+$strTasks = 'zz';
+$strTaskList = 'zz';
+$strTelephone = 'zz';
+$strTerminated = 'zz';
+$strTitle = 'zz';
+$strTodaysStats = 'zz';
+$strToday = 'zz';
+
+$strUpdated = 'zz';
+$strUpdatedAutomaticallyBy = 'zz';
+$strUpdateType = 'zz';
+$strUnknown = 'zz';
+$strUnlimited = 'zz';
+$strUserGroups = 'zz';
+$strUsername = 'zz';
+$strUsers = 'zz';
+
+$strValue = 'zz';
+$strVendor = 'zz';
+$strVersion = 'zz';
+$strView = 'zz';
+$strViewIncidents = 'zz';
+$strViewTask = 'zz';
+$strViewTasks = 'zz';
+$strViewUsers = 'zz';
+
+$strWaiting = 'zz';
+$strWatchIncidents = 'zz';
+$strWelcome = 'zz';
+$strWorkAroundsAttempted = 'zz';
+$strWorkAroundsAttemptedCustomerText = 'zz';
+$strWebsite = 'zz';
+$strWithheld = 'zz';
+
+$strYes = 'zz';
+$strYesterday = 'zz';
+$strYourCurrentIncidents = 'zz';
+$strYourSupportEntitlement = 'zz';
+
+$strZeroRemaining = 'zz';
 ?>
