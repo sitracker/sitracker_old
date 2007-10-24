e7e57709148c3cf975075d789f69f95b3f5f47c5
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2007-10-24 22:34:12 +0000

    some lovely french.

--- includes/i18n/fr-FR.inc.php
+++ includes/i18n/fr-FR.inc.php
@@ -1,34 +1,88 @@
 <?php
-// Translation by Camille Clymer <camille.clymer[at]gmail.com>
-$languagestring='French';
-$i18ncharset='UTF-8';
+$languagestring = 'French (fr-FR)';
+$i18ncharset = 'UTF-8';
 
-// list of strings (Alphabetical)
-$strClosed = 'Terminé';
+$strAdd = 'Ajouter';
+$strAdded = 'Ajouté';
+$strAddress = 'Adresse';
+$strAddress1 = 'Adresse1';
+$strAddress2 = 'Adresse2';
+
+$strAuthor = 'Auteur';
+
+$strChooseLanguage = 'Veuillez de choisir votre langue';
+
+$strCity = 'Ville';
+
 $strControlPanel = 'Panneau de Contrôle';
+
 $strCustomers = 'Clients';
-$strCurrentlyOpen = 'Actuellement ouvert';
+
+$strCountry = 'Pays';
+
+$strCurrentlyOpen = 'actuellement ouvert';
+
 $strDashboard = 'Tableau de bord';
+$strDate = 'Date';
+
+$strDelete = 'Supprimer';
+$strDeleted = 'Supprimé';
 $strHelp = 'Au secours';
+
 $strKnowledgeBase = 'Base de Connaissance';
+
 $strLogged = 'Connecté';
 $strLogout = 'Déconnexion';
-$strNoKBArticles = 'Pas d\'articles de la Base de Connaissance';
-$strNoIncidents = 'Pas d\'Incident';
+
 $strMyDashboard = 'Mon Panneau de Contrôle';
 $strMyDetails = 'Mes Détails Personnels';
 $strMyHolidays = 'Mes Vacances';
 $strMyProfile = 'Mon Profil';
 $strMySkills = 'Mes Habiletés';
 $strMySubstitutes = 'Mes Remplaçants';
-$strPublished = 'Publié';
+
+$strNo = 'Non';
+$strNoKBArticles = 'Pas d\'articles de la Base de Connaissance';
+$strNoIncidents = 'Pas d\'Incident';
+$strNow = 'Actuellement';
+
+$strOnHoliday = 'En Congé';
+
+$strPostcode = 'Code Postale';
+
+$strPublished = 'Public';
+
 $strRandomTip = 'Petit Conseil';
+
 $strReports = 'Rapports';
-$strSearch = 'Moteur de Recherche';
+$strSearch = 'Rechercher';
+$strSearchAgain = 'Rechercher de Nouveau';
+
 $strSupport = 'Assistance';
+
+$strTask = 'Tâche';
 $strTasks = 'Tâches';
+
+$strTime = 'L\'heure';
+$strTitle = 'Titre';
+
+$strTo = 'À';
 $strTodaysStats = 'Statistiques Quotidiennes';
+$strToday = 'Aujourd\'hui';
+
+$strTranslate = 'Traduire';
+$strTranslation = 'Traduction';
+
 $strUpdated = 'Mise à Jour';
+
+$strUnknown = 'Inconnu';
+
+$strUsed = 'Utilisé';
 $strUsers = 'Utilisants';
 
+$strWelcome = 'Bienvenue';
+$strYes = 'Oui';
+
+$strYou = 'Vous';
 ?>
+About SiT!
