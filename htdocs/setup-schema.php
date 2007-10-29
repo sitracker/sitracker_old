df418a52999beed1e19d124bdaf9c664ee18c8d5
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2007-10-29 22:51:08 +0000

    initial commit of global notices, still work to be done, probably best not to use it for now.

--- htdocs/setup-schema.php
+++ htdocs/setup-schema.php
@@ -1481,6 +1481,30 @@ CREATE TABLE IF NOT EXISTS `drafts` (
 
 ALTER TABLE `software` ADD `vendorid` INT( 5 ) NOT NULL AFTER `name` ;
 
+CREATE TABLE IF NOT EXISTS `notices` (
+  `id` int(11) NOT NULL auto_increment,
+  `text` tinytext NOT NULL,
+  `timestamp` date NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `id_2` (`id`),
+  KEY `id_3` (`id`),
+  FULLTEXT KEY `text` (`text`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+INSERT INTO `notices` (`id`, `text`, `timestamp`) VALUES 
+(1, 'My first notice.', '1970-01-01');
+
+CREATE TABLE IF NOT EXISTS `usernotices` (
+  `noticeid` int(11) NOT NULL,
+  `userid` tinyint(4) NOT NULL,
+  `dismissed` tinyint(1) default NULL,
+  PRIMARY KEY  (`noticeid`,`userid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+INSERT INTO `usernotices` (`noticeid`, `userid`, `dismissed`) VALUES 
+(1, 1, 0);
+
 ";
 
 
