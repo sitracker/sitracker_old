3c9f3940cf0f377934d047e32f08d49e276fd008
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2007-10-30 09:50:59 +0000

    more french translations.

--- includes/i18n/fr-FR.inc.php
+++ includes/i18n/fr-FR.inc.php
@@ -1,39 +1,97 @@
 <?php
-// SiT! Language File - French
+// SiT! Language File - French (fr-FR) by Administrator
 
-$languagestring = 'Français (fr-FR)';
+$languagestring = 'French (fr-FR)';
 $i18ncharset = 'UTF-8';
 
+$strAbsentSick = 'Malade';
+$strAccepting = 'Acceptant';
+$strActionNeeded = 'Action Nécessaire';
+$strActionPlan = 'Plan d\'Action';
+$strActionPlanBy = 'Plan d\'Action par %s';
+$strActions = 'Actions';
+$strActive = 'Actif';
+$strActivity = 'Activité';
 $strAdd = 'Ajouter';
 $strAdded = 'Ajouté';
+$strAddFeedbackForm = 'Ajouter Document de Commentaire';
+$strAddIncident = 'Ajouter Évènement';
+$strAddLink = 'Ajouter Lien';
+$strAddNote = 'Ajouter Note';
+$strAddTask = 'Ajouter Tâche';
+$strAddTemplate = 'Ajouter Formulaire';
+$strAddUser = 'Ajouter Utilisant';
+$strAddContact = 'Ajouter Contact';
+$strAddContract = 'Ajouter Accord';
+$strAddProduct = 'Ajouter Produit';
+$strAddReseller = 'Ajouter Revendeur';
+$strAddServiceLevel = 'Ajouter Niveau de Service';
+$strAddSite = 'Ajouter Site';
+$strAddSkill = 'Ajouter Habileté';
+$strAddVendor = 'Ajouter Vendeur';
 $strAddress = 'Adresse';
 $strAddress1 = 'Adresse1';
 $strAddress2 = 'Adresse2';
-
+$strAdminContact = 'Contact d\'Administration';
+$strAdvanced = 'Avancé';
+$strAge = 'Âge';
+$strAgo = 'Il y a %s';
+$strAll = 'Tous';
+$strAllClosed = 'Tous Terminés';
+$strAllOpen = 'Tous Ouverts';
+$strAlternative = 'Alternative';
+$strApproveHolidays = 'Autoriser Congé';
+$strAssign = 'Assigner';
+$strAssignTo = 'Assigner À';
+$strAtLunch = 'Au Déjeuner';
+$strAttachFile = 'Attacher Fichier';
+$strAttachment = 'Fichier';
 $strAuthor = 'Auteur';
+$strAverageIncidentDuration = 'Moyenne Durée d\'Évènement';
 
-$strChooseLanguage = 'Veuillez de choisir votre langue';
+$strBrowse = 'Parcourir';
+$strBrowseContacts = 'Parcourir contacts';
+$strBrowseFeedback = 'Parcourir commentaires';
+$strBrowseFeedbackForms = 'Parcourir Documents de Commentaire';
+$strBrowseKB = 'Parcourir Base de Connaissance';
 
+$strChooseLanguage = 'Veuillez choisir votre langue';
 $strCity = 'Ville';
-
+$strContact = 'Contact';
+$strContinue = 'Continuer';
+$strContacts = 'Contacts';
+$strContactsMulti = '%d Contacts';
+$strContract = 'Accord';
+$strContracts = 'Accords';
 $strControlPanel = 'Panneau de Contrôle';
-
+$strCustomer = 'Client';
 $strCustomers = 'Clients';
-
 $strCountry = 'Pays';
-
 $strCurrentlyOpen = 'actuellement ouvert';
 
 $strDashboard = 'Tableau de bord';
 $strDate = 'Date';
-
+$strDay = 'Jour';
+$strDays = 'Jours';
 $strDelete = 'Supprimer';
 $strDeleted = 'Supprimé';
+$strDeleteTask = 'Supprimer Tâche';
+
+$strEnglish = 'Anglais';
+$strExpiryDate = 'Date d\'Expiration';
+
+$strGo = 'Aller';
+
 $strHelp = 'Au secours';
+$strHelpChar = '?';
+$strHelpToTranslate = 'Cette page se vise à aider les traducteurs pour
+        traduire SiT !';
 
 $strKnowledgeBase = 'Base de Connaissance';
 
 $strLogged = 'Connecté';
+$strLogIn = 'Se Connecter';
+$strLogin = 'Entrer';
 $strLogout = 'Déconnexion';
 
 $strMyDashboard = 'Mon Panneau de Contrôle';
@@ -44,47 +102,40 @@ $strMySkills = 'Mes Habiletés';
 $strMySubstitutes = 'Mes Remplaçants';
 
 $strNo = 'Non';
-$strNoKBArticles = 'Pas d\'articles de la Base de Connaissance';
 $strNoIncidents = 'Pas d\'Incident';
+$strNoKBArticles = 'Pas d\'articles de la Base de Connaissance';
 $strNow = 'Actuellement';
 
 $strOnHoliday = 'En Congé';
 
 $strPostcode = 'Code Postale';
-
 $strPublished = 'Public';
 
 $strRandomTip = 'Petit Conseil';
-
 $strReports = 'Rapports';
+
 $strSearch = 'Rechercher';
 $strSearchAgain = 'Rechercher de Nouveau';
-
 $strSupport = 'Assistance';
 
 $strTask = 'Tâche';
 $strTasks = 'Tâches';
-
 $strTime = 'L\'heure';
 $strTitle = 'Titre';
-
 $strTo = 'À';
 $strTodaysStats = 'Statistiques Quotidiennes';
 $strToday = 'Aujourd\'hui';
-
 $strTranslate = 'Traduire';
 $strTranslation = 'Traduction';
 
 $strUpdated = 'Mise à Jour';
-
 $strUnknown = 'Inconnu';
-
 $strUsed = 'Utilisé';
 $strUsers = 'Utilisants';
+$strUsersTasks = 'Tâches de %s';
 
 $strWelcome = 'Bienvenue';
-$strYes = 'Oui';
 
+$strYes = 'Oui';
 $strYou = 'Vous';
 ?>
-About SiT!
