8d08d5dbabc7bb6a35e07187b66e6563df9042f7
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2007-11-12 11:01:13 +0000

    finish details page.

--- htdocs/portal.php
+++ htdocs/portal.php
@@ -341,6 +341,43 @@ switch ($page)
 
     //show user's details
     case 'details':
+        //if new details posted
+        if(cleanvar($_REQUEST['action']) == 'update')
+        {
+            $forenames = cleanvar($_REQUEST['forenames']);
+            $surname = cleanvar($_REQUEST['surname']);
+            $department = cleanvar($_REQUEST['department']);
+            $address1 = cleanvar($_REQUEST['address1']);
+            $address2 = cleanvar($_REQUEST['address2']);
+            $county = cleanvar($_REQUEST['county']);
+            $country = cleanvar($_REQUEST['country']);
+            $postcode = cleanvar($_REQUEST['postcode']);
+            $phone = cleanvar($_REQUEST['phone']);
+            $fax = cleanvar($_REQUEST['fax']);
+            $email = cleanvar($_REQUEST['email']);
+            $errors = 0;
+
+            // VALIDATION CHECKS */
+
+            if ($surname == '')
+            {
+                $errors = 1;
+                echo "<p class='error'>You must enter a surname</p>\n";
+            }            
+            if ($email == "" OR $email=='none' OR $email=='n/a')
+            {
+                $errors = 1;
+                echo "<p class='error'>You must enter an email address</p>\n";
+            }
+            if($errors == 0)
+            {
+                $updatesql = "UPDATE contacts SET forenames='$forenames', surname='$surname', department='$department', address1='$address1', address2='$address2', county='$county', country='$country', postcode='$postcode', phone='$phone', fax='$fax', email='$email' ";
+                $updatesql .= "WHERE id='{$_SESSION['contactid']}'";
+                mysql_query($updatesql);
+                if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
+            }
+        }
+        
         echo "<h2>{$strYourDetails}</h2>";
         $sql = "SELECT contacts.forenames, contacts.surname, contacts.department, contacts.address1, contacts.address2, contacts.county, contacts.country, contacts.postcode, contacts.phone, contacts.fax, contacts.email ";
         $sql .= "FROM contacts, sites ";
@@ -352,21 +389,20 @@ switch ($page)
 
         echo "<table align='center' class='vertical'>";
         echo "<tr><th colspan='2'><h3><img src='{$CONFIG['application_webpath']}images/icons/{$iconset}/32x32/contact.png' width='32' height='32' alt='' /> ".stripslashes($user->forenames).' '.stripslashes($user->surname)."</h3></th></tr>";
-        echo "<form>";
+        echo "<form action='$_SERVER[PHP_SELF]?page=details&action=update' method='post'>";
         echo "<tr><th>{$strForenames}: </th><td><input name='forenames' value='{$user->forenames}' /></td></tr>";
-        echo "<tr><th>{$strSurname}: </th><td><input name='forenames' value='{$user->surname}' /></td></tr>";
-        echo "<tr><th>{$strDepartment}: </th><td><input name='forenames' value='{$user->department}' /></td></tr>";
-        echo "<tr><th>{$strAddress1}: </th><td><input name='forenames' value='{$user->address1}' /></td></tr>";
-        echo "<tr><th>{$strAddress2}: </th><td><input name='forenames' value='{$user->address2}' /></td></tr>";
-        echo "<tr><th>{$strCounty}: </th><td><input name='forenames' value='{$user->county}' /></td></tr>";
-        echo "<tr><th>{$strCountry}: </th><td><input name='forenames' value='{$user->country}' /></td></tr>";
-        echo "<tr><th>{$strPostcode}: </th><td><input name='forenames' value='{$user->postcode}' /></td></tr>";
-        echo "<tr><th>{$strTelephone}: </th><td><input name='forenames' value='{$user->phone}' /></td></tr>";
-        echo "<tr><th>{$strFax}: </th><td><input name='forenames' value='{$user->fax}' /></td></tr>";
-        echo "<tr><th>{$strEmail}: </th><td><input name='forenames' value='{$user->email}' /></td></tr>";
+        echo "<tr><th>{$strSurname}: </th><td><input name='surname' value='{$user->surname}' /></td></tr>";
+        echo "<tr><th>{$strDepartment}: </th><td><input name='department' value='{$user->department}' /></td></tr>";
+        echo "<tr><th>{$strAddress1}: </th><td><input name='address1' value='{$user->address1}' /></td></tr>";
+        echo "<tr><th>{$strAddress2}: </th><td><input name='address2' value='{$user->address2}' /></td></tr>";
+        echo "<tr><th>{$strCounty}: </th><td><input name='county' value='{$user->county}' /></td></tr>";
+        echo "<tr><th>{$strCountry}: </th><td><input name='country' value='{$user->country}' /></td></tr>";
+        echo "<tr><th>{$strPostcode}: </th><td><input name='postcode' value='{$user->postcode}' /></td></tr>";
+        echo "<tr><th>{$strTelephone}: </th><td><input name='phone' value='{$user->phone}' /></td></tr>";
+        echo "<tr><th>{$strFax}: </th><td><input name='fax' value='{$user->fax}' /></td></tr>";
+        echo "<tr><th>{$strEmail}: </th><td><input name='email' value='{$user->email}' /></td></tr>";
         echo "</table>";
         echo "<p align='center'><input type='submit' value='{$strUpdate}' /></p>";
-
         break;
 
     //show specified incident
