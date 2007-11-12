293652e29eee8447c65ecda30b000480a7a85a29
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-11-12 21:44:12 +0000

    Fix mismatch args

--- includes/htmlheader.inc.php
+++ includes/htmlheader.inc.php
@@ -141,7 +141,7 @@ if($sit[0] != '')
         //if($notice->text == '$strFirstLogin') $notice->text = $strFirstLogin;
         $notice->text = str_replace('$strFirstLogin', $strFirstLogin, $notice->text);
         $notice->text = str_replace('$strNoEmailSet', $strNoEmailSet, $notice->text);
-        $notice->text = str_replace('$strSitUpgraded', sprintf($strSitUpgraded, $CONFIG['application_shortname']), $notice->text);
+        $notice->text = str_replace('$strSitUpgraded', sprintf($strSitUpgraded, $CONFIG['application_shortname'], "v{$application_version} {$application_revision}"), $notice->text);
         $notice->text = str_replace('$strSitUpgradedLink', $strSitUpgradedLink, $notice->text);
         //critical error
         if($notice->type == '1')
