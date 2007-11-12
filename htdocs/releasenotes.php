24ee83c524468dba7505a95001e7ca916d071142
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2007-11-12 10:03:43 +0000

    placeholder

---
+++ htdocs/releasenotes.php
@@ -0,0 +1,16 @@
+<?php
+
+$permission=0;
+require('db_connect.inc.php');
+require('functions.inc.php');
+
+// This page requires authentication
+require('auth.inc.php');
+include_once('htmlheader.inc.php');
+
+echo "<h2>Placeholder, forgot to commit real version</h2>";
+
+include_once('htmlfooter.inc.php');
+
+
+?>
