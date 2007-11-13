04d8d049bf3d0930fa9372ae019ae10abd25b0ef
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-11-13 15:41:56 +0000

    Updated en-US translation

--- includes/i18n/en-US.inc.php
+++ includes/i18n/en-US.inc.php
@@ -1,17 +1,28 @@
 <?php
 // Translation by Ivan Lucas <ivanlucas[at]users.sourceforge.net>
-$languagestring='English (US)';
-$i18ncharset='UTF-8';
+$languagestring = 'English (US) (en-US)';
+$i18ncharset = 'UTF-8';
+
+$strApproveHolidays = 'Approve Vacations';
+
+$strBookHoliday = 'Book Vacation';
 
 $strCounty = 'State';
+
+$strEditHolidays = 'Edit Vacations';
+$strEditHolidayEntitlement = 'Edit Vacation Entitlement';
 
 $strHolidayEntitlement = 'Vacation Entitlement';
 $strHolidayPlanner = 'Vacation Planner';
 
 $strLicense = 'License';
+$strLicenseType = 'License Type';
+$strLicenseQuantity = 'License Quantity';
 
 $strMobile = 'Cell Phone';
 $strMyHolidays = 'My Vacations';
 
 $strPostcode = 'Zip Code';
+
+$strResolutionReprioritisation = 'Resolution/Reprioritization';
 ?>
