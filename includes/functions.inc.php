edcc2ea4f73066f04ca2630800ef69280e064ec3
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-11-14 12:11:52 +0000

    * More i18n * Fix icons on update page, broken by putting into a function without globalising $iconset

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -1470,18 +1470,18 @@ function priority_drop_down($name, $id, $max=4, $disable=FALSE)
     if ($id == 0) $html .= "<option selected='selected' value='0'></option>\n";
     $html .= "<option style='text-indent: 14px; background-image: url({$CONFIG['application_webpath']}/images/low_priority.gif); background-repeat:no-repeat;' value='1'";
     if ($id==1) $html .= " selected='selected'";
-    $html .= ">Low</option>\n";
+    $html .= ">{$GLOBALS['strLow']}</option>\n";
     $html .= "<option style='text-indent: 14px; background-image: url({$CONFIG['application_webpath']}/images/med_priority.gif); background-repeat:no-repeat;' value='2'";
     if ($id==2) $html .= " selected='selected'";
-    $html .= ">Medium</option>\n";
+    $html .= ">{$GLOBALS['strMedium']}</option>\n";
     $html .= "<option style='text-indent: 14px; background-image: url({$CONFIG['application_webpath']}/images/high_priority.gif); background-repeat:no-repeat;' value='3'";
     if ($id==3) $html .= " selected='selected'";
-    $html .= ">High</option>\n";
+    $html .= ">{$GLOBALS['strHigh']}</option>\n";
     if ($max >=4)
     {
         $html .= "<option style='text-indent: 14px; background-image: url({$CONFIG['application_webpath']}/images/crit_priority.gif); background-repeat:no-repeat;' value='4'";
         if ($id==4) $html .= " selected='selected'";
-        $html .= ">Critical</option>\n";
+        $html .= ">{$GLOBALS['strCritical']}</option>\n";
     }
     $html .= "</select>\n";
     return $html;
