fac91f94eee3a5e21547d42de782cc83b0069956
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-11-22 15:45:49 +0000

    German language file (3.18%)

---
+++ includes/i18n/de-DE.inc.php
@@ -0,0 +1,42 @@
+<?php
+// SiT! Language File - German (de-DE) by Ivan Lucas
+
+$languagestring = 'German (de-DE)';
+$i18ncharset = 'UTF-8';
+
+$strAll = 'Alle';
+
+$strDefault = 'Standard';
+$strDelete = 'Löschen';
+
+$strEdit = 'Bearbeiten';
+
+$strGo = 'OK';
+
+$strLanguage = 'Sprache';
+$strLogIn = 'Login';
+$strLogout = 'Neu einloggen';
+
+$strNo = 'Nein';
+$strNone = 'keine';
+
+$strOperation = 'Operationen';
+
+$strPassword = 'Passwort';
+
+$strReset = 'Zurücksetzen';
+
+$strSave = 'Speichern';
+$strSearch = 'Suche';
+$strShowAll = 'Alles anzeigen';
+
+$strTotal = 'Insgesamt';
+$strType = 'Typ';
+
+$strUnknown = 'Unbekannt';
+$strUsername = 'Benutzername';
+
+$strView = 'Ansicht';
+
+$strYes = 'Ja';
+?>
