1e9976be26d66d434107b3072d08d32ed1b491f2
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-11-26 20:02:46 +0000

    Updated german (de-DE) translation (now 6.5% complete)

--- includes/i18n/de-DE.inc.php
+++ includes/i18n/de-DE.inc.php
@@ -4,32 +4,61 @@
 $languagestring = 'German (de-DE)';
 $i18ncharset = 'UTF-8';
 
+$strAdd = 'Hinzufügen';
+$strAddNote = 'Notiz hinzufügen';
 $strAll = 'Alle';
+$strAssign = 'Zuordnen';
+$strAssignTo = 'Zuordnen zu';
+
+$strClose = 'Schließen';
+$strClosed = 'Geschlossen';
+
+$strDays = 'Tage';
+
+$strdays = 'tage';
 
 $strDefault = 'Standard';
 $strDelete = 'Löschen';
+$strDescription = 'Beschreibung';
 
 $strEdit = 'Bearbeiten';
+
+$strForgottenDetails = 'Passwort vergessen?';
+$strFrom = 'Von';
 
 $strGo = 'OK';
 
 $strLanguage = 'Sprache';
+$strLoggedInAs = 'Angemeldet als: %s';
 $strLogIn = 'Login';
 $strLogout = 'Neu einloggen';
 
-$strNo = 'Nein';
-$strNone = 'keine';
+$strManageUsers = 'Benutzer verwalten';
+$strMessage = 'Meldung';
 
+$strNo = 'Nein';
+$strNone = 'Keine';
+$strNotes = 'Notiz';
+
+$strOpened = 'Geöffnet';
 $strOperation = 'Operationen';
 
 $strPassword = 'Passwort';
+$strPriority = 'Priorität';
+$strPrivate = 'Privat';
+$strPublic = 'Öffentlich';
 
+$strRealName = 'Vollständiger Name';
+$strRemove = 'Entfernen';
 $strReset = 'Zurücksetzen';
 
 $strSave = 'Speichern';
 $strSearch = 'Suche';
 $strShowAll = 'Alles anzeigen';
+$strStatus = 'Status';
+$strSummary = 'Zusammenfassung';
 
+$strTo = 'An';
 $strTotal = 'Insgesamt';
 $strType = 'Typ';
 
