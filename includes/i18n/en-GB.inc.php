c03b49ffdb5bd35a082795ad13cda6f320da79d3
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-11-26 13:19:56 +0000

    * Committing work on reassigning (still incomplete) * Some defines for holidays

--- includes/i18n/en-GB.inc.php
+++ includes/i18n/en-GB.inc.php
@@ -533,6 +533,7 @@ $strQueue = 'Queue';
 $strRandomTip = 'Random Tip';
 $strRealName = 'Real Name';
 $strReassign = 'Reassign';
+$strReassignTo = 'Reassign to';
 $strReassignedToBy = 'Reassigned to %s by %s';
 $strReassignText = 'Explain in detail why you are reassigning this incident and include instructions to the new owner as to what action should be taken next.  Please be as detailed as possible and include full descriptions of any work you have performed.';
 $strReceivedByEmail = 'This information was received in the email.  It is not editable.';
