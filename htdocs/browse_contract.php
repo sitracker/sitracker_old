58433169113be034959085b6111dbf957069715c
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-11-27 22:39:46 +0000

    Remove debugging

--- htdocs/browse_contract.php
+++ htdocs/browse_contract.php
@@ -141,7 +141,6 @@ if (!empty($sort))
     if ($order=='a' OR $order=='ASC' OR $order='') $sql .= "ASC";
     else $sql .= "DESC";
 }
-echo $sql;
 $result = mysql_query($sql);
 if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
 
