bc411a929cd061b6ed678c55131919578518feae
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-11-27 20:52:11 +0000

    Fix divide by zero bug in timecalc slacalc

--- htdocs/auto/timecalc.php
+++ htdocs/auto/timecalc.php
@@ -110,19 +110,21 @@ while ($incident=mysql_fetch_array($incident_result)) {
         if($incident['slanotice']==0)
         {
             //reaching SLA
-            if(($newSlaTime / $times['next_sla_time']) >= ($CONFIG['urgent_threshold'] * 0.01))
+            if ($times['next_sla_time'] > 0) $reach = $newSlaTime / $times['next_sla_time'];
+            else $reach = 0;
+            if($reach >= ($CONFIG['urgent_threshold'] * 0.01))
             {
                 //create notice, workaround until triggers are implemented - KMH 26/11/07
                 $timetil = $times['next_sla_time']-$newSlaTime;
-                
+
                 $sql = "INSERT into notices(type, text, linktext, link, timestamp) ";
-                
+
                 if($timetil >= 0)
                 {
                     $text = "will exceed its SLA soon";
                     $sql .= "VALUES({$CONFIG['NEARING_SLA_TYPE']}, 'Incident {$incident['id']} - \'{$incident['title']}\' $text', 'View Incident', 'javascript:incident_details_window(\'{$incident['id']}\',\'incident{$incident['id']}\')', NOW())";
                 }
-                elseif($timetil < 0) 
+                elseif($timetil < 0)
                 {
                     $text = "has exceeded its SLA";
                     $sql .= "VALUES({$CONFIG['OUT_OF_SLA_TYPE']}, 'Incident {$incident['id']} - \'{$incident['title']}\' $text', 'View Incident', 'javascript:incident_details_window(\'{$incident['id']}\',\'incident{$incident['id']}\')', NOW())";
