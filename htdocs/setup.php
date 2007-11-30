4421cb81ef649f9772edab32ac70cbfc49654e05
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-11-30 16:47:11 +0000

    quotes around strings in sql

--- htdocs/setup.php
+++ htdocs/setup.php
@@ -227,7 +227,7 @@ function user_notify_upgrade()
     while($user = mysql_fetch_object($result))
     {
         $noticesql = "INSERT into notices(userid, type, text, linktext, link, gid, timestamp) ";
-        $noticesql .= "VALUES({$user->id}, ".USER_LANG_DIFFERS_TYPE.", '\$strSitUpgraded', '\$strSitUpgradedLink', '{$CONFIG['application_webpath']}releasenotes.php', {$gid}, NOW())";
+        $noticesql .= "VALUES ({$user->id}, ".USER_LANG_DIFFERS_TYPE.", '\$strSitUpgraded', '\$strSitUpgradedLink', '{$CONFIG['application_webpath']}releasenotes.php', '{$gid}', NOW())";
         mysql_query($noticesql);
         if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
     }
