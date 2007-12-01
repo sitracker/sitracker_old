7f7354611405462700a29a03867719181dbe9ccb
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-12-01 17:35:08 +0000

    Use html_redirect()

--- htdocs/add_vendor.php
+++ htdocs/add_vendor.php
@@ -74,7 +74,7 @@ else
         {
             $id=mysql_insert_id();
             journal(CFG_LOGGING_DEBUG, 'Vendor Added', "Vendor $id was added", CFG_JOURNAL_DEBUG, $id);
-            confirmation_page("2", "products.php", "<h2>Vendor Addition Successful</h2><p align='center'>{$strPleaseWaitRedirect}...</p>");
+            html_redirect("products.php");
         }
     }
     else
