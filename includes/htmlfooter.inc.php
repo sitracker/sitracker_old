482b83462d80be2d5bf1ddaef54e35236026f0b2
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-12-02 12:43:37 +0000

    Print $dbg debugging info at the bottom of the page

--- includes/htmlfooter.inc.php
+++ includes/htmlfooter.inc.php
@@ -22,6 +22,7 @@ if ($CONFIG['debug'] == TRUE)
     $exec_time_end = getmicrotime();
     $exec_time = $exec_time_end - $exec_time_start;
     echo "<p>CPU Time: ".number_format($exec_time,3)." seconds</p>";
+    if (isset($dbg)) echo "<hr /><pre>".print_r($dbg,true)."</pre>";
     echo "</div>";
 }
 echo "</body>\n</html>\n";
