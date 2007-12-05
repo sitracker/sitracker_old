8c99cc3074a03fee3e31b524a4b826af6d47fc47
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-12-05 13:29:25 +0000

    Speed up redirection for 'success'

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -2261,7 +2261,8 @@ function confirmation_page($refreshtime, $location, $message)
 function html_redirect($url, $success=TRUE, $message='')
 {
     global $CONFIG, $headerdisplayed;
-    if (empty($message)) $refreshtime = 3;
+    if (empty($message)) $refreshtime = 0;
+    elseif ($sucess==FALSE) $refreshtime = 3;
     else $refreshtime = 6;
     $refresh = "{$refreshtime}; url={$url}";
 
