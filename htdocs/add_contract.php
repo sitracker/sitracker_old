a54e7cc02c203ac07c51c092eee4d0a68762edc3
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2007-12-07 16:12:53 +0000

    more forms

--- htdocs/add_contract.php
+++ htdocs/add_contract.php
@@ -36,53 +36,85 @@ if ($action == "showform" OR $action=='')
     }
     </script>
     <?php
+    echo show_errors();
+    $_SESSION['formerrors'] = NULL;
     echo "<h2><img src='{$CONFIG['application_webpath']}images/icons/{$iconset}/32x32/contract.png' width='32' height='32' alt='' /> ";
     echo "{$strAddContract}</h2>";
     echo "<h5>".sprintf($strMandatoryMarked,"<sup class='red'>*</sup>")."</h5>";
     echo "<form name='addcontract' action='{$_SERVER['PHP_SELF']}?action=add' method='post' onsubmit='return confirm_submit();'>";
     echo "<table align='center' class='vertical'>";
-    echo "<tr><th>{$strSite}: <sup class='red'>*</sup></th><td>".site_drop_down("site", $siteid)." </td></tr>\n";
-    echo "<tr><th>{$strContacts}:<sup class='red'>*</sup></th><td>";
+    echo "<tr><th>{$strSite} <sup class='red'>*</sup></th><td>";
+    if($_SESSION['formdata']['site'] != "")
+        echo site_drop_down("site", $_SESSION['formdata']['site'])." </td></tr>\n";
+    else
+        echo site_drop_down("site", $siteid)." </td></tr>\n";
+
+    echo "<tr><th>{$strContacts}<sup class='red'>*</sup></th><td>";
     // TODO all supportedcontacts disabled for 3.31 release
     // echo "<input value='amount' type='radio' name='contacts' checked='checked' />";
+ 
     echo "<input type='hidden' name ='contacts' value='amount' />";
-    echo "{$strLimitTo} <input size='2' value='0' name='amount' /> {$strSupportedContacts} ({$str0MeansUnlimited})<br />";
+    echo "{$strLimitTo} <input size='2' name='amount' ";
+    if($_SESSION['formdata']['contacts'] != "")
+        echo "value='{$_SESSION['formdata']['amount']}'";
+    else
+        echo "value='0'";
+    echo " /> {$strSupportedContacts} ({$str0MeansUnlimited})<br />";
     // echo "<input type='radio' value='all' name='contacts' />";
     // echo "{$strAllSiteContactsSupported}";
     echo "</td></tr>";
-    echo "<tr><th>{$strProduct}: <sup class='red'>*</sup></th><td>".product_drop_down("product", 0)."</td></tr>\n";
-    echo "<tr><th>{$strExpiryDate}: <sup class='red'>*</sup></th>";
-    echo "<td><input name='expiry' size='10' /> ".date_picker('addcontract.expiry')." <input type='checkbox' name='noexpiry' /> {$strUnlimited}</td></tr>\n";
-    echo "<tr><th>{$strServiceLevel}:</th><td>".servicelevel_drop_down('servicelevelid', 1, TRUE)."</td></tr>\n";
+    echo "<tr><th>{$strProduct} <sup class='red'>*</sup></th><td>";
+    if($_SESSION['formdata']['product'] != "")
+        echo product_drop_down("product", $_SESSION['formdata']['product'])."</td></tr>\n";
+    else
+        echo product_drop_down("product", 0)."</td></tr>\n";
 
-    echo "<tr><th>{$strAdminContact}: <sup class='red'>*</sup></th><td>".contact_drop_down("admincontact", 0, true)."</td></tr>\n";
-    echo "<tr><th>{$strNotes}:</th><td><textarea cols='40' name='notes' rows='5'></textarea></td></tr>\n";
+    echo "<tr><th>{$strExpiryDate} <sup class='red'>*</sup></th>";
+    echo "<td><input name='expiry' size='10' ";
+    if($_SESSION['formdata']['expiry'] != "")
+        echo "value='{$_SESSION['formdata']['expiry']}'";
+    echo "/> ".date_picker('addcontract.expiry');
+    echo "<input type='checkbox' name='noexpiry' ";
+    if($_SESSION['formdata']['noexpiry'] == "on");
+        echo "checked='checked' ";
+    echo "onclick=\"this.form.expiry.value=''\" /> {$strUnlimited}</td></tr>\n";
 
-    echo "<tr><th></th><td><a href=\"javascript:toggleDiv('hidden');\">{$strAdvanced}</a></td></tr>\n";
+    echo "<tr><th>{$strServiceLevel}</th><td>";
+    if($_SESSION['formdata']['servicelevelid'] != "")
+        echo servicelevel_drop_down('servicelevelid', $_SESSION['formdata']['servicelevelid'], TRUE)."</td></tr>\n";
+    else
+        echo servicelevel_drop_down('servicelevelid', 1, TRUE)."</td></tr>\n";
+
+    echo "<tr><th>{$strAdminContact} <sup class='red'>*</sup></th><td>".contact_drop_down("admincontact", 0, true)."</td></tr>\n";
+    echo "<tr><th>{$strNotes}</th><td><textarea cols='40' name='notes' rows='5'></textarea></td></tr>\n";
+
+    echo "<tr><th></th><td><a href=\"javascript:toggleDiv('hidden');\">{$strMore}</a></td></tr>\n";
 
     echo "<tbody id='hidden' style='display:none'>";
 
-    echo "<tr><th>{$strReseller}:</th><td>";
+    echo "<tr><th>{$strReseller}</th><td>";
     reseller_drop_down("reseller", 0);
     echo "</td></tr>\n";
 
-    echo "<tr><th>{$strLicenseQuantity}:</th><td><input value='0' maxlength='7' name='licence_quantity' size='5' />";
+    echo "<tr><th>{$strLicenseQuantity}</th><td><input value='0' maxlength='7' name='licence_quantity' size='5' />";
     echo " ({$str0MeansUnlimited})</td></tr>\n";
 
-    echo "<tr><th>{$strLicenseType}:</th><td>";
+    echo "<tr><th>{$strLicenseType}</th><td>";
     licence_type_drop_down("licence_type", 0);
     echo "</td></tr>\n";
 
-    echo "<tr><th>{$strIncidentPool}:</th>";
+    echo "<tr><th>{$strIncidentPool}</th>";
     $incident_pools = explode(',', "Unlimited,{$CONFIG['incident_pools']}");
     echo "<td>".array_drop_down($incident_pools,'incident_poolid',$maint['incident_quantity'])."</td></tr>";
 
-    echo "<tr><th>{$strProductOnly}:</th><td><input name='productonly' type='checkbox' value='yes' /></td></tr></tbody>\n";
+    echo "<tr><th>{$strProductOnly}</th><td><input name='productonly' type='checkbox' value='yes' /></td></tr></tbody>\n";
 
     echo "</table>\n";
     echo "<p align='center'><input name='submit' type='submit' value=\"{$strAddContract}\" /></p>";
     echo "</form>";
     include('htmlfooter.inc.php');
+    
+    $_SESSION['formdata'] = NULL;
 }
 elseif ($action == "add")
 {
@@ -108,43 +140,45 @@ elseif ($action == "add")
 
     $incident_pools = explode(',', "0,{$CONFIG['incident_pools']}");
     $incident_quantity = $incident_pools[$_POST['incident_poolid']];
+    
+    $_SESSION['formdata'] = $_REQUEST;
 
     // Add maintenance to database
     $errors = 0;
     // check for blank site
     if ($site == 0)
     {
-        $errors = 1;
-        $errors_string .= "<p class='error'>You must select a site</p>\n";
+        $errors++;
+        $_SESSION['formerrors'][''] = "You must select a site</p>\n";
     }
     // check for blank product
     if ($product == 0)
     {
-        $errors = 1;
-        $errors_string .= "<p class='error'>You must select a product</p>\n";
+        $errors++;
+        $_SESSION['formerrors'][''] = "You must select a product</p>\n";
     }
     // check for blank admin contact
     if ($admincontact == 0)
     {
-        $errors = 1;
-        $errors_string .= "<p class='error'>You must select an admin contact</p>\n";
+        $errors++;
+        $_SESSION['formerrors'][''] = "You must select an admin contact</p>\n";
     }
     // check for blank service level
     if ($admincontact == 0)
     {
-        $errors = 1;
-        $errors_string .= "<p class='error'>You must select a service level</p>\n";
+        $errors++;
+        $_SESSION['formerrors'][''] = "You must select a service level</p>\n";
     }
     // check for blank expiry day
     if ($expirydate == 0)
     {
-        $errors = 1;
-        $errors_string .= "<p class='error'>You must enter an expiry date</p>\n";
+        $errors++;
+        $_SESSION['formerrors'][''] = "You must enter an expiry date</p>\n";
     }
-    if ($expirydate < $now AND $expirydate != -1)
+    elseif ($expirydate < $now AND $expirydate != -1)
     {
-        $errors = 1;
-        $errors_string .= "<p class='error'>Expiry date cannot be in the past</p>\n";
+        $errors++;
+        $_SESSION['formerrors'][''] = "Expiry date cannot be in the past</p>\n";
     }
     // add maintenance if no errors
     if ($errors == 0)
@@ -185,13 +219,14 @@ elseif ($action == "add")
 
             html_redirect("contract_details.php?id=$maintid");
         }
+        $_SESSION['formdata'] = NULL;
+        $_SESSION['formerrors'] = NULL;
     }
     else
     {
         // show error message if errors
         include('htmlheader.inc.php');
-        echo $errors_string;
-        include('htmlfooter.inc.php');
+        html_redirect("add_contract.php", FALSE);
     }
 }
 ?>
