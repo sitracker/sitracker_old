73ee25cc9a1b5b3978b9b05dddd401b770ede1d9
Ivan Lucas <ivanlucas@users.sourceforge.net>
2007-12-13 16:31:50 +0000

    Remove select box label as per the rest of SiT

--- htdocs/incident/email.inc.php
+++ htdocs/incident/email.inc.php
@@ -245,7 +245,7 @@ switch ($step)
         echo "<input name='date' size='10' value='{$date}' onclick=\"window.document.updateform.timetonextaction_none[2].checked = true;\"/> ";
         echo date_picker('updateform.date');
         echo "<select name='timeoffset' onchange='window.document.updateform.timetonextaction_none[2].checked = true;'>";
-        echo "<option>Choose Time</option>";
+        echo "<option value=''></option>";
         echo "<option value='0'>8:00 AM</option>";
         echo "<option value='1'>9:00 AM</option>";
         echo "<option value='2'>10:00 AM</option>";
