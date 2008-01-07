ebcc44913f1211ef3f6c1751fa66ef80ef7330ed
Ivan Lucas <ivanlucas@users.sourceforge.net>
2008-01-07 21:24:44 +0000

    * Use internationalised text for full day names in ldate() * Use ldate() for readable dates * Add french language day names

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -4596,7 +4596,8 @@ function readable_date($date)
     elseif (date('dmy', $date) == date('dmy', (time()-86400)))
         $datestring = "{$GLOBALS['strYesterday']} @ ".date('g:ia', $date);
     else
-        $datestring = date("l jS M y @ g:ia", $date);
+        $datestring = ldate("l jS M y @ g:ia", $date);
+
     return $datestring;
 }
 
@@ -6437,11 +6438,21 @@ function truncate_string($text, $maxlength=255, $html=TRUE)
     * @param $format string. date() format
     * @param $date int. UNIX timestamp
     * @returns string. An internationised date/time string
-    * @todo actually do some internationisation
+    * @todo Currently only translates day names in full, needs to do
+    *       short day names, month names in full, short month names, am/pm?
 */
 function ldate($format, $date)
 {
     $datestring = date($format, $date);
+
+    // Internationalise full day names
+    if (strpos($format, 'l') !== FALSE)
+    {
+        $days = array('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday');
+        $i18ndays = array($GLOBALS['strMonday'], $GLOBALS['strTuesday'], $GLOBALS['strWednesday'],
+                          $GLOBALS['strThursday'], $GLOBALS['strFriday'], $GLOBALS['strSaturday'], $GLOBALS['strSunday']);
+        $datestring = str_replace($days, $i18ndays, $datestring);
+    }
 
     return $datestring;
 }
--- includes/i18n/en-GB.inc.php
+++ includes/i18n/en-GB.inc.php
@@ -924,7 +924,6 @@ $strUsersTasks = '%s\'s Tasks';
 $strUsersBoldSkills = 'Users shows in bold typeface are known to have relevant skills';
 $strUseThisInFuture = 'Use this in future';
 
-
 $strValue = 'Value';
 $strVariable = 'Variable';
 $strVendor = 'Vendor';
