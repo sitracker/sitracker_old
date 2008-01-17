43f5ee1030de5dda03b7c166dd4a52dc57588b15
Ivan Lucas <ivanlucas@users.sourceforge.net>
2008-01-17 14:22:21 +0000

    Merged revisions 2124 via svnmerge from https://sitracker.svn.sourceforge.net/svnroot/sitracker/trunk
    ........
      r2124 | paulheaney | 2008-01-13 19:31:47 +0000 (Sun, 13 Jan 2008) | 3 lines
      
      * Finished implementation of drop down on dashlet allowing status selection
    ........
    
    
    git-svn-id: https://sitracker.svn.sourceforge.net/svnroot/sitracker/branches/mincepie@2167 6e0da8f0-d90d-0410-b14f-823b0ad1652c

--- htdocs/display_watch_incidents.inc.php
+++ htdocs/display_watch_incidents.inc.php
@@ -9,13 +9,16 @@
 //
 // Author: Paul Heaney <paulheaney[at]users.sourceforge.net>
 
-@include ('set_include_path.inc.php');
-$permission = 0; // not required
-require ('db_connect.inc.php');
-require ('functions.inc.php');
+@include('set_include_path.inc.php');
+$permission=0; // not required
+require('db_connect.inc.php');
+require('functions.inc.php');
 
 // This page requires authentication
-require ('auth.inc.php');
+require('auth.inc.php');
+
+$queue = $_REQUEST['queue'];
+if (empty($queue)) $queue = 5;
 
 ?>
 <script type="text/javascript">
@@ -28,22 +31,31 @@ function statusform_submit(user)
 </script>
 <?php
 
-$queue = $_REQUEST['queue'];
+//Removed by INL 26Nov07 in prep for 3.31 release
+echo "<form action='{$_SERVER['PHP_SELF']}' style='text-align: center;'>";
+echo "{$strQueue}: <select class='dropdown' name='queue' onchange='window.location.href=this.options[this.selectedIndex].value'>\n";
+echo "<option ";
+if ($queue == 5)
+{
+    echo "selected='selected' ";
+}
+echo "value=\"javascript:get_and_display('display_watch_incidents.inc.php?queue=5','watch_incidents_windows');\">{$strAll}</option>\n";
+echo "<option ";
+if ($queue == 1)
+{
+    echo "selected='selected' ";
+}
+echo "value=\"javascript:get_and_display('display_watch_incidents.inc.php?queue=1','watch_incidents_windows');\">{$strActionNeeded}</option>\n";
+echo "<option ";
+if ($queue == 3)
+{
+    echo "selected='selected' ";
+}
+echo "value=\"javascript:get_and_display('display_watch_incidents.inc.php?queue=3','watch_incidents_windows');\">{$strAllOpen}</option>\n";
+echo "</select>\n";
+echo "</form>";
 
-// Removed by INL 26Nov07 in prep for 3.31 release
-// echo "<form action='{$_SERVER['PHP_SELF']}' style='text-align: center;'>";
-// echo "{$strQueue}: <select class='dropdown' name='queue' onchange='window.location.href=this.options[this.selectedIndex].value'>\n";
-// echo "<option ";
-// if ($queue == 1) echo "selected='selected' ";
-// echo "value=\"javascript:get_and_display('display_watch_incidents.inc.php?queue=1','watch_incidents_windows');\">{$strActionNeeded}</option>\n";
-// echo "<option ";
-// if ($queue == 3) echo "selected='selected' ";
-// echo "value=\"javascript:get_and_display('display_watch_incidents.inc.php?queue=3','watch_incidents_windows');\">{$strAllOpen}</option>\n";
-// echo "</select>\n";
-// echo "</form>";
-
-
-$sql = "SELECT type, id FROM `{$dbDashboardWatchIncidents}` WHERE userid = {$sit[2]} ORDER BY type";
+$sql = "SELECT type, id FROM dashboard_watch_incidents WHERE userid = {$sit[2]} ORDER BY type";
 $result = mysql_query($sql);
 if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
 
@@ -52,55 +64,55 @@ if (mysql_num_rows($result) > 0)
 {
     $header_printed = FALSE;
     $previous = 0;
-    while ($obj = mysql_fetch_object($result))
+    while($obj = mysql_fetch_object($result))
     {
-        if ($obj->type !=3 AND $previous == 3)
+        if($obj->type !=3 AND $previous == 3)
         {
             echo "</table>";
         }
 
-        if ($obj->type == 3 AND !$header_printed)
+        if($obj->type == 3 AND !$header_printed)
         {
             echo "<table align='center' style='width: 100%'>";
         }
-        else if ($obj->type != 3)
+        else if($obj->type != 3)
         {
             echo "<table align='center' style='width: 100%'>";
         }
 
-        switch ($obj->type)
+        switch($obj->type)
         {
             case '0': //Site
-                $sql = "SELECT i.id, i.title, i.status, i.servicelevel, i.maintenanceid, i.priority, c.forenames, c.surname, c.siteid ";
-                $sql .= "FROM `{$dbIncidents}` AS i, `{$dbContacts}` AS c ";
-                $sql .= "WHERE i.contact = c.id AND c.siteid = {$obj->id} ";
-                $sql .= "AND i.status != 2 AND i.status != 7";
+                $sql = "SELECT incidents.id, incidents.title, incidents.status, incidents.servicelevel, incidents.maintenanceid, incidents.priority, contacts.forenames, contacts.surname, contacts.siteid ";
+                $sql .= "FROM incidents, contacts ";
+                $sql .= "WHERE incidents.contact = contacts.id AND contacts.siteid = {$obj->id} ";
+                $sql .= "AND incidents.status != 2 AND incidents.status != 7 ";
 
-                $lsql = "SELECT name FROM `{$dbSites}` WHERE id = {$obj->id}";
+                $lsql = "SELECT name FROM sites WHERE id = {$obj->id}";
                 $lresult = mysql_query($lsql);
                 if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
                 $lobj = mysql_fetch_object($lresult);
                 echo "<tr><th colspan='3'>{$lobj->name} ({$strSite})</th></tr>";
                 break;
             case '1': //contact
-                $sql = "SELECT i.id, i.title, i.status, i.servicelevel, i.maintenanceid, i.priority, c.forenames, c.surname, c.siteid ";
-                $sql .= "FROM `{$dbIncidents}` AS i, `{$dbContacts}` AS c ";
-                $sql .= "WHERE i.contact = c.id AND i.contact = {$obj->id} ";
-                $sql .= "AND i.status != 2 AND i.status != 7";
+                $sql = "SELECT incidents.id, incidents.title, incidents.status, incidents.servicelevel, incidents.maintenanceid, incidents.priority, contacts.forenames, contacts.surname, contacts.siteid ";
+                $sql .= "FROM incidents, contacts ";
+                $sql .= "WHERE incidents.contact = contacts.id AND incidents.contact = {$obj->id} ";
+                $sql .= "AND incidents.status != 2 AND incidents.status != 7 ";
 
-                $lsql = "SELECT forenames, surname FROM `{$dbContacts}` WHERE id = {$obj->id}";
+                $lsql = "SELECT forenames, surname FROM contacts WHERE id = {$obj->id}";
                 $lresult = mysql_query($lsql);
                 if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
                 $lobj = mysql_fetch_object($lresult);
                 echo "<tr><th colspan='3'>{$lobj->forenames} {$lobj->surname} ({$strContact})</th></tr>";
                 break;
             case '2': //engineer
-                $sql = "SELECT i.id, i.title, i.status, i.servicelevel, i.maintenanceid, i.priority, c.forenames, c.surname, c.siteid ";
-                $sql .= "FROM `{$dbIncidents}` AS i, `{$dbContacts}` AS c ";
-                $sql .= "WHERE i.contact = c.id AND (i.owner = {$obj->id} OR i.towner = {$obj->id}) ";
-                $sql .= "AND i.status != 2 AND i.status != 7";
+                $sql = "SELECT incidents.id, incidents.title, incidents.status, incidents.servicelevel, incidents.maintenanceid, incidents.priority, contacts.forenames, contacts.surname, contacts.siteid ";
+                $sql .= "FROM incidents, contacts ";
+                $sql .= "WHERE incidents.contact = contacts.id AND (incidents.owner = {$obj->id} OR incidents.towner = {$obj->id}) ";
+                $sql .= "AND incidents.status != 2 AND incidents.status != 7 ";
 
-                $lsql = "SELECT realname FROM `{$dbUsers}` WHERE id = {$obj->id}";
+                $lsql = "SELECT realname FROM users WHERE id = {$obj->id}";
                 $lresult = mysql_query($lsql);
                 if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
                 $lobj = mysql_fetch_object($lresult);
@@ -110,9 +122,9 @@ if (mysql_num_rows($result) > 0)
 
                 break;
             case '3': //incident
-                $sql = "SELECT i.id, i.title, i.status, i.servicelevel, i.maintenanceid, i.priority ";
-                $sql .= "FROM `{$dbIncidents}` AS i ";
-                $sql .= "WHERE i.id = {$obj->id} ";
+                $sql = "SELECT incidents.id, incidents.title, incidents.status, incidents.servicelevel, incidents.maintenanceid, incidents.priority ";
+                $sql .= "FROM incidents ";
+                $sql .= "WHERE incidents.id = {$obj->id} ";
                 //$sql .= "AND incidents.status != 2 AND incidents.status != 7";
                 break;
             default:
@@ -121,12 +133,30 @@ if (mysql_num_rows($result) > 0)
 
         if (!empty($sql))
         {
+            
+            switch ($queue)
+            {
+                case 1: // awaiting action
+                    $sql .= "AND ((timeofnextaction > 0 AND timeofnextaction < $now) OR ";
+                    $sql .= "(IF ((status >= 5 AND status <=8), ($now - lastupdated) > ({$CONFIG['regular_contact_days']} * 86400), 1=2 ) ";  // awaiting
+                    $sql .= "OR IF (status='1' OR status='3' OR status='4', 1=1 , 1=2) ";  // active, research, left message - show all
+                    $sql .= ") AND timeofnextaction < $now ) ";
+                    break;
+                case 3: // All Open
+                    $sql .= "AND status!='2' ";
+                    break;
+                case 5: // ALL
+                default:
+                    break;  
+            }
+            
             $iresult = mysql_query($sql);
             if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
-            if (mysql_num_rows($iresult) > 0)
+            if(mysql_num_rows($iresult) > 0)
             {
-                if ($obj->type == 3 AND !$header_printed)
+                if($obj->type == 3 AND !$header_printed)
                 {
+                    echo "<tr><th colspan='3'>{$strIncidents}</th></tr>";
                     echo "<tr>";
                     echo colheader('id', $GLOBALS['strID']);
                     echo colheader('title', $GLOBALS['strTitle']);
@@ -135,7 +165,7 @@ if (mysql_num_rows($result) > 0)
                     echo "</tr>\n";
                     $header_printed = TRUE;
                 }
-                else if ($obj->type != 3)
+                else if($obj->type != 3)
                 {
                     echo "<tr>";
                     echo colheader('id', $GLOBALS['strID']);
@@ -162,9 +192,21 @@ if (mysql_num_rows($result) > 0)
                     else $shade='shade1';
                 }
             }
-            else echo "<tr><td colspan='3'>{$GLOBALS['strNoOpenIncidents']}</td></tr>\n";
+            else
+            {
+                if($obj->type == 3 AND !$header_printed)
+                {
+                    echo "<tr><th colspan='3'>{$strIncidents}</th></tr>";
+                    echo "<tr><td colspan='3'>{$GLOBALS['strNoIncidents']}</td></tr>\n";
+                    $header_printed = TRUE;
+                }
+                else if($obj->type != 3)
+                {
+                    echo "<tr><td colspan='3'>{$GLOBALS['strNoIncidents']}</td></tr>\n";
+                }
+            }
         }
-        if ($obj->type == 3 AND !$header_printed)
+        if($obj->type == 3 AND !$header_printed)
         {
             echo "</table>\n";
         }
