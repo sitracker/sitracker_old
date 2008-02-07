e85f061729b53567928e469f38d57c180a144150
Paul Heaney <paulheaney@users.sourceforge.net>
2008-02-07 22:17:19 +0000

    * In this case we want time waiting on the customer

--- htdocs/incident/sla.inc.php
+++ htdocs/incident/sla.inc.php
@@ -69,9 +69,13 @@ while($row = mysql_fetch_object($result))
 {
     $updatearray[$row->currentstatus]['name'] = $row->name;
     if($last == -1)
+    {
         $updatearray[$row->currentstatus]['time'] = 0;
+    }
     else
-        $updatearray[$laststatus]['time'] += 60 * calculate_incident_working_time($row->incidentid, $last, $row->timestamp);
+    {
+        $updatearray[$laststatus]['time'] += 60 * calculate_working_time($row->incidentid, $last, $row->timestamp);
+    }
 
     $laststatus = $row->currentstatus;
     $last = $row->timestamp;
