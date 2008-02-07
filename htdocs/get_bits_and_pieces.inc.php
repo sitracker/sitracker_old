e1f9a0663f8cd17976531e732733092b487cf4f4
Paul Heaney <paulheaney@users.sourceforge.net>
2008-02-07 21:07:56 +0000

    * Opps last commit wont work without this

---
+++ htdocs/get_bits_and_pieces.inc.php
@@ -0,0 +1,58 @@
+<?
+// get_bits_and_pieces.inc.php - Page to get items to be displayed else where
+//
+// SiT (Support Incident Tracker) - Support call tracking system
+// Copyright (C) 2000-2006 Salford Software Ltd.
+//
+// This software may be used and distributed according to the terms
+// of the GNU General Public License, incorporated herein by reference.
+//
+// Author: Paul Heaney <paulheaney[at]users.sourceforge.net>
+
+
+@include('set_include_path.inc.php');
+$permission=0; // not required
+require('db_connect.inc.php');
+require('functions.inc.php');
+
+// This page requires authentication
+require('auth.inc.php');
+
+$toget = cleanvar($_REQUEST['toget']);
+
+if (!empty($toget))
+{
+    switch ($toget)
+    {
+        case 'slas':
+            $sql = "SELECT DISTINCT tag FROM servicelevels";
+            $result = mysql_query($sql);
+            if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
+            while ($obj = mysql_fetch_object($result))
+            {
+                echo "<option value='{$obj->tag}'>{$obj->tag}</option>";
+            }
+            break;
+        case 'products':
+            $sql = "SELECT id, name FROM products ORDER BY name ASC";
+            $result = mysql_query($sql);
+            if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
+            while ($obj = mysql_fetch_object($result))
+            {
+                echo "<option value='{$obj->id}'>{$obj->name}</option>";
+            }
+            break;
+        case 'skills':
+            $sql = "SELECT id, name FROM software ORDER BY name ASC";
+            $result = mysql_query($sql);
+            if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
+            while ($obj = mysql_fetch_object($result))
+            {
+                echo "<option value='{$obj->id}'>{$obj->name}</option>";
+            }
+            break;
+    }
+    
+}
+
+?>
