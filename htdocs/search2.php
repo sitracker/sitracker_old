1aa5285c1c98b3071b414ee6fdbaf72501ca1a6b
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2008-03-31 20:49:20 +0000

    beginning of new search to work on

---
+++ htdocs/search2.php
@@ -0,0 +1,45 @@
+<?php
+// search2.php - New search
+//
+// SiT (Support Incident Tracker) - Support call tracking system
+// Copyright (C) 2000-2008 Salford Software Ltd. and Contributors
+//
+// This software may be used and distributed according to the terms
+// of the GNU General Public License, incorporated herein by reference.
+//
+
+// Author: Kieran Hogg <kieran_hogg[at]users.sourceforge.net>
+
+@include ('set_include_path.inc.php');
+require ('db_connect.inc.php');
+require ('functions.inc.php');
+
+// This page requires authentication
+require ('auth.inc.php');
+
+include ('htmlheader.inc.php');
+
+echo "<h2>{$strSearch}</h2>";
+if(isset($_GET['q']))
+{
+    $search = cleanvar($_GET['q']);
+    $sql = "SELECT * FROM updates, incidents ";
+    $sql .= "WHERE MATCH (bodytext, title) against ('{$search}')";
+    $result = mysql_query($sql);
+    
+    while($row = mysql_fetch_object($result))
+    {
+        echo "<p>{$row->id} - {$row->bodytext}</p>";
+    }
+}
+else
+{
+    echo "<input></input>";
+    echo "<input type='submit' />";
+}
+
+
+
+include ('htmlfooter.inc.php');
+
+?>
