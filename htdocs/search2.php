46f1617ecafa54d35297527b8590a5c0c1c2d30c
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2008-04-01 10:06:00 +0000

    slightly more working search

--- htdocs/search2.php
+++ htdocs/search2.php
@@ -17,25 +17,133 @@ require ('functions.inc.php');
 // This page requires authentication
 require ('auth.inc.php');
 
+$resultsperpage = 20;
+if(!isset($_GET['start']))
+   $start = 1;
+else
+    $start = $_GET['start'];
+$end = $start + $resultsperpage;
+
+function search_highlight($x,$var)
+{
+    //$x is the string, $var is the text to be highlighted
+    $x = strip_tags($x);
+    $x = str_replace("\n", '', $x);
+    // Trim the string to a reasonable length
+    $pos1=stripos($x,$var);
+    if ($pos1===FALSE) $pos1=0;
+    if ($pos1>30) $pos1-=25;
+    $pos2=strlen($var)+70;
+    $x = substr($x,$pos1,$pos2);
+
+    if ($var != "")
+    {
+        $xtemp = "";
+        $i=0;
+
+        while ($i < strlen($x))
+        {
+            if ((($i + strlen($var)) <= strlen($x)) && (strcasecmp($var, substr($x, $i, strlen($var))) == 0))
+            {
+                $xtemp .= "<span style='background: #FFFACD; font-weight: bolder;'>" . substr($x, $i , strlen($var)) . "</span>";
+                $i += strlen($var);
+            }
+            else
+            {
+                $xtemp .= $x{$i};
+                $i++;
+            }
+        }
+        $x = $xtemp;
+    }
+    return $x;
+}
+
+
 include ('htmlheader.inc.php');
 
-echo "<h2>{$strSearch}</h2>";
+echo "<h2><img src='{$CONFIG['application_webpath']}images/icons/{$iconset}/32x32/search.png' width='32' height='32' alt='' /> {$strSearch} {$CONFIG['application_shortname']}</h2>";
+
 if(isset($_GET['q']))
 {
     $search = cleanvar($_GET['q']);
-    $sql = "SELECT * FROM updates ";
-    $sql .= "WHERE MATCH (bodytext) against ('{$search}')";
-    $result = mysql_query($sql);
+    $domain = strtolower($_GET['domain']);
     
+    $sql = "SELECT *,incidentid AS id FROM updates ";
+    $sql .= "WHERE MATCH (bodytext) against ('{$search}') GROUP BY incidentid LIMIT {$start}, {$end}";
+    echo $sql;
+    $result = mysql_query($sql);
+    $results = mysql_num_rows($result);
+    
+    echo "<strong>Display results {$start} to ";
+    if(($start + $resultsperpage) > $results)
+        echo $results;
+    else
+        echo $start + $resultsperpage;
+        
+    echo " of $results</strong>";
+    
+    echo "<table align='center' width='60%'>";
+    echo "<tr>".colheader(id, $strID, $sort, $order, $filter);
+    echo colheader(result, $strResult, $sort, $order, $filter);
+    echo colheader(score, $strScore, $sort, $order, $filter);
+    echo colheader(date, $strDate, $sort, $order, $filter);
+
+
     while($row = mysql_fetch_object($result))
     {
-        echo "<p>{$row->id} - {$row->bodytext}</p>";
+        switch ($domain)
+        {
+            case 'incidents':
+                $url = "javascript:incident_details_window('{$row->id}', 'incident{$row->id}')";
+                break;
+            case 'contact':
+                $url = "contact_details.php?id={$row->id}";
+                break;
+            case 'site':
+                $url = "site_details.php?id={$row->id}";
+                break;
+            case 'contract':
+                $url = "contract_details.php?id={$row->id}";
+                break;
+            case 'kb':
+                $url = "kb_view_article.php?id={$row->id}";
+                break;
+            default:
+                $url = "javascript:alert('nothing to link to');";
+        }
+        echo "<tr><td><a href=\"{$url}\">{$row->id}</a></td><td>".search_highlight($row->bodytext, $search)."</td>";
+        echo "<td></td><td></td></tr>";
     }
+    
+    echo "</table>";
 }
 else
 {
-    echo "<input></input>";
-    echo "<input type='submit' />";
+    $search_string = $_REQUEST['q'];
+    $search_domain = cleanvar($_REQUEST['domain']);
+    if (empty($search_domain) OR strtolower($search_domain)=='all') $domain='incidents';
+    $sort = cleanvar($_REQUEST['sort']);
+    if (empty($sort)) $sort='date';
+    $order = cleanvar($_REQUEST['order']);
+    if (empty($order)) $order='d';
+
+    echo "<form action='{$_SERVER['PHP_SELF']}' method='get'>";
+    echo "<table align='center'>";
+    echo "<tr><th>";
+    echo "{$strSearch} ";
+    $domains=array('incidents'=> $strIncidents, 'customers' => $strCustomers, 'maintenance' => $strContracts, 'knowledgebase' => $strKnowledgeBase);
+    echo array_drop_down($domains, 'domain', $search_domain);
+    echo " {$strFor}:";
+    echo "</th>";
+    echo "<td>";
+    echo "<input maxlength='100' name='q' size='35' type='text' value='".strip_tags(urldecode($search_string))."' /> ";
+    echo "(<a href='advanced_search_incidents.php'>{$strAdvanced}</a> | <a href='view_tags.php'>{$strTagCloud}</a>)";
+    echo "</td>";
+    echo "</tr>\n";
+    echo "</table>\n";
+    echo "<p><input type='submit' value='{$strSearch}' /></p>";
+    echo "</form>";
 }
 
 
