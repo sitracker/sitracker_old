d50bc789cec8d32d05ceef2b9e88166ce04e59c8
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2008-04-03 22:25:11 +0000

    fix display for no expiry date

--- htdocs/site_details.php
+++ htdocs/site_details.php
@@ -277,7 +277,12 @@ if (user_permission($sit[2],19)) // View contracts
             }
 
             echo "</td>";
-            echo "<td class='{$class}'>".ldate($CONFIG['dateformat_date'], $results['expirydate'])."</td>";
+            echo "<td class='{$class}'>";
+            if($results['expirydate'])
+                echo $strUnlimited;
+            else	    
+                echo ldate($CONFIG['dateformat_date'], $results['expirydate']);
+            echo "</td>";
             echo "<td class='{$class}'>{$results['admincontactsforenames']}  {$results['admincontactssurname']}</td>";
             echo "<td class='{$class}'>";
             if ($results['maintnotes'] == '')
