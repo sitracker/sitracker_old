a0ef2a2301a3fe94712e6d0d5a75b86638729b28
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2008-04-03 22:26:18 +0000

    compare it to something...

--- htdocs/site_details.php
+++ htdocs/site_details.php
@@ -278,7 +278,7 @@ if (user_permission($sit[2],19)) // View contracts
 
             echo "</td>";
             echo "<td class='{$class}'>";
-            if($results['expirydate'])
+            if($results['expirydate'] == -1)
                 echo $strUnlimited;
             else	    
                 echo ldate($CONFIG['dateformat_date'], $results['expirydate']);
