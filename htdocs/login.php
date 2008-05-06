f821252f5093adca71a3ff75e6e8633e1f99e3fa
Ivan Lucas <ivanlucas@users.sourceforge.net>
2008-05-06 11:21:05 +0000

    Fix warning about "session side-effect" session.bug_compat_42, that occurred when the user setting var_i18n was null, which it is by default for new users

--- htdocs/login.php
+++ htdocs/login.php
@@ -87,7 +87,7 @@ elseif (authenticate($username, $password) == 1)
 
     if ($user->var_i18n != $CONFIG['default_i18n'] AND $_SESSION['lang']=='')
     {
-        $_SESSION['lang'] = $user->var_i18n;
+        $_SESSION['lang'] = is_null($user->var_i18n) ? '' : $user->var_i18n;
     }
 
     // Make an array full of users permissions
