e98647892ae63303aa4fbfd224b7fbb2920f9dae
Ivan Lucas <ivanlucas@users.sourceforge.net>
2008-05-20 22:02:27 +0000

    Re-write of get_and_display() js function, using prototype, fixes problem with Feeds etc. dashlets

--- htdocs/webtrack.js
+++ htdocs/webtrack.js
@@ -79,66 +79,22 @@ function pausecomp(millis)
     } while (curDate-date < millis);
 }
 
+
 function get_and_display(page, component)
 {
-    pausecomp(25);
-
-    var xmlhttp=false;
-    /*@cc_on @*/
-    /*@if (@_jscript_version >= 5)
-    // JScript gives us Conditional compilation, we can cope with old IE versions.
-    // and security blocked creation of the objects.
-    try {
-    xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
-    } catch (e) {
-    try {
-    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    } catch (E) {
-    xmlhttp = false;
-    }
-    }
-    @end @*/
-    if (!xmlhttp && typeof XMLHttpRequest!='undefined')
+    new Ajax.Request(application_webpath + page + '?rand=' + get_random(),
     {
-        try
-        {
-            xmlhttp = new XMLHttpRequest();
-        }
-        catch (e)
-        {
-            xmlhttp=false;
-        }
-    }
-
-    if (!xmlhttp && window.createRequest)
-    {
-        try
-        {
-            xmlhttp = window.createRequest();
-        }
-        catch (e)
-        {
-            xmlhttp=false;
-        }
-    }
-
-    xmlhttp.open("GET", page, true);
-
-    var rsswindow = byId(component);
-
-    xmlhttp.onreadystatechange=function()
-    {
-        //remove this in the future after testing
-        if (xmlhttp.readyState==4)
-        {
-            if (xmlhttp.responseText != "")
+        method:'get',
+            onSuccess: function(transport)
             {
-                //alert(xmlhttp.responseText);
-                rsswindow.innerHTML = xmlhttp.responseText;
-            }
-        }
-    }
-    xmlhttp.send(null);
+                var response = transport.responseText || "no response text";
+                if (transport.responseText)
+                {
+                    $(component).innerHTML = transport.responseText;
+                }
+            },
+            onFailure: function(){ $(component).innerHTML = 'Error'; }
+    });
 }
 
 
