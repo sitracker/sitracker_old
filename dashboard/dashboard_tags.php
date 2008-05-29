64c3da849cba68e0fa6ac44693ca2897f6a4f3bb
Ivan Lucas <ivanlucas@users.sourceforge.net>
2008-05-29 15:42:30 +0000

    Make tags dashlet use new function

--- dashboard/dashboard_tags.php
+++ dashboard/dashboard_tags.php
@@ -13,13 +13,10 @@ $dashboard_tags_version = 1;
 function dashboard_tags($dashletid)
 {
     global $CONFIG, $iconset;
-    echo "<div class='windowbox' id='$row-$dashboardid'>";
-    echo "<div class='windowtitle'><a href='view_tags.php'>".icon('tag', 16);
-    echo " {$GLOBALS['strTags']}</a></div>";
-    echo "<div class='window'>";
-    echo show_tag_cloud();
-    echo "</div>";
-    echo "</div>";
+
+    $content = show_tag_cloud();
+
+    echo dashlet('tags', $dashletid, icon('tag', 16), $GLOBALS['strTags'], '', $content);
 }
 
 ?>
