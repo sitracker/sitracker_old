6c2ed5686e72dc535a70418bca89b86b0e0d076c
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2008-06-27 15:09:32 +0000

    remove second OR

--- htdocs/users.php
+++ htdocs/users.php
@@ -44,7 +44,7 @@ if ($numgroups >= 1 AND $filtergroup == '0')
 {
     $sql .= "AND (groupid='0' OR groupid='' OR groupid IS NULL) ";
 }
-elseif ($numgroups < 1 OR $filtergroup=='all' OR OR $filtergroup=='allonline')
+elseif ($numgroups < 1 OR $filtergroup=='all' OR $filtergroup=='allonline')
 {
     $sql .= "AND 1=1 ";
 }
