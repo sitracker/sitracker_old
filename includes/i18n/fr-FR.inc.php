aaccc4e67e908716ab3031d4aebaab7bb6a07d3a
Ivan Lucas <ivanlucas@users.sourceforge.net>
2008-07-04 19:35:31 +0000

    Updated French (fr-FR) translation by Gabriel Riam

--- includes/i18n/fr-FR.inc.php
+++ includes/i18n/fr-FR.inc.php
@@ -1,23 +1,39 @@
 <?php
-// SiT! Language File - French (fr-FR)
+// SiT! Language File - French (fr-FR) by Gabriel RIAM
 
 $languagestring = 'French (fr-FR)';
 $i18ncharset = 'UTF-8';
 
+$str0MeansUnlimited = '0 = illimité';
+
+$str1Day = '1 Jour';
+$str1Minute = '1 Minute';
+$str1Month = '1 Mois';
+$str1Hour = '1 Heure';
+
+$strAbout = 'à propos';
 $strAbsentSick = 'Malade';
 $strAccepting = 'Acceptant';
 $strActionNeeded = 'Action Nécessaire';
 $strActionPlan = 'Plan d\'Action';
 $strActionPlanBy = 'Plan d\'Action par %s';
 $strActive = 'Actif';
+$strActiveContracts = 'Contrats Actif';
+$strActivities = 'Activités';
 $strActivity = 'Activité';
+$strActivityAdded = 'Activité ajouté';
 $strAdd = 'Ajouter';
 $strAddContact = 'Ajouter Contact';
 $strAddContract = 'Ajouter Accord';
+$strAddContractConfirm = 'etes vous sur de vouloir ajouter ce contrat ?';
 $strAdded = 'Ajouté';
+$strAddedSuccessfully = 'Ajouté avec succès';
 $strAddFeedbackForm = 'Ajouter Document de Commentaire';
 $strAddIncident = 'Ajouter Évènement';
+$strAdditionalInfo = 'Information Complémentaire';
+$strAddKBArticle = 'Ajouter un article à la Base de Connaissance';
 $strAddLink = 'Ajouter Lien';
+$strAddNew = 'Ajouter Nouveau';
 $strAddNote = 'Ajouter Note';
 $strAddProduct = 'Ajouter Produit';
 $strAddReseller = 'Ajouter Revendeur';
@@ -32,16 +48,21 @@ $strAddTemplate = 'Ajouter Formulaire';
 $strAddUser = 'Ajouter Utilisateur';
 $strAddVendor = 'Ajouter Vendeur';
 $strAdminContact = 'Contact d\'Administration';
+$strAdministratorsOnly = 'Administrateur uniquement';
 $strAdvanced = 'Avancé';
+$strAfternoon = 'Après-midi';
 $strAge = 'Âge';
 $strAgo = 'Il y a %s';
 $strAll = 'Tous';
 $strAllClosed = 'Tous Terminés';
+$strAllDay = 'tous les jours';
 $strAllOpen = 'Tous Ouverts';
 $strAlternative = 'Alternative';
+$strAnswer = 'Réponse';
 $strApproveHolidays = 'Autoriser Congé';
 $strAprAbbr = 'Avr';
 $strApril = 'Avril';
+$strArticlesPublishedRecently = 'Articles publiés récement';
 $strAssign = 'Assigner';
 $strAssignTo = 'Assigner À';
 $strAtLunch = 'Au Déjeuner';
@@ -58,15 +79,21 @@ $strBrowseFeedback = 'Parcourir commentaires';
 $strBrowseFeedbackForms = 'Parcourir Documents de Commentaire';
 $strBrowseKB = 'Parcourir Base de Connaissance';
 
+$strChangePassword = 'Changer de mot de passe';
 $strChooseLanguage = 'Veuillez choisir votre langue';
 $strCity = 'Ville';
+$strCollapseData = 'Scinder les Données';
+$strConfirmNewPassword = 'Confirmer le Nouveau Mot de Passe';
 $strContact = 'Contact';
 $strContinue = 'Continuer';
+$strContactDetails = 'Détails du Contact';
 $strContacts = 'Contacts';
 $strContactsMulti = '%d Contacts';
 $strContract = 'Accord';
 $strContracts = 'Accords';
 $strControlPanel = 'Panneau de Contrôle';
+$strCompleted = 'Completé';
+$strCompletion = 'Progrès';
 $strCountry = 'Pays';
 $strCurrentlyOpen = 'actuellement ouvert';
 $strCustomer = 'Client';
@@ -81,7 +108,14 @@ $strDecember = 'Décembre';
 $strDelete = 'Supprimer';
 $strDeleted = 'Supprimé';
 $strDeleteTask = 'Supprimer Tâche';
+$strDisplayPreferences = 'Préférences d\'affichage';
+$strDueDate = 'date d\'échéance';
 
+$strEmailSettings = 'Réglages Email';
+$strEmailSignature = 'Signature Email';
+$strEmailSignatureTip = 'inséré automatiquement en bas de vos Emails envoyés';
+$strEndDate = 'Date de Fin';
+$strEndOfLife = 'Fin de Vie';
 $strEnglish = 'Anglais';
 $strExpiryDate = 'Date d\'Expiration';
 
@@ -91,30 +125,58 @@ $strForgottenDetails = 'Mot de passe oublié?';
 $strFriday = 'Vendredi';
 
 $strGo = 'Aller';
+$strGroupMembership = 'Membre du groupe';
 
 $strHelp = 'Aide';
 $strHelpChar = '?';
 $strHelpToTranslate = 'Cette page se vise à aider les traducteurs pour traduire SiT !';
+$strHigh = 'Haute';
+$strHighPriority = 'Priorité Haute';
+
+$strIncidentLogOrder = 'Ordre des rapports d\'incidents';
+$strIncidentRefresh = 'Incidents rafraichie toutes les';
+$strIncidentUpdatesPerPage = 'Incidents mis à jour par page';
+$strInterfaceStyle = 'Style de l\'interface (Thème)';
 
 $strJanAbbr = 'Jan';
 $strJanuary = 'Janvier';
+$strJobTitle = 'Titre du poste';
 $strJulAbbr = 'juil';
 $strJuly = 'Juillet';
+$strJumpTo = 'Aller à';
 $strJunAbbr = 'juin';
 $strJune = 'Juin';
 
+$strKBSelectSectionsText = 'Vous pouvez activer les sections suivantes en cochant la case correspondante';
+$strKeywords = 'Mots Clés';
 $strKnowledgeBase = 'Base de Connaissance';
 
 $strLanguage = 'Langue';
+$strListSkills = 'Liste d\'Aptitudes';
 $strLogged = 'Connecté';
+$strLoggedInAs = 'Connecté en tant que: %s';
 $strLogIn = 'Connexion';
 $strLogin = 'Connexion';
 $strLogout = 'Déconnexion';
+$strLow = 'Bas';
+$strLowPriority = 'Priorité Basse';
 
+$strMaintenance = 'Maintenance';
+$strManageDashboardComponents = 'Gérer les composants du tableau de bord';
+$strManageUsers = 'Gérer les Utilisateurs';
+$strManageYourDashboard = 'Gérer votre Tableau de Bord';
+$strMandatoryMarked = 'les champs obligatoire sont marqués %s';
 $strMarAbbr = 'Mar';
 $strMarch = 'Mars';
+$strMarkComplete = 'Marquer comme Complet';
 $strMayAbbr = 'Mai';
 $strMay = 'Mai';
+$strMedium = 'Moyenne';
+$strMediumPriority = 'Priorité Moyenne';
+$strMessage = 'Message';
+$strMessageTip = 'e.g. &quot;En france jusqu\'au mardi 22&quot;<br />Afficher sur la page \'Voir les utilisateurs\'.';
+$strMinutes = 'Minutes';
+$strMobile = 'Mobile';
 $strMonday = 'Lundi';
 $strMyDashboard = 'Mon Panneau de Contrôle';
 $strMyDetails = 'Mes Détails Personnels';
@@ -123,37 +185,67 @@ $strMyProfile = 'Mon Profil';
 $strMySkills = 'Mes Habiletés';
 $strMySubstitutes = 'Mes Remplaçants';
 
+$strName = 'Nom';
+$strNewKBArticle = 'Nouvelle Article';
+$strNewPassword = 'Nouveau Mot de Passe';
+$strNewUser = 'Nouvelle Utilisateur';
 $strNo = 'Non';
 $strNoIncidents = 'Pas d\'Incident';
+$strNoKBArticles = 'Pas d\'articles de la Base de Connaissance';
+$strNoRecords = 'Pas d\'Enregistrements';
+$strNoTasks = 'Pas de Taches';
+$strNotSet = 'Non Attribué';
 $strNovAbbr = 'Nov';
 $strNovember = 'Novembre';
 $strNow = 'Actuellement';
 
 $strOctAbbr = 'Oct';
 $strOctober = 'Octobre';
+$strOldPassword = 'Ancien Mot de Passe';
 $strOnHoliday = 'En Congé';
 
 $strPassword = 'Mot de passe';
+$strPleaseWaitRedirect = 'Veuillez attendre pendant que vous etes redirigez Svp';
 $strPostcode = 'Code Postale';
+$strPriority = 'Priorité';
+$strPrivacy = 'Confidentialité';
+$strPrivate = 'Privé';
 $strPublished = 'Public';
 
+$strQualificationsTip = 'Entrez chacune de vos qualifications professionnelle séparé par une virgule';
+
 $strRandomTip = 'Petit Conseil';
+$strRealName = 'Nom de famille';
+$strRemove = 'Supprimer';
 $strReports = 'Rapports';
+$strReset = 'Annuler';
+$strResults = 'Résultats';
 
 $strSaturday = 'Samedi';
+$strSave = 'Sauvegarder';
 $strSearch = 'Rechercher';
 $strSearchAgain = 'Rechercher de Nouveau';
-$strSepAbbr = 'Sep';
+$strSeconds = 'Secondes';
+$strSep = 'Sep';
 $strSeptember = 'Septembre';
+$strSetYourStatus = 'Indiquez votre Statut';
+$strShowAll = 'Tous Afficher';
+$strShowMine = 'Afficher mes tâches';
+$strStartDate = 'Date de Début';
+$strStatus = 'Statut';
+$strSummary = 'Sommaire';
 $strSunday = 'Dimanche';
 $strSupport = 'Assistance';
+$strSymptoms = 'Symptomes';
 
 $strTask = 'Tâche';
 $strTasks = 'Tâches';
+$strTelephone = 'Téléphone';
 $strThursday = 'Jeudi';
 $strTime = 'L\'heure';
 $strTitle = 'Titre';
 $strTo = 'À';
+$strToChangePassword = 'Pour changer de Mot de Passe, veuillez entrer l\'ancien mot de passe , puis le nouveau mot de passe 2 fois';
 $strTodaysStats = 'Statistiques Quotidiennes';
 $strToday = 'Aujourd\'hui';
 $strTranslate = 'Traduire';
@@ -163,12 +255,30 @@ $strTuesday = 'Mardi';
 $strUpdated = 'Mise à Jour';
 $strUnknown = 'Inconnu';
 $strUsed = 'Utilisé';
+$strUser = 'Utilisateur';
 $strUsername = 'Nom d\'utilisateur';
 $strUsers = 'Utilisateurs';
 $strUsersTasks = 'Tâches de %s';
 
+$strValue = 'Valeur';
+$strVariable = 'Variable';
+$strVendor = 'Vendeur';
+$strVersion = 'Version';
+$strView = 'Vue';
+$strViewResponse = 'Afficher la Réponse';
+$strViewTask = 'Afficher la Tache';
+$strViewTasks = 'Afficher les Taches';
+$strViewUsers = 'Afficher les Utilisateurs';
+$strVisibleToCustomer = 'Visible par les Clients';
+$strVisibility = 'Visibilité';
+
+$strWaiting = 'En Attente';
+$strWarning = 'Avertissement';
 $strWednesday = 'Mercredi';
 $strWelcome = 'Bienvenue';
+$strWhosAwayToday = 'Qui est Absent Aujourd\'hui';
+$strWorkaround = 'Travail effectué';
+$strWorkStatus = 'Statut de Travail';
 
 $strYes = 'Oui';
 $strYou = 'Vous';
