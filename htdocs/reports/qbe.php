94c6c8aad20b93ace768ef109d51d8533d8cb4bb
Ivan Lucas <ivanlucas@users.sourceforge.net>
2008-07-18 15:04:12 +0000

    Fix for Mantis 121 QBE report can reveal sensitive information.  QBE report now requires Administrate permission (22) as well as reports permission (67)

--- htdocs/reports/qbe.php
+++ htdocs/reports/qbe.php
@@ -11,7 +11,7 @@
 // Author: Ivan Lucas <ivanlucas[at]users.sourceforge.net>
 
 @include ('../set_include_path.inc.php');
-$permission = 37; // Run Reports
+$permission = array(22, 67); // Administrate / Run Reports
 
 require ('db_connect.inc.php');
 require ('functions.inc.php');
