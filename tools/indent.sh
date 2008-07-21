2ec9870d408479d387f5e2e6d89c7894157f9527
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2008-07-21 13:59:22 +0000

    check if indent is intalled

--- tools/indent.sh
+++ tools/indent.sh
@@ -3,6 +3,10 @@ if [ -z $1 ]; then
 echo "usage: ./indent.sh file.php";
 exit
 fi
+if [ -z `which indent` ]; then
+echo "requires GNU indent";
+exit
+fi
 indent -bap -bbo -nbc -bl -bli0 -c33 -cd33 -ncdb -ci4 -cli0 -cp33 -cs -d0 -di1
 -nfc1 -nfca -hnl -i4 -ip0 -l75 -lp -npcs -nprs -npsl -saf -sai -saw -nsc -nsob
 -nss -nut $1
