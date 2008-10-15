6487742eaf63908c971e4f65de54b2cddf3887f0
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2008-10-15 13:17:11 +0000

    fix syntax errors

--- htdocs/auto.php
+++ htdocs/auto.php
@@ -667,22 +667,24 @@ function saction_CheckIncomingMail()
 
 function saction_CheckTasksDue()
 {
-    $sql = "SELECT interval FROM $GLOBALS['dbScheduler'] ";
+    $sql = "SELECT interval FROM {$GLOBALS['dbScheduler']} ";
     $sql .= "WHERE action = 'CheckTasksDue'";
     $result = mysql_query($sql);
-    $intervalobj = mysql_fetch_obj($result);
+    $intervalobj = mysql_fetch_object($result);
     
     // check the tasks due between now and in N minutes time,
     // where N is the time this action is run
     $format = "Y-m-d H:i:s";
     $startdue = date($format, $GLOBALS['now']);
     $enddue =  date($format, $GLOBALS['now'] + $intervalobj->interval);
-    $sql = "SELECT * FROM $GLOBALS['dbTasks'] ";
+    $sql = "SELECT * FROM {$GLOBALS['dbTasks']} ";
     $sql .= "WHERE duedate > {$startdue} AND duedate < {$enddue} ";
-    $result = mysql_query($sql);
-    while ($row = mysql_fetch_object($result))
+    if ($result = mysql_query($sql))
     {
-        trigger('TRIGGER_TASK_DUE', array('taskid' => $row->id));
+        while ($row = mysql_fetch_object($result))
+        {
+            trigger('TRIGGER_TASK_DUE', array('taskid' => $row->id));
+        }
     }
 }
 
