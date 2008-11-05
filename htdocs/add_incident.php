a3e50b3d82998a31cfcf3468976f508b01917f71
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2008-11-05 19:55:37 +0000

    revert remaining incident ID changes

--- htdocs/add_incident.php
+++ htdocs/add_incident.php
@@ -691,33 +691,14 @@ elseif ($action == 'assign')
                 $priority = $highestpriority;
             }
 
-	    // Find next incident number
-	    if ($CONFIG['incident_number_type'] == 2)
-	    {
-                $sql = "SELECT COUNT(id) AS count FROM `{$dbIncidents}` ";
-                $sql .= "WHERE opened > '$todayrecent'";
-                $result = mysql_query($sql);
-                $row = mysql_fetch_object($result);
-                $today_count = $row->count + 1;
-		$date = date("ymd");
-		$next_id = $date . $today_count;
-	    }
-	    else
-            {
-	    	$sql = "SELECT COUNT(id) as count FROM `{$dbIncidents}`";
-                $result = mysql_query($sql);
-                $row = mysql_fetch_object($result);
-                $next_id = $row->count + 1;
-	    }
-
-            $sql  = "INSERT INTO `{$dbIncidents}` (id, title, owner, contact, priority, servicelevel, status, type, maintenanceid, ";
+            $sql  = "INSERT INTO `{$dbIncidents}` (title, owner, contact, priority, servicelevel, status, type, maintenanceid, ";
             $sql .= "product, softwareid, productversion, productservicepacks, opened, lastupdated, timeofnextaction) ";
-            $sql .= "VALUES ('{$next_id}', '$incidenttitle', '".$sit[2]."', '$contactid', '$priority', '$servicelevel', '1', 'Support', '$maintid', ";
+            $sql .= "VALUES ('$incidenttitle', '".$sit[2]."', '$contactid', '$priority', '$servicelevel', '1', 'Support', '$maintid', ";
             $sql .= "'$productid', '$software', '$productversion', '$productservicepacks', '$now', '$now', '$timeofnextaction')";
             $result = mysql_query($sql);
             if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
 
-            $incidentid = $next_id;
+            $incidentid = mysql_insert_id();
             $_SESSION['incidentid'] = $incidentid;
 
             // Save productinfo if there is some
