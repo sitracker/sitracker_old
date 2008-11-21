b074e5ccd50d385bdbc5dbd57f97b395a7344f1a
Ivan Lucas <ivanlucas@users.sourceforge.net>
2008-11-21 16:06:51 +0000

    Prevent updating or closing of incidents not associated with your own contact record from portal

--- htdocs/portal/close.php
+++ htdocs/portal/close.php
@@ -18,39 +18,57 @@ $accesslevel = 'any';
 include 'portalauth.inc.php';
 include 'portalheader.inc.php';
 
-if (empty($_REQUEST['reason']))
-{
-    $id = $_REQUEST['id'];
-    echo "<h2>".icon('close', 32, $strClosureRequestForIncident);
-    echo " {$strClosureRequestForIncident} {$_REQUEST['id']}</h2>";
-    echo "<div id='update' align='center'><form action='{$_SERVER[PHP_SELF]}?page=close&amp;id={$id}' method='post'>";
-    echo "<p>{$strReason}:</p><textarea name='reason' cols='50' rows='10'></textarea><br />";
-    echo "<p><input type='submit' value=\"{$strRequestClosure}\" /></p></form></div>";
+// External vars
+$id = intval($_REQUEST['id']);
 
-    include 'htmlfooter.inc.php';
+// First check the portal user is allowed to access this incident
+$sql = "SELECT contact FROM `{$dbIncidents}` WHERE id = $id LIMIT 1";
+$result = mysql_query($sql);
+if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_WARNING);
+list($incidentcontact) = mysql_fetch_row($result);
+if ($incidentcontact == $_SESSION['contactid'])
+{
+    if (empty($_REQUEST['reason']))
+    {
+        $id = $_REQUEST['id'];
+        echo "<h2>".icon('close', 32, $strClosureRequestForIncident);
+        echo " {$strClosureRequestForIncident} {$_REQUEST['id']}</h2>";
+        echo "<div id='update' align='center'><form action='{$_SERVER[PHP_SELF]}?page=close&amp;id={$id}' method='post'>";
+        echo "<p>{$strReason}:</p><textarea name='reason' cols='50' rows='10'></textarea><br />";
+        echo "<p><input type='submit' value=\"{$strRequestClosure}\" /></p></form></div>";
+
+        include 'htmlfooter.inc.php';
+    }
+    else
+    {
+        $usersql = "SELECT forenames, surname FROM `{$dbContacts}` WHERE id={$_SESSION['contactid']}";
+        $result = mysql_query($usersql);
+        $user = mysql_fetch_object($result);
+        if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_WARNING);
+
+        // FIXME i18n ? In db ?
+        $reason = "Incident closure requested via the portal by [b]{$user->forenames} {$user->surname}[/b]\n\n";
+        $reason .= "<b>Reason:</b> {$_REQUEST['reason']}";
+        $sql = "INSERT into `{$dbUpdates}` (incidentid, userid, type, currentstatus, bodytext, timestamp, customervisibility) ";
+        $sql .= "VALUES('{$_REQUEST['id']}', '0', 'customerclosurerequest',  '1', '{$reason}',
+        '{$now}', 'show')";
+        mysql_query($sql);
+        if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
+
+        //set incident back to active
+        $sql = "UPDATE `{$dbIncidents}` SET status=1, lastupdated={$now} WHERE id={$_REQUEST['id']}";
+        mysql_query($sql);
+        if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
+
+
+        html_redirect("index.php");
+    }
 }
 else
 {
-    $usersql = "SELECT forenames, surname FROM `{$dbContacts}` WHERE id={$_SESSION['contactid']}";
-    $result = mysql_query($usersql);
-    $user = mysql_fetch_object($result);
-    if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_WARNING);
-
-    // FIXME i18n ? In db ?
-    $reason = "Incident closure requested via the portal by [b]{$user->forenames} {$user->surname}[/b]\n\n";
-    $reason .= "<b>Reason:</b> {$_REQUEST['reason']}";
-    $sql = "INSERT into `{$dbUpdates}` (incidentid, userid, type, currentstatus, bodytext, timestamp, customervisibility) ";
-    $sql .= "VALUES('{$_REQUEST['id']}', '0', 'customerclosurerequest',  '1', '{$reason}',
-    '{$now}', 'show')";
-    mysql_query($sql);
-    if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
-
-    //set incident back to active
-    $sql = "UPDATE `{$dbIncidents}` SET status=1, lastupdated={$now} WHERE id={$_REQUEST['id']}";
-    mysql_query($sql);
-    if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
-
-
-    html_redirect("index.php");
+    echo "<p class='warning'>$strNoPermission.</p>";
+    include ('htmlfooter.inc.php');
+    exit;
 }
+
 ?>
