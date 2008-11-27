e678adcfb0f32f4130d63b5381417b06ac78c448
Ivan Lucas <ivanlucas@users.sourceforge.net>
2008-11-27 15:52:27 +0000

    i18n and code tidy

--- htdocs/reopen_incident.php
+++ htdocs/reopen_incident.php
@@ -33,7 +33,8 @@ if (mysql_num_rows($result) > 0)
 
 // Find out whether the service level in use allows reopening
 $slsql = "SELECT allow_reopen FROM `{$dbServiceLevels}` ";
-$slsql .= "WHERE tag = '{$incident->servicelevel}' AND priority = '{$incident->priority}' LIMIT 1";
+$slsql .= "WHERE tag = '{$incident->servicelevel}' ";
+$slsql .= "AND priority = '{$incident->priority}' LIMIT 1";
 
 $slresult = mysql_query($slsql);
 if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
@@ -48,14 +49,16 @@ if ($allow_reopen == 'yes')
     {
         // No submit detected show update form
         $incident_title = incident_title($id);
-        $title = "{$strReopen}: ".$id . " - " . $incident_title;  // FIXME i18n
+        $title = "{$strReopen}: ".$id . " - " . $incident_title;
         include ('incident_html_top.inc.php');
 
         echo "<h2>{$strReopenIncident}</h2>";
         echo "<form action='{$_SERVER['PHP_SELF']}?id={$id}' method='post'>";
         echo "<table class='vertical'>";
-        echo "<tr><th>{$strUpdate}</th><td><textarea name='bodytext' rows='20' cols='60'></textarea></td></tr>";
-        echo "<tr><th>{$strStatus}</th><td>".incidentstatus_drop_down("newstatus", 1)."</td></tr>";
+        echo "<tr><th>{$strUpdate}</th><td><textarea name='bodytext' rows='20' ";
+        echo "cols='60'></textarea></td></tr>";
+        echo "<tr><th>{$strStatus}</th><td>".incidentstatus_drop_down("newstatus", 1);
+        echo "</td></tr>\n";
         echo "</table>";
         echo "<p><input name='submit' type='submit' value='{$strReopenIncident}' /></p>";
         echo "</form>";
@@ -66,20 +69,27 @@ if ($allow_reopen == 'yes')
         // Reopen the incident
         // update incident
         $time = time();
-        $sql = "UPDATE `{$dbIncidents}` SET status='$newstatus', lastupdated='$time', closed='0' WHERE id='$id' LIMIT 1";
+        $sql = "UPDATE `{$dbIncidents}` SET status='$newstatus', ";
+        $sql .= "lastupdated='$time', closed='0' WHERE id='$id' LIMIT 1";
         mysql_query($sql);
         if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
 
         // add update
-        $sql  = "INSERT INTO `{$dbUpdates}` (incidentid, userid, type, bodytext, timestamp) ";
+        $sql  = "INSERT INTO `{$dbUpdates}` (incidentid, userid, type, ";
+        $sql .= "bodytext, timestamp) ";
         $sql .= "VALUES ($id, $sit[2], 'reopening', '$bodytext', $time)";
         $result = mysql_query($sql);
         if (mysql_error()) trigger_error(mysql_error(),E_USER_ERROR);
 
-        // Insert the first SLA update for the reopened incident, this indicates the start of an sla period
-        // This insert could possibly be merged with another of the 'updates' records, but for now we keep it seperate for clarity
-        $sql  = "INSERT INTO `{$dbUpdates}` (incidentid, userid, type, timestamp, currentowner, currentstatus, customervisibility, sla, bodytext) ";
-        $sql .= "VALUES ('$id', '".$sit[2]."', 'slamet', '$now', '".$sit[2]."', '1', 'show', 'opened','The incident is open and awaiting action.')";
+        // Insert the first SLA update for the reopened incident, this indicates
+        // the start of an sla period
+        // This insert could possibly be merged with another of the 'updates'
+        // records, but for now we keep it seperate for clarity
+        $sql  = "INSERT INTO `{$dbUpdates}` (incidentid, userid, type, ";
+        $sql .= "timestamp, currentowner, currentstatus, customervisibility, ";
+        $sql .= "sla, bodytext) ";
+        $sql .= "VALUES ('$id', '".$sit[2]."', 'slamet', '$now', '".$sit[2];
+        $sql .= "', '1', 'show', 'opened','The incident is open and awaiting action.')"; // FIXME i18n
         mysql_query($sql);
         if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
 
