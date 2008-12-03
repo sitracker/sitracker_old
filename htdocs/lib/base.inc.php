7274e9097b4c1c758ef9b2a1e6d6c9c05f8a43a8
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2008-12-03 15:37:22 +0000

    move across a few more bits

--- htdocs/lib/base.inc.php
+++ htdocs/lib/base.inc.php
@@ -7,4 +7,81 @@
 // This software may be used and distributed according to the terms
 // of the GNU General Public License, incorporated herein by reference.
 
+//**** Begin constant/variable definitions ****//
+ 
+// Version number of the application, (numbers only)
+$application_version = '3.41';
+
+// Revision string, e.g. 'beta2' or 'svn' or ''
+$application_revision = 'svn';
+
+// Clean PHP_SELF server variable to avoid potential XSS security issue
+$_SERVER['PHP_SELF'] = substr($_SERVER['PHP_SELF'], 0,
+                            (strlen($_SERVER['PHP_SELF'])
+                            - @strlen($_SERVER['PATH_INFO'])));
+
+$fsdelim = DIRECTORY_SEPARATOR;
+
+// Time settings
+$now = time();
+// next 16 hours, based on reminders being run at midnight this is today
+$today = $now + (16 * 3600);
+$lastweek = $now - (7 * 86400); // the previous seven days
+$todayrecent = $now -(16 * 3600);  // past 16 hours
+
+$CONFIG['upload_max_filesize'] = return_bytes($CONFIG['upload_max_filesize']);
+
+// Set a string to be the full version number and revision of the application
+$application_version_string = trim("v{$application_version} {$application_revision}");
+
+//**** Begin internal functions ****//
+// Append SVN data for svn versions
+if ($application_revision == 'svn')
+{
+    // Add the svn revision number
+    preg_match('/([0-9]+)/','$LastChangedRevision: 4343 $',$revision);
+    $application_revision .= $revision[0];
+}
+
+// Report all PHP errors
+error_reporting(E_ALL);
+$oldeh = set_error_handler("sit_error_handler");
+
+// Decide which language to use and setup internationalisation
+require ('i18n/en-GB.inc.php');
+if ($CONFIG['default_i18n'] != 'en-GB')
+{
+    @include ("i18n/{$CONFIG['default_i18n']}.inc.php");
+}
+if (!empty($_SESSION['lang'])
+    AND $_SESSION['lang'] != $CONFIG['default_i18n'])
+{
+    include ("i18n/{$_SESSION['lang']}.inc.php");
+}
+ini_set('default_charset', $i18ncharset);
+
+
+//**** Begin functions ****//
+
+/**
+    * Strip slashes from an array
+    * @param $data an array
+    * @return An array with slashes stripped
+*/
+function stripslashes_array($data)
+{
+    if (is_array($data))
+    {
+        foreach ($data as $key => $value)
+        {
+            $data[$key] = stripslashes_array($value);
+        }
+        return $data;
+    }
+    else
+    {
+        return stripslashes($data);
+    }
+}
+
 ?>
--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -19,36 +19,14 @@
 
 include ('classes.inc.php');
 include ('ldap.inc.php');
-
-// Version number of the application, (numbers only)
-$application_version = '3.41';
-// Revision string, e.g. 'beta2' or 'svn' or ''
-$application_revision = 'svn';
+include ('../htdocs/lib/base.inc.php');
 
 $ldap_conn = "";
-
-// Append SVN data for svn versions
-if ($application_revision == 'svn')
-{
-    // Add the svn revision number
-    preg_match('/([0-9]+)/','$LastChangedRevision$',$revision);
-    $application_revision .= $revision[0];
-}
-
-// Clean PHP_SELF server variable to avoid potential XSS security issue
-$_SERVER['PHP_SELF'] = substr($_SERVER['PHP_SELF'], 0,
-                            (strlen($_SERVER['PHP_SELF'])
-                            - @strlen($_SERVER['PATH_INFO'])));
-// Report all PHP errors
-error_reporting(E_ALL);
-$oldeh = set_error_handler("sit_error_handler");
 
 if (version_compare(PHP_VERSION, "5.1.0", ">="))
 {
     date_default_timezone_set($CONFIG['timezone']);
 }
-
-$fsdelim = DIRECTORY_SEPARATOR;
 
 // Journal Logging
 // 0 = No logging
@@ -116,31 +94,6 @@ define ('NO_BILLABLE_CONTRACT', 0);
 define ('CONTACT_HAS_BILLABLE_CONTRACT', 1);
 define ('SITE_HAS_BILLABLE_CONTRACT', 2);
 
-// Decide which language to use and setup internationalisation
-require ('i18n/en-GB.inc.php');
-if ($CONFIG['default_i18n'] != 'en-GB')
-{
-    @include ("i18n/{$CONFIG['default_i18n']}.inc.php");
-}
-if (!empty($_SESSION['lang'])
-    AND $_SESSION['lang'] != $CONFIG['default_i18n'])
-{
-    include ("i18n/{$_SESSION['lang']}.inc.php");
-}
-ini_set('default_charset', $i18ncharset);
-
-// Time settings
-$now = time();
-$today = $now+(16*3600);  // next 16 hours, based on reminders being run at midnight this is today
-$lastweek = $now - (7 * 86400); // the previous seven days
-$todayrecent = $now-(16*3600);  // past 16 hours
-
-$CONFIG['upload_max_filesize'] = return_bytes($CONFIG['upload_max_filesize']);
-
-// Set a string to be the full version number and revision of the application
-$application_version_string = trim("v{$application_version} {$application_revision}");
-
-
 //Prevent Magic Quotes from affecting scripts, regardless of server settings
 //Make sure when reading file data,
 //PHP doesn't "magically" mangle backslashes!
@@ -180,28 +133,6 @@ if (get_magic_quotes_gpc())
 
 
 require ('triggers.inc.php');
-
-
-/**
-    * Strip slashes from an array
-    * @param $data an array
-    * @return An array with slashes stripped
-*/
-function stripslashes_array($data)
-{
-    if (is_array($data))
-    {
-        foreach ($data as $key => $value)
-        {
-            $data[$key] = stripslashes_array($value);
-        }
-        return $data;
-    }
-    else
-    {
-        return stripslashes($data);
-    }
-}
 
 /**
     * Authenticate a user with a username/password pair
