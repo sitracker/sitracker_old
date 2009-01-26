d8a842924e97d35d04c4de0339bb3b49dcb37548
Ivan Lucas <ivanlucas@users.sourceforge.net>
2009-01-26 11:37:26 +0000

    Fix table name and ajax call

--- htdocs/reports/external_engineers.php
+++ htdocs/reports/external_engineers.php
@@ -59,19 +59,19 @@ if (!empty($filterby))
     switch ($filterby)
     {
         case 'sla':
-            $filterSQL = "AND incidents.servicelevel = '{$filter}' ";
+            $filterSQL = "AND i.servicelevel = '{$filter}' ";
             $slaChecked = "checked='yes'";
             break;
         case 'maintenanceid':
-            $filterSQL = "AND incidents.maintenanceid = '{$filter}' ";
+            $filterSQL = "AND i.maintenanceid = '{$filter}' ";
             $maintenanceChecked = "checked='yes'";
             break;
         case 'softwareid':
-            $filterSQL = "AND incidents.softwareid = '{$filter}' ";
+            $filterSQL = "AND i.softwareid = '{$filter}' ";
             $softwareChecked = "checked='yes'";
             break;
         case 'product':
-            $filterSQL = "AND incidents.product = '{$filter}' ";
+            $filterSQL = "AND i.product = '{$filter}' ";
             $productCheck = "checked='yes'";
             break;
         default:
@@ -98,7 +98,7 @@ if (!empty($filterby))
     switch ($filterby)
     {
         case 'sla':
-            echo "get_and_display('../ajaxdata.php?actiont=slas&selected={$filter}', 'filter'); hide_filter(false);";
+            echo "get_and_display('../ajaxdata.php?action=slas&selected={$filter}', 'filter'); hide_filter(false);";
             break;
         case 'softwareid':
             echo "get_and_display('../ajaxdata.php?action=skills&selected={$filter}', 'filter'); hide_filter(false);";
@@ -123,122 +123,122 @@ $sql = "SELECT id, name FROM `{$dbEscalationPaths}`";
 $escs = mysql_query($sql);
 while ($escalations = mysql_fetch_object($escs))
 {
-        $html .= "<h3>{$escalations->name}</h3>";
+    $html .= "<h3>{$escalations->name}</h3>";
 
-        $sql = "SELECT i.*, sw.name, c.forenames, c.surname, s.name AS siteName ";
-        $sql .= "FROM `{$dbIncidents}` AS i, `{$dbSoftware}` AS sw, `{$dbContacts}` AS c, `{$dbSites}` AS s ";
-        $sql .= "WHERE escalationpath = '{$escalations->id}' AND closed = '0' AND sw.id = i.softwareid ";
-        $sql .= " AND i.contact = c.id AND c.siteid = s.id ";
+    $sql = "SELECT i.*, sw.name, c.forenames, c.surname, s.name AS siteName ";
+    $sql .= "FROM `{$dbIncidents}` AS i, `{$dbSoftware}` AS sw, `{$dbContacts}` AS c, `{$dbSites}` AS s ";
+    $sql .= "WHERE escalationpath = '{$escalations->id}' AND closed = '0' AND sw.id = i.softwareid ";
+    $sql .= " AND i.contact = c.id AND c.siteid = s.id ";
 
-        if (!empty($filterby))
+    if (!empty($filterby))
+    {
+        switch ($filterby)
         {
-            switch ($filterby)
-            {
-                case 'sla':
-                    $sql .= "AND i.servicelevel = '{$filter}' ";
-                    break;
-                case 'maintenanceid':
-                    $sql .= "AND i.maintenanceid = '{$filter}' ";
-                    break;
-                case 'softwareid':
-                    $sql .= "AND i.softwareid = '{$filter}' ";
-                    break;
-                case 'product':
-                    $sql .= "AND i.product = '{$filter}' ";
-                    break;
-             }
-        }
-        $sql .= $filterSQL;
-
-        $sql .= "ORDER BY externalengineer";
-
-        $result = mysql_query($sql);
-        if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
-
-        $i = 0;
-        while ($obj = mysql_fetch_object($result))
-        {
-            $name = $obj->externalengineer;
-            if (empty($name)) $name = $strNoNameAssociated;
-            $esc[$name]['name'] = $name;
-            $esc[$name]['count']++;
-            $esc[$name][$obj->priority]++;
-            $str = "<span><strong>".$obj->forenames." ".$obj->surname."</strong><br />".$obj->siteName."</span>";
-            $esc[$name]['calls'][$i]['text'] = "<a href=\"javascript:incident_details_window_l('".$obj->id."', 'incident".$obj->id."')\"  title=\"{$obj->title}\" class='info'>[".$obj->id."]{$str}</a> #".$obj->externalid." ".$obj->title;
-            $esc[$name]['calls'][$i]['software'] = $obj->name;
-            $esc[$name]['calls'][$i]['status'] = $obj->status;
-            $esc[$name]['calls'][$i]['localowner'] = $obj->owner;
-            $esc[$name]['calls'][$i]['salfordtowner'] = $obj->towner;
-            $i++;
-        }
-        if (!empty($esc))
-        {
-            $html .= "<table align='center'>";
-            $html .= "<tr><th>{$strExternalEngineersName}</th><th>{$strNumOfCalls}</th>";
-            $html .= "<th align='center'>".priority_icon(4)."</th>";
-            $html .= "<th align='center'>".priority_icon(3)."</th>";
-            $html .= "<th align='center'>".priority_icon(2)."</th>";
-            $html .= "<th align='center'>".priority_icon(1)."</th>";
-            $html .= "<td>";
-            $html .= "<table width='100%'><tr><th width='50%'>{$strIncident}</th><th width='12%'>{$strInternalEngineer}</th><th width='25%'>{$strSoftware}</th><th>{$strStatus}</th></tr></table>\n";
-            $html .= "</td>";
-            $html .= "</tr>\n";
-
-            foreach ($esc AS $engineer)
-            {
-                if (empty($engineer['4']))  $engineer['4'] = 0;
-                if (empty($engineer['3']))  $engineer['3'] = 0;
-                if (empty($engineer['2']))  $engineer['2'] = 0;
-                if (empty($engineer['1']))  $engineer['1'] = 0;
-
-                $html .= "<tr>";
-                $html .= "<td class='shade1'>{$engineer['name']}</td>";
-                $html .= "<td class='shade1'>".$engineer['count']."</td>";
-                $html .= "<td class='shade1'>".$engineer['4']."</td>";
-                $html .= "<td class='shade1'>".$engineer['3']."</td>";
-                $html .= "<td class='shade1'>".$engineer['2']."</td>";
-                $html .= "<td class='shade1'>".$engineer['1']."</td>";
-                $html .= "<td  class='shade1' >";
-                $html .= "<table width='100%'>";
-                foreach ($engineer['calls'] AS $call)
-                {
-                    $replace = array("Response","Action");
-                    $html .= "<tr><td width='50%'>{$call['text']}</td>";
-                    $html .= "<td width='12%'>".user_realname($call['localowner']);
-                    if (!empty($call['salfordtowner']))
-                    {
-                        $html .= "<br />T: ".user_realname($call['salfordtowner']);
-                    }
-                    $html .= "</td><td width='25%'>".$call['software']."</td>";
-                    $html .= "<td>".str_replace($replace,"",incidentstatus_name($call['status']))."</td></tr>";
-                }
-                $html .= "</table>\n\n";
-                $html .= "</td>";
-                $total += $engineer['count'];
-                $c['4'] += $engineer['4'];
-                $c['3'] += $engineer['3'];
-                $c['2'] += $engineer['2'];
-                $c['1'] += $engineer['1'];
-                $html .= "</tr>\n";
+            case 'sla':
+                $sql .= "AND i.servicelevel = '{$filter}' ";
+                break;
+            case 'maintenanceid':
+                $sql .= "AND i.maintenanceid = '{$filter}' ";
+                break;
+            case 'softwareid':
+                $sql .= "AND i.softwareid = '{$filter}' ";
+                break;
+            case 'product':
+                $sql .= "AND i.product = '{$filter}' ";
+                break;
             }
-            $html .= "<tr><td>{$strTotal}:</td><td>{$total}</td>";
+    }
+    $sql .= $filterSQL;
 
-            if (empty($c['4'])) $c['4'] = 0;
-            if (empty($c['3'])) $c['3'] = 0;
-            if (empty($c['2'])) $c['2'] = 0;
-            if (empty($c['1'])) $c['1'] = 0;
+    $sql .= "ORDER BY externalengineer";
 
-            $html .= "<td>".$c['4']."</td>";
-            $html .= "<td>".$c['3']."</td>";
-            $html .= "<td>".$c['2']."</td>";
-            $html .= "<td>".$c['1']."</td>";
-            $html .= "</tr>\n";
-            $html .= "</table>\n\n";
-        }
-        else
+    $result = mysql_query($sql);
+    if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
+
+    $i = 0;
+    while ($obj = mysql_fetch_object($result))
+    {
+        $name = $obj->externalengineer;
+        if (empty($name)) $name = $strNoNameAssociated;
+        $esc[$name]['name'] = $name;
+        $esc[$name]['count']++;
+        $esc[$name][$obj->priority]++;
+        $str = "<span><strong>".$obj->forenames." ".$obj->surname."</strong><br />".$obj->siteName."</span>";
+        $esc[$name]['calls'][$i]['text'] = "<a href=\"javascript:incident_details_window_l('".$obj->id."', 'incident".$obj->id."')\"  title=\"{$obj->title}\" class='info'>[".$obj->id."]{$str}</a> #".$obj->externalid." ".$obj->title;
+        $esc[$name]['calls'][$i]['software'] = $obj->name;
+        $esc[$name]['calls'][$i]['status'] = $obj->status;
+        $esc[$name]['calls'][$i]['localowner'] = $obj->owner;
+        $esc[$name]['calls'][$i]['salfordtowner'] = $obj->towner;
+        $i++;
+    }
+    if (!empty($esc))
+    {
+        $html .= "<table align='center'>";
+        $html .= "<tr><th>{$strExternalEngineersName}</th><th>{$strNumOfCalls}</th>";
+        $html .= "<th align='center'>".priority_icon(4)."</th>";
+        $html .= "<th align='center'>".priority_icon(3)."</th>";
+        $html .= "<th align='center'>".priority_icon(2)."</th>";
+        $html .= "<th align='center'>".priority_icon(1)."</th>";
+        $html .= "<td>";
+        $html .= "<table width='100%'><tr><th width='50%'>{$strIncident}</th><th width='12%'>{$strInternalEngineer}</th><th width='25%'>{$strSoftware}</th><th>{$strStatus}</th></tr></table>\n";
+        $html .= "</td>";
+        $html .= "</tr>\n";
+
+        foreach ($esc AS $engineer)
         {
-            $html .= "<p align='center'>{$strNoIncidents}</p>";
+            if (empty($engineer['4']))  $engineer['4'] = 0;
+            if (empty($engineer['3']))  $engineer['3'] = 0;
+            if (empty($engineer['2']))  $engineer['2'] = 0;
+            if (empty($engineer['1']))  $engineer['1'] = 0;
+
+            $html .= "<tr>";
+            $html .= "<td class='shade1'>{$engineer['name']}</td>";
+            $html .= "<td class='shade1'>".$engineer['count']."</td>";
+            $html .= "<td class='shade1'>".$engineer['4']."</td>";
+            $html .= "<td class='shade1'>".$engineer['3']."</td>";
+            $html .= "<td class='shade1'>".$engineer['2']."</td>";
+            $html .= "<td class='shade1'>".$engineer['1']."</td>";
+            $html .= "<td  class='shade1' >";
+            $html .= "<table width='100%'>";
+            foreach ($engineer['calls'] AS $call)
+            {
+                $replace = array("Response","Action");
+                $html .= "<tr><td width='50%'>{$call['text']}</td>";
+                $html .= "<td width='12%'>".user_realname($call['localowner']);
+                if (!empty($call['salfordtowner']))
+                {
+                    $html .= "<br />T: ".user_realname($call['salfordtowner']);
+                }
+                $html .= "</td><td width='25%'>".$call['software']."</td>";
+                $html .= "<td>".str_replace($replace,"",incidentstatus_name($call['status']))."</td></tr>";
+            }
+            $html .= "</table>\n\n";
+            $html .= "</td>";
+            $total += $engineer['count'];
+            $c['4'] += $engineer['4'];
+            $c['3'] += $engineer['3'];
+            $c['2'] += $engineer['2'];
+            $c['1'] += $engineer['1'];
+            $html .= "</tr>\n";
         }
+        $html .= "<tr><td>{$strTotal}:</td><td>{$total}</td>";
+
+        if (empty($c['4'])) $c['4'] = 0;
+        if (empty($c['3'])) $c['3'] = 0;
+        if (empty($c['2'])) $c['2'] = 0;
+        if (empty($c['1'])) $c['1'] = 0;
+
+        $html .= "<td>".$c['4']."</td>";
+        $html .= "<td>".$c['3']."</td>";
+        $html .= "<td>".$c['2']."</td>";
+        $html .= "<td>".$c['1']."</td>";
+        $html .= "</tr>\n";
+        $html .= "</table>\n\n";
+    }
+    else
+    {
+        $html .= "<p align='center'>{$strNoIncidents}</p>";
+    }
     unset($obj);
     unset($esc);
 }
