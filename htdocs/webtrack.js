edc13c17f0991e6a9fc9eb99abb17847fbd69e4b
Ivan Lucas <ivanlucas@users.sourceforge.net>
2009-01-26 20:26:56 +0000

    Copyright date

--- htdocs/webtrack.js
+++ htdocs/webtrack.js
@@ -1,7 +1,7 @@
 // webtrack.js - Main SiT javascript library
 
 // SiT (Support Incident Tracker) - Support call tracking system
-// Copyright (C) 2000-2008 Salford Software Ltd. and Contributors
+// Copyright (C) 2000-2009 Salford Software Ltd. and Contributors
 //
 // This software may be used and distributed according to the terms
 // of the GNU General Public License, incorporated herein by reference.
