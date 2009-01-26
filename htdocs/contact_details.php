0fad4f6dec823ba8b51831ff649d8a06a91d9e24
Ivan Lucas <ivanlucas@users.sourceforge.net>
2009-01-26 20:48:20 +0000

    Rename to new filename format

--- htdocs/contact_details.php
+++ htdocs/contact_details.php
@@ -223,7 +223,7 @@ while ($contactrow = mysql_fetch_array($contactresult))
     echo "<a href='incident_add.php?action=findcontact&amp;contactid={$id}'>{$strAddIncident}</a> | ";
     echo "<a href='contact_details.php?id={$id}&amp;output=vcard'>".icon('vcard', 16)." vCard</a> | ";
     echo "<a href='contact_edit.php?action=edit&amp;contact={$id}'>{$strEdit}</a> | ";
-    echo "<a href='delete_contact.php?id={$id}'>{$strDelete}</a>";
+    echo "<a href='contact_delete.php?id={$id}'>{$strDelete}</a>";
     echo "</p>\n";
 
 
