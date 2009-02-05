f20d456ec85ef91b49adcea0bc64c5713fb2f68c
Ivan Lucas <ivanlucas@users.sourceforge.net>
2009-02-05 17:57:32 +0000

    Moving files to main sit dir

---
+++ portal/addcontact.php
@@ -0,0 +1,29 @@
+<?php
+// portal/addcontact.php - Add a site contact
+//
+// SiT (Support Incident Tracker) - Support call tracking system
+// Copyright (C) 2000-2009 Salford Software Ltd. and Contributors
+//
+// This software may be used and distributed according to the terms
+// of the GNU General Public License, incorporated herein by reference.
+//
+// Author Kieran Hogg <kieran[at]sitracker.org>
+
+$lib_path = dirname( __FILE__ ).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR;
+require $lib_path.'db_connect.inc.php';
+require $lib_path.'functions.inc.php';
+
+$accesslevel = 'admin';
+
+include $lib_path.'portalauth.inc.php';
+include '../inc/portalheader.inc.php';
+
+if (isset($_POST['submit']))
+{
+    echo process_add_contact('external');
+}
+
+echo show_add_contact($_SESSION['siteid'], 'external');
+
+include '../inc/htmlfooter.inc.php';
+?>
---
+++ portal/contracts.php
@@ -0,0 +1,68 @@
+<?php
+// portal/contracts.inc.php - Shows contact details
+//
+// SiT (Support Incident Tracker) - Support call tracking system
+// Copyright (C) 2000-2009 Salford Software Ltd. and Contributors
+//
+// This software may be used and distributed according to the terms
+// of the GNU General Public License, incorporated herein by reference.
+//
+// Author Kieran Hogg <kieran[at]sitracker.org>
+
+$lib_path = dirname( __FILE__ ).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR;
+require $lib_path.'db_connect.inc.php';
+require $lib_path.'functions.inc.php';
+
+$accesslevel = 'admin';
+
+include $lib_path.'portalauth.inc.php';
+include '../inc/portalheader.inc.php';
+
+
+$id = intval($_GET['id']);
+$contactid = intval($_GET['contactid']);
+$action = cleanvar($_GET['action']);
+if ($id != 0 AND $contactid != 0 AND $action == 'remove')
+{
+    if (in_array($id,
+                 admin_contact_contracts($_SESSION['contactid'], $_SESSION['siteid'])))
+    {
+        $sql = "DELETE FROM `{$dbSupportContacts}`
+                WHERE maintenanceid='{$id}'
+                AND contactid='{$contactid}'
+                LIMIT 1";
+        $result = mysql_query($sql);
+        if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
+        else
+        {
+            html_redirect($_SERVER['PHP_SELF']."?id={$id}");
+            exit;
+        }
+    }
+    else
+    {
+        echo "<p class='error'>{$strPermissionDenied}</p>";
+    }
+}
+elseif ($id != 0 AND $action == 'add' AND intval($_POST['contactid'] != 0))
+{
+    $contactid = intval($_POST['contactid']);
+    $sql = "INSERT INTO `{$dbSupportContacts}`
+            (maintenanceid, contactid)
+            VALUES('{$id}', '{$contactid}')";
+    $result = mysql_query($sql);
+    if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
+    else
+    {
+        html_redirect($_SERVER['PHP_SELF']."?id={$id}");
+        exit;
+    }
+}
+
+echo "<h2>".icon('contract', 32)." {$GLOBALS['strContract']}</h2>";
+
+echo contract_details($id, 'external');
+
+include '../inc/htmlfooter.inc.php'
+
+?>
