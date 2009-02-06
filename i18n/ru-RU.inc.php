c87e89500d3e52b54b8640ea50bf0d1230b7c092
Ivan Lucas <ivanlucas@users.sourceforge.net>
2009-02-06 15:33:42 +0000

    Russian language stub (ru-RU)

---
+++ i18n/ru-RU.inc.php
@@ -0,0 +1,77 @@
+<?php
+// SiT! Language File - Russian (ru-RU)
+
+$languagestring = 'Russian (ru-RU)';
+$i18ncharset = 'UTF-8';
+
+$strAnswer = 'Ответ';
+$strAttachment = 'Присоединен';
+$strAttachments = 'Присоединены';
+$strAwaitingApproval = 'Ожидание Утверждения';
+
+$strClosed = 'Закрыто';
+$strControlPanel = 'Панель управления';
+
+$strDate = 'Дата';
+$strDelete = 'Удалить';
+$strDescription = 'Описание';
+
+$strEdit = 'Редактировать';
+$strEmailAddress = 'Email Адрес';
+
+$strForenames = 'Имя';
+
+$strGo = 'Вперед';
+$strGroup = 'Группа';
+
+$strHours = 'часов';
+
+$strInactive = 'Неактивно';
+
+$strKeywords = 'Ключевые слова';
+$strKnowledgeBase = 'База знаний';
+
+$strLanguage = 'Язык';
+$strLogIn = 'Логин';
+$strLogout = 'Выход';
+
+$strMinutes = 'минут';
+
+$strName = 'Имя';
+$strNever = 'Никогда';
+$strNewPassword = 'Новый Пароль';
+$strNext = 'Следующий';
+$strNobody = 'никто';
+$strNotes = 'Замечания';
+
+$strOpen = 'Открыть';
+$strOpened = 'Открыта';
+
+$strPassword = 'Пароль';
+$strPriority = 'Приоритет';
+$strPrivate = 'Приватный';
+$strPublic = 'Публичный';
+
+$strQuestion = 'Вопрос';
+
+$strRequired = 'Требуется';
+$strReset = 'Сброс';
+
+$strSave = 'Сохранить';
+$strSeconds = 'секунд';
+$strSize = 'Размер';
+$strStatus = 'Состояние';
+$strSuccess = 'Успех';
+$strSurname = 'Фамилия';
+
+$strTelephone = 'Телефон';
+$strTemplates = 'Шаблоны';
+$strTheme = 'Тема';
+$strTime = 'Время';
+$strTotal = 'Всего';
+
+$strUser = 'Пользователь';
+$strUsername = 'Логин';
+
+$strValue = 'Значение';
+?>
--- lib/strings.inc.php
+++ lib/strings.inc.php
@@ -253,6 +253,7 @@ $availablelanguages = array('en-GB' => 'English (British)',
                             'lt-LT' => 'Lietuvių',
                             'nl-NL' => 'Nederlands',
                             'pt-PT' => 'Português',
+                            'ru-RU' => 'Русский',
                             'sl-SL' => 'Slovenščina',
                             'zh-TW' => '繁體中文',
                            );
