fe0e91bf4185e82cee5b1f27c7cc576c37af6e54
Ivan Lucas <ivanlucas@users.sourceforge.net>
2009-02-07 11:00:57 +0000

    Russian Alphabet

--- i18n/ru-RU.inc.php
+++ i18n/ru-RU.inc.php
@@ -4,6 +4,11 @@
 $languagestring = 'Russian (ru-RU)';
 $i18ncharset = 'UTF-8';
 
+// List of letters of the alphabet for this language
+// in standard alphabetical order (upper case, where applicable)
+$i18nAlphabet = 'AБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ';
+
+// list of strings (Alphabetical by key)
 $strAnswer = 'Ответ';
 $strAttachment = 'Присоединен';
 $strAttachments = 'Присоединены';
