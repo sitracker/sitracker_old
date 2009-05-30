39b5086d14afb8681e36c41903586d718da405d9
Ivan Lucas <ivanlucas@users.sourceforge.net>
2009-05-30 12:56:43 +0000

    It's 3.50 that isn't yet released

--- releasenotes.php
+++ releasenotes.php
@@ -27,7 +27,7 @@ echo "<h2>Release Notes</h2>";
 echo "<div id='help'>";
 echo "<h4>This is a summary of the full release notes showing only the most important changes, for more detailed notes and the latest information on this release please <a href='http://sitracker.org/wiki/ReleaseNotes'>see the SiT website</a>:</h4>";
 
-echo "<h3><a name='3.45'>v3.45</a></h3>";
+echo "<h3><a name='3.50'>v3.50</a></h3>";
 echo "<div>";
 echo "<ul><li>Not yet released</li></ul>";
 echo "</div>";
