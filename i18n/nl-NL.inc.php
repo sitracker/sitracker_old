3c4216079f8cd94b6ec316e896f57e34b9dac772
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2009-06-08 12:02:50 +0000

    Add Dutch translation by Ed Kolman

--- i18n/nl-NL.inc.php
+++ i18n/nl-NL.inc.php
@@ -1,11 +1,144 @@
 <?php
-// SiT! Language File - Dutch (nl-NL)
+// SiT! Language File - Dutch (nl-NL) by Ed Kolman
 
 $languagestring = ' (nl-NL)';
 $i18ncharset = 'UTF-8';
 
 // list of strings (Alphabetical by key)
+$str0MeansUnlimited = '0 betekend zonder limiet';
+
+$str1Day = '1 dag';
+$str1Minute = '1 minuut';
+$str1Month = '1 maand';
+$str1Hour = '1 uur';
+$str1IncidentsLoggedViaPortal = '1 Incindent gelogd via de portal';
+$str1Second = '1 seconde';
+$str1st = 'Eerste';
+$str1Year = '1 jaar';
+
+$str2nd = 'Tweede';
+
+$str3rd = 'Derde';
+
+$strAbout = 'ongeveer';
+$strAboutPerson = 'ongeveer %s';
+$strAboutXMinutesAgo = 'ongeveer %d minuten geleden';
+$strAboveMustBeCompletedToAllowDeductions = 'Bovenste moet afgerond zijn voordat';
+$strAbsentSick = 'Niet aanwezig, ziek';
+$strAcceptingIncidents = 'Accepteerd incidenten';
+$strAccountManager = 'Account Manager';
+$strActual = 'Aktueel';
+$strAcknowledge = 'Mededeling';
+$strAccepting = 'Accepteren';
+$strAccessDetails = 'Toegang details';
+$strAction = 'Actie';
+$strActions = 'Acties';
+$strActionNeeded = 'Actie nodig';
+$strActionPlan = 'Actie plan';
+$strActionPlanBy = 'Actie plan bij %s';
+$strActionPlanHasBeenMade = 'Actieplan is gemaakt';
+$strActionTaken = 'Actie taken';
+$strActive = 'Actief';
+$strActiveContracts = 'Actieve contracten';
+$strActiveUnassigned = 'Actief (niet toegewezen)';
+$strActivities = 'Activiteiten';
+$strActivitiesForX = 'Activiteiten voor %s';
+$strActivity = 'Activiteit';
+$strActivityAdded = 'Bijgevoegde activiteit';
+$strActivityBilling = 'Actieve rekening';
+$strActivityBillingInfo = 'Alleen gesloten activiteiten worden getoond voor afrekening';
+$strActivityCompleted = 'Activiteit %s, afgerond totale tijs was: %s minuten';
+$strActivityContainsNoNotes = 'Activiteit heeft geen aantekeningen';
+$strActivityList = 'Activiteitenlijst';
+$strActivityMarkedCompleteSuccessfully = 'Activiteit gemarkeerd als afgeronf';
+$strActivityStarted = 'Activiteit gestart: %s';
+$strAdd = 'Voeg toe';
+$strAddAction = 'Voeg actie toe';
+$strAddActionToXTrigger = 'Voeg actie aan \'%s\' trigger';
+$strAddContact = 'Voeg contact toe';
+$strAddContract = 'Voeg contract toe';
+$strAddContractConfirm = 'Weet je zeker dat je dit contact wil toevoegen?';
+$strAdded = 'Toegevoegd';
+$strAddEmailConfirm = 'Weet je zeker dat je deze e-mail wil toevoegen?';
+$strAddEmailTemplate = 'Voeg e-mail template toe';
+$strAddFeedbackForm = 'Voeg feedback formulier toe';
+$strAddIncident = 'Voeg incident toe';
+$strAdditional = 'Aditioneel';
+$strAdditionalInfo = 'Aditionele informatie';
+$strAddKBArticle = 'Voeg een knowledge base artikel toe';
+$strAddLink = 'Voeg link toe';
+$strAddNew = 'Voeg nieuwe toe';
+$strAddNewSiteContact = 'Voeg klantencontact toe';
+$strAddNewSupportedContact = 'Voeg een supportcontact toe';
+$strAddNote = 'Voeg opmerking toe';
+$strAddProduct = 'Voeg product toe';
+$strAddProductInformation = 'Voeg productinformatie toe';
+$strAddProductQuestion = 'Voeg productvragen toe';
+$strAddReseller = 'Voeg reseller toe';
+$strAddress = 'Adres';
+$strAddress1 = 'Adres 1';
+$strAddress2 = 'Adres 2';
+$strAddRSSAtomFeed = 'Voeg RSS/Atom feed toe';
+$strAddService = 'Voeg service toe';
+$strAddServiceLevel = 'Voeg servicelevel toe';
+$strAddsIncidentBasedOnEmail = 'Voeg een incident toe op basis van de inhoud van een e-mail';
+$strAddSite = 'Voeg klant toe';
+$strAddSiteSupportIncident = 'Voeg klant support incident toe';
+$strAddSkill = 'Voeg een skill toe';
+$strAddTask = 'Voeg een taak toe';
+$strAddTemplate = 'Voeg een template toe';
+$strAdditionFail = 'Toevoegen niet gelukt';
+$strADuplicateAlreadyExists = 'Een dubbele entry bestaat';
+$strAddUser = 'Voeg gebruiker toe';
+$strAddVendor = 'Voeg leverancier toe';
+$strAdjust = 'Voeg toe';
+$strAdjustActivityDuration = 'Voeg een activiteitsduur toe';
+$strAdmin = 'Admin';
+$strAdminContact = 'Admin contact';
+$strAdminContactForContracts = 'U bent een admin contact voor de volgende contracten, U kunt kiezen wie deze incidenten in de portal kan zien.';
+$strAdministratorsOnly = 'Alleen voor administartors';
+$strAdminOnly = 'Alleen voor administartors';
+$strAdvanced = 'Geavanceerd';
+$strAfternoon = 'Middag';
+$strAge = 'Leeftijd';
+$strAgo = '%s geleden';
+$strAListOfAvailableTriggers = 'Een lijst van beschikbare triggers en acties die gezet zijn wanneer trigger gebeurde';
+$strAll = 'Alle';
+$strAllActivities = 'Alle activiteiten';
+$strAllClosed = 'Alle gesloten';
+$strAllContacts = 'Alle contacten';
+$strAllDay = 'Alle dagen';
+$strAllIncidents = 'Alle incidenten';
+$strAllOnline = 'Alle online';
+$strAllOpen = 'Alle open';
+$strAllTasks = 'Alle taken';
+$strAllowIncidentReopen = 'Sta toe voor incident heropening';
+$strAllSiteContactsSupported = 'Alle klant contacten zijn toegestaan';
+$strAlphabetically = 'Alfabetisch';
+$strAlternative = 'Alternatief';
+$strAmount = 'Hoeveelheid';
+$strAmountToEditBy = 'Hoeveelheid aan te passen door +ve/-ve';
+$strAM = 'ben';
 $strAnswer = 'Antwoord';
+$strApplication = 'Applicatie';
+$strApprovalStatusUnknown = 'Status goedkeuring onbekend';
+$strApprove = 'Keur goed';
+$strApproveAll = 'Keur alle goed';
+$strApproveTimesheets = 'Goedkeuring timesheets';
+$strApproved = 'Goedgekeurd';
+$strApproveFree = 'Keur vrij goed';
+$strApprovedFree = 'Vrij goedgekeurd';
+$strApproveHolidays = 'Goedkeuring vakantie';
+$strAprAbbr = 'Apt';
+$strApril = 'April';
+$strArchivedApproved = 'Gearchiveerd goedgekeurd';
+$strArchivedApprovedFree = 'Gearchiveerd goedgekeurd \'vrij\'';
+$strArchiveDaysBookedPriorToX = 'Gearchiveerde dagen geboekt eerder als %s';
+$strArchivedDenied = 'Gearchiveerd afgekeurd';
+$strArchivedNotRequested = 'Gearchiveerd niet aangevraagd';
+$strArchivedRequested = 'Gearchiveerd aangevraagd';
+$strArticlesPublishedRecently = 'Recent gepubliceerde artikelen';
+$strArticlesPublishedToday = 'Vaandag gepubliceerde artikelen';
 $strAttachment = 'Bijlage';
 $strAwaitingApproval = 'Wacht op goedkeuring';
 
@@ -76,4 +209,23 @@ $strUsers = 'Gebruikerss';
 $strUTCOffset = 'UTC Tijdsoffset (Tijdzone)';
 
 $strValue = 'Waarde';
+
+$strWed = 'Woens';
+$strWednesday = 'Woensdag';
+$strWeek = 'Week';
+$strWelcome = 'Welkom';
+$strWeekView = 'Weekoverzicht';
+$strWhatsNew = 'Wat is nieuw?';
+$strWhosAwayToday = 'Wie zijn er vandaag niet aanwezig';
+$strWordList = 'Woordenlijst';
+$strWorkaround = 'Workaround';
+
+$strYear = 'Jaar';
+$strYearly = 'Jaarlijks';
+$strYearView = 'Jaaroverzicht';
+$strYes = 'Ja';
+$strYesterday = 'Gisteren';
+$strYou = 'U';
+
+$strZeroRemaining = 'Nul over';
 ?>
