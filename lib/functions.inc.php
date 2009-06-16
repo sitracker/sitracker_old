d9f14d8a296061a6bd5007d868d247005cb5189a
Ivan Lucas <ivanlucas@users.sourceforge.net>
2009-06-16 16:45:30 +0000

    Patch by mfeider67.  Fix race condition when checking for scheduler events to run. Mantis 736

--- lib/functions.inc.php
+++ lib/functions.inc.php
@@ -5547,7 +5547,7 @@ function schedule_actions_due()
     $actions = FALSE;
     $sql = "SELECT * FROM `{$dbScheduler}` WHERE status = 'enabled' AND type = 'interval' ";
     $sql .= "AND UNIX_TIMESTAMP(start) <= $now AND (UNIX_TIMESTAMP(end) >= $now OR UNIX_TIMESTAMP(end) = 0) ";
-    $sql .= "AND IF(UNIX_TIMESTAMP(lastran) > 0, UNIX_TIMESTAMP(lastran) + `interval` < $now, UNIX_TIMESTAMP(NOW())) ";
+    $sql .= "AND IF(UNIX_TIMESTAMP(lastran) > 0, UNIX_TIMESTAMP(lastran) + `interval` <= $now, UNIX_TIMESTAMP(NOW())) ";
     $sql .= "AND laststarted <= lastran";
     $result = mysql_query($sql);
     if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
