d729ae7e302e4acd75af3be1c10b04e414d9bfa8
Kieran Hogg <kieran_hogg@users.sourceforge.net>
2009-07-24 16:06:45 +0000

    bodged paste

--- inventory_view.php
+++ inventory_view.php
@@ -6,7 +6,7 @@
 //
 // This software may be used and distributed according to the terms
 // of the GNU General Public License, incorporated herein by reference.
-if (
+
 $permission = 0;
 
 require ('core.php');
