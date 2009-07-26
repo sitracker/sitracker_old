17b1590249d3a59820a5e76ef54f5ff6562ae53c
Ivan Lucas <ivanlucas@users.sourceforge.net>
2009-07-26 10:06:50 +0000

    i18n / wording fix on button

--- role_edit.php
+++ role_edit.php
@@ -24,74 +24,74 @@ $submit = cleanvar($_REQUEST['submit']);
 if (empty($submit))
 {
     include (APPLICATION_INCPATH . 'htmlheader.inc.php');
-    
+
     echo show_form_errors('role_edit');
     clear_form_errors('role_edit');
-    
-	$sql = "SELECT * FROM `{$dbRoles}` WHERE id = {$roleid}";
+
+    $sql = "SELECT * FROM `{$dbRoles}` WHERE id = {$roleid}";
     $result = mysql_query($sql);
     if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_WARNING);
 
     echo "<h2>{$strEditRole}</h2>";
-    
+
     if (mysql_num_rows($result) > 0)
     {
         $obj = mysql_fetch_object($result);
         echo "<form action'{$_SERVER['PHP_SELF']}' name='role_edit' method='post' >";
         echo "<table class='vertical' align='center'>";
         echo "<tr><th>{$strRole}</th><td>{$obj->id}</td></tr>";
-        echo "<tr><th>{$strName}</th><td><input type='text' name='rolename' id='rolename' value='{$obj->rolename}' />'</td></tr>";
+        echo "<tr><th>{$strName}</th><td><input type='text' name='rolename' id='rolename' value='{$obj->rolename}' /></td></tr>";
         echo "<tr><th>{$strDescription}</th><td><textarea name='description' id='description' rows='5' cols='30'>{$obj->description}</textarea></td></tr>";
         echo "</table>";
         echo "<input type='hidden' name='roleid' id='roleid' value='{$roleid}' />";
-        echo "<p><input name='submit' type='submit' value='{$strAddRole}' /></p>";
+        echo "<p><input name='submit' type='submit' value='{$strEditRole}' /></p>";
         echo "</form>";
     }
     else
     {
-    	echo "<p class='warning'>{$strNoRecords}</p>";
+        echo "<p class='warning'>{$strNoRecords}</p>";
     }
-    
+
     include (APPLICATION_INCPATH . 'htmlfooter.inc.php');
 }
 else
 {
-	$rolename = cleanvar($_REQUEST['rolename']);
+    $rolename = cleanvar($_REQUEST['rolename']);
     $description = cleanvar($_REQUEST['description']);
     $copyfrom = cleanvar($_REQUEST['copyfrom']);
-    
+
     $_SESSION['formdata']['role_edit'] = $_REQUEST;
-    
+
     if (empty($rolename))
     {
         $errors++;
-        $_SESSION['formerrors']['role_edit']['rolename']= "{$strYouMissedARequiredField}</p>\n";
+        $_SESSION['formerrors']['role_edit']['rolename']= sprintf($strFieldMustNotBeBlank, $strName);
     }
-    
+
     $sql = "SELECT * FROM `{$dbRoles}` WHERE rolename = '{$rolename}' AND id != {$roleid}";
     $result = mysql_query($sql);
     if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
-    
+
     if (mysql_num_rows($result) > 0)
     {
         $errors++;
         $_SESSION['formerrors']['role_edit']['duplicaterole']= "{$strADuplicateAlreadyExists}</p>\n";
     }
-    
+
     if ($errors == 0)
     {
         clear_form_data('role_add');
         clear_form_errors('role_add');
-        
+
         $sql = "UPDATE `{$dbRoles}` SET rolename = '{$rolename}', description = '{$description}' WHERE id = {$roleid}";
         $result = mysql_query($sql);
         if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
         if (mysql_affected_rows() > 0) html_redirect("role.php?roleid={$roleid}", TRUE);
-        else html_redirect($_SESSION['PHP_SELF'], FALSE); 
+        else html_redirect($_SESSION['PHP_SELF'], FALSE);
     }
     else
     {
-    	html_redirect($_SESSION['PHP_SELF'], FALSE);
+        html_redirect($_SESSION['PHP_SELF'], FALSE);
     }
 }
 
